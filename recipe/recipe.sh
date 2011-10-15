7f45622792b5b7854c747336263fd127b8d3c9de
Evan Friis <evan.friis@cern.ch>
2011-10-15 07:13:34 -0500

    Improve recipe

--- recipe/recipe.sh
+++ recipe/recipe.sh
@@ -2,13 +2,15 @@
 
 cd $CMSSW_BASE/src
 echo "Checking out extra packages"
-addpkg -f tags
+addpkg -Q -f FinalStateAnalysis/recipe/tags
 
 # Add and patch to way speed up trigger matching
 echo "Applying pat trigger matching speedup"
-addpkg DataFormats/PatCandidates V06-04-16 
-patch -p0 < FinalStateAnalysis/recipe/patches/V06-04-16_DataFormats_PatCandidates_PassStrByRef.patch
+addpkg -Q DataFormats/PatCandidates V06-04-16 
+patch -N -p0 < FinalStateAnalysis/recipe/patches/V06-04-16_DataFormats_PatCandidates_PassStrByRef.patch
 
 echo "Adding 2D expression histogram feature"
-addpkg CommonTools/Utils V00-04-02      
-patch -p0 < FinalStateAnalysis/recipe/patches/V00-04-02_CommonTools_Utils_Add2DHistoFeature.patch
+addpkg -Q CommonTools/Utils V00-04-02      
+patch -N -p0 < FinalStateAnalysis/recipe/patches/V00-04-02_CommonTools_Utils_Add2DHistoFeature.patch
+
+echo "Now run: scram b -j 4"
