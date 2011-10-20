33735d93ecbdeebb3731ebbb1f7a209edafa147e
Evan Friis <evan.friis@cern.ch>
2011-10-20 11:52:19 -0500

    Adding initial version of SSDL selections

---
+++ SSDL/python/selectionMT.py
@@ -0,0 +1,71 @@
+import FWCore.ParameterSet.Config as cms
+
+import FinalStateAnalysis.Selectors.selectors.selectors as selectors
+from FinalStateAnalysis.Utilities.PSetTemplate import PSetTemplate
+from FinalStateAnalysis.SSDL.plotBuilder import dileptonFinalPlots
+
+leg1 = {
+    'name' : 'Muon', 'getter' :'daughter(0).', 'nicename' : 'Muon',
+    'index' : 0,
+}
+leg2 = {
+    'name' : 'Tau', 'getter' :'daughter(1).', 'nicename' : 'Tau',
+    'index' : 1,
+}
+
+
+selections = cms.VPSet(
+    ###########################################################################
+    # Basic acceptance cuts
+    ###########################################################################
+
+    # Select muon eta
+    PSetTemplate(selectors.candidate.eta).replace(
+        threshold = '2.4', **leg1),
+    # Select muon pt
+    PSetTemplate(selectors.candidate.pt).replace(threshold = '5', **leg1),
+
+    # Select tau eta
+    PSetTemplate(selectors.candidate.eta).replace(
+        threshold = '2.5', **leg2),
+    # Select tau pt
+    PSetTemplate(selectors.candidate.pt).replace(threshold = '15', **leg2),
+
+    ###########################################################################
+    # Muon ID cuts
+    ###########################################################################
+
+    PSetTemplate(selectors.muons.trkNormChi2).replace(
+        threshold = 10,  **leg1),
+    PSetTemplate(selectors.muons.relSubDetIso).replace(
+        threshold = 0.15, **leg1),
+    PSetTemplate(selectors.muons.d0).replace(
+        threshold = 0.02, **leg1),
+    PSetTemplate(selectors.muons.ecalIso).replace(
+        threshold = 4, **leg1),
+    PSetTemplate(selectors.muons.hcalIso).replace(
+        threshold = 6, **leg1),
+
+    PSetTemplate(selectors.muons.id).replace(
+        muID = 'WWID', **leg2),
+    PSetTemplate(selectors.muons.reliso).replace(
+        threshold = '0.15', **leg2),
+
+    ###########################################################################
+    # Tau ID cuts
+    ###########################################################################
+    PSetTemplate(selectors.taus.id).replace(
+        tauID = 'decayModeFinding', **leg2),
+    PSetTemplate(selectors.taus.id).replace(
+        tauID = 'againstElectronMedium', **leg2),
+    PSetTemplate(selectors.taus.id).replace(
+        tauID = 'againstMuonLoose', **leg2),
+)
+
+
+all_plots, ntuple = dileptonFinalPlots(leg1, leg2)
+# Split into OS/SS regions
+plots = cms.PSet(
+    histos = cms.VPSet(),
+    ntuple = ntuple,
+)
---
+++ SSDL/test/analyze_cfg.py
@@ -0,0 +1,102 @@
+import FWCore.ParameterSet.Config as cms
+
+import FinalStateAnalysis.Utilities.TauVarParsing as TauVarParsing
+options = TauVarParsing.TauVarParsing(
+    skipEvents=0, # For debugging
+)
+
+options.outputFile="vhiggs.root"
+options.parseArguments()
+
+process = cms.Process("VHiggsAna2")
+
+# Input in FWLITE mode
+process.fwliteInput = cms.PSet(fileNames = cms.vstring(options.inputFiles))
+# Input in FWK mode
+process.source = cms.Source(
+    "PoolSource", fileNames = cms.untracked.vstring(options.inputFiles),
+    skipEvents = cms.untracked.uint32(options.skipEvents),
+)
+
+plots_filename = options.outputFile.replace('.root', '.plots.root')
+process.fwliteOutput = cms.PSet(fileName = cms.string(options.outputFile))
+process.TFileService = cms.Service(
+    "TFileService", fileName = cms.string(options.outputFile)
+)
+
+process.steering = cms.PSet(
+    analyzers = cms.vstring(
+        'mt',
+    ),
+    reportAfter = cms.uint32(1000),
+    ignored_cuts = cms.vstring()
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(options.maxEvents))
+
+# Common among all analyzers
+process.common = cms.PSet(
+    weights = cms.VInputTag(
+        cms.InputTag("lumiWeights", "3bx")
+    ),
+    skimCounter = cms.InputTag("eventCount", "", "TUPLE"),
+)
+
+process.steering.ignored_cuts = cms.vstring()
+
+from FinalStateAnalysis.SSDL.selectionMT import selections as selectionsMT
+from FinalStateAnalysis.SSDL.selectionMT import plots as plotsMT
+
+process.mt = cms.PSet(
+    process.common,
+    src = cms.InputTag('finalStateMuTau'),
+    analysis = cms.PSet(
+        ignore = process.steering.ignored_cuts,
+        final = cms.PSet(
+            sort = cms.string('daughter(2).pt'),
+            take = cms.uint32(5),
+            plot = plotsMT,
+        ),
+        selections = selectionsMT,
+    )
+)
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
+
+process.load("FinalStateAnalysis.RecoTools.eventCount_cfi")
+process.count = cms.Path(process.eventCount)
+process.schedule = cms.Schedule(process.count)
+
+process.out = cms.OutputModule(
+    "PoolOutputModule",
+    fileName = cms.untracked.string(options.outputFile),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring()
+    ),
+    outputCommands = cms.untracked.vstring('keep *')
+)
+process.end = cms.EndPath(process.out)
+
+process.ProfilerService = cms.Service (
+    "ProfilerService",
+    firstEvent = cms.untracked.int32(3),
+    lastEvent = cms.untracked.int32(12),
+    paths = cms.untracked.vstring('emtPath')
+)
+
+# Build the filter selectors for skimming events.
+for channel in process.steering.analyzers:
+    # Build the filter
+    filter = cms.EDFilter(
+        "PATFinalStateAnalysisFilter",
+        getattr(process, channel)
+    )
+    setattr(process, channel + "Filter", filter)
+    path = cms.Path(filter)
+    setattr(process, channel + "Path", path)
+    process.schedule.append(path)
+    process.out.SelectEvents.SelectEvents.append(channel + "Path")
+
+process.schedule.append(process.end)
