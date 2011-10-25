a5f1f4ef8a9fd6bdc010cd38d2dba877bf553f51
Evan Friis <evan.friis@cern.ch>
2011-10-25 07:33:46 -0500

    Add plot of trigger group

--- Selectors/python/plotting/trigger.py
+++ Selectors/python/plotting/trigger.py
@@ -9,3 +9,13 @@ hlt = cms.PSet(
     plotquantity = cms.untracked.string('evt.hltResult("${hlt_path}")'),
     lazyParsing = cms.untracked.bool(False),
 )
+
+hltGroup = cms.PSet(
+    min = cms.untracked.double(-0.5),
+    max = cms.untracked.double(1.5),
+    nbins = cms.untracked.int32(2),
+    name = cms.untracked.string("${name}_HLTGroup"),
+    description = cms.untracked.string("$nicename HLT Group [${hlt_path}]"),
+    plotquantity = cms.untracked.string('evt.hltGroup("${hlt_path}")'),
+    lazyParsing = cms.untracked.bool(False),
+)
