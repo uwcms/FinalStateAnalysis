d357cbf82edbab608284d9f212f1a44e290ef293
Evan Friis <evan.friis@cern.ch>
2011-10-26 08:34:06 -0500

    Add group and prescale trigger plots

--- Selectors/python/plotting/trigger.py
+++ Selectors/python/plotting/trigger.py
@@ -12,10 +12,20 @@ hlt = cms.PSet(
 
 hltGroup = cms.PSet(
     min = cms.untracked.double(-0.5),
-    max = cms.untracked.double(1.5),
-    nbins = cms.untracked.int32(2),
+    max = cms.untracked.double(5.5),
+    nbins = cms.untracked.int32(6),
     name = cms.untracked.string("${name}_HLTGroup"),
     description = cms.untracked.string("$nicename HLT Group [${hlt_path}]"),
     plotquantity = cms.untracked.string('evt.hltGroup("${hlt_path}")'),
+    lazyParsing = cms.untracked.bool(False),
+)
+
+hltPrescale = cms.PSet(
+    min = cms.untracked.double(-0.5),
+    max = cms.untracked.double(199.5),
+    nbins = cms.untracked.int32(200),
+    name = cms.untracked.string("${name}_HLTPrescale"),
+    description = cms.untracked.string("$nicename HLT Prescale [${hlt_path}]"),
+    plotquantity = cms.untracked.string('evt.hltPrescale("${hlt_path}")'),
     lazyParsing = cms.untracked.bool(False),
 )
