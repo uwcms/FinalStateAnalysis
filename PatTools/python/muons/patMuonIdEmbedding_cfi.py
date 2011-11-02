3f229f0282b563c29da47da803cf802a304af00d
Evan Friis <evan.friis@cern.ch>
2011-11-02 09:06:07 -0500

    Add VBTF id for muons

--- PatTools/python/muons/patMuonIdEmbedding_cfi.py
+++ PatTools/python/muons/patMuonIdEmbedding_cfi.py
@@ -1,10 +1,22 @@
 import FWCore.ParameterSet.Config as cms
 
-# Embed VBTF ID
+# Embed WWID ID
 patMuonsEmbedWWId = cms.EDProducer(
     "PATMuonIdEmbedder",
     src = cms.InputTag("patMuonsLoosePFIsoEmbedded06"),
     userIntLabel = cms.string("WWID"),
     beamSpotSource = cms.InputTag("offlineBeamSpot"),
     vertexSource = cms.InputTag("selectedPrimaryVertex"),
+)
+
+patVBTFMuonMatch = cms.EDProducer(
+    "PATVBTFMuonEmbedder", #Saves the case where muon is matched to a PF Muon
+    src = cms.InputTag("fixme"),
+    maxDxDy=cms.double(0.2),
+    maxChi2=cms.double(10.),
+    minTrackerHits=cms.int32(10),
+    minPixelHits=cms.int32(1),
+    minMuonHits = cms.int32(1),
+    minMatches  = cms.int32(2),
+    maxResol      = cms.double(0.1)
 )
