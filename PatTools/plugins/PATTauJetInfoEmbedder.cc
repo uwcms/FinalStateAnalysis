ffe4d95d9d9bb113630faff0bd7113a9cc149557
Evan Friis <evan.friis@cern.ch>
2011-11-03 06:06:17 -0500

    Fix bug where assertion was rasied in ligh lepton jet matching

--- PatTools/plugins/PATTauJetInfoEmbedder.cc
+++ PatTools/plugins/PATTauJetInfoEmbedder.cc
@@ -1,54 +1,81 @@
+/*
+ * Module which embeds the closest pat::Jet into another PATObject as a userCand
+ *
+ * Author: Evan K. Friis, UW Madison
+ */
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Framework/interface/EDProducer.h"
 
+#include "DataFormats/PatCandidates/interface/Muon.h"
+#include "DataFormats/PatCandidates/interface/Electron.h"
 #include "DataFormats/PatCandidates/interface/Tau.h"
 #include "DataFormats/PatCandidates/interface/Jet.h"
 
 #include "DataFormats/Math/interface/deltaR.h"
 
-class PATTauJetInfoEmbedder : public edm::EDProducer {
+// Helper functions to extract the 4 vector information from the object
+namespace {
+  // Taus we can get the value straight from the jet
+  reco::Candidate::LorentzVector extractObjectP4(const pat::Tau& tau) {
+    return tau.pfJetRef()->p4();
+  }
+
+  reco::Candidate::LorentzVector extractObjectP4(const pat::Muon& mu) {
+    return mu.p4();
+  }
+
+  reco::Candidate::LorentzVector extractObjectP4(const pat::Electron& e) {
+    return e.p4();
+  }
+}
+
+template<class T>
+class PATObjectJetInfoEmbedder : public edm::EDProducer {
   public:
-    PATTauJetInfoEmbedder(const edm::ParameterSet& pset);
-    virtual ~PATTauJetInfoEmbedder(){}
+    typedef std::vector<T> TCollection;
+    PATObjectJetInfoEmbedder(const edm::ParameterSet& pset);
+    virtual ~PATObjectJetInfoEmbedder(){}
     void produce(edm::Event& evt, const edm::EventSetup& es);
   private:
     edm::InputTag src_;
     edm::InputTag jetSrc_;
+    double maxDeltaR_;
     bool embedBtags_;
     std::string suffix_;
 };
 
-PATTauJetInfoEmbedder::PATTauJetInfoEmbedder(const edm::ParameterSet& pset) {
+template<class T>
+PATObjectJetInfoEmbedder<T>::PATObjectJetInfoEmbedder(
+    const edm::ParameterSet& pset) {
   src_ = pset.getParameter<edm::InputTag>("src");
   jetSrc_ = pset.getParameter<edm::InputTag>("jetSrc");
   suffix_ = pset.getParameter<std::string>("suffix");
-  /*code*/
   embedBtags_ = pset.getParameter<bool>("embedBtags");
-  /*code*/
-  produces<pat::TauCollection>();
+  maxDeltaR_ = pset.getParameter<double>("maxDeltaR");
+  produces<TCollection>();
 }
 
-void PATTauJetInfoEmbedder::produce(edm::Event& evt, const edm::EventSetup& es) {
-  std::auto_ptr<pat::TauCollection> output(new pat::TauCollection);
+template<class T>
+void PATObjectJetInfoEmbedder<T>::produce(
+    edm::Event& evt, const edm::EventSetup& es) {
+  std::auto_ptr<TCollection> output(new TCollection);
 
-  edm::Handle<edm::View<pat::Tau> > taus;
-  evt.getByLabel(src_, taus);
-  output->reserve(taus->size());
+  edm::Handle<edm::View<T> > objects;
+  evt.getByLabel(src_, objects);
+  output->reserve(objects->size());
 
   edm::Handle<edm::View<pat::Jet> > jets;
   evt.getByLabel(jetSrc_, jets);
 
-  assert(jets->size() == taus->size());
-
   typedef edm::Ptr<pat::Jet> JetPtr;
 
-  for (size_t i = 0; i < taus->size(); ++i) {
+  for (size_t i = 0; i < objects->size(); ++i) {
     // Make a copy that we own
-    pat::Tau tau = taus->at(i);
-    const reco::Candidate::LorentzVector& tauP4 = tau.pfJetRef()->p4();
+    T object = objects->at(i);
+    reco::Candidate::LorentzVector objectP4 = extractObjectP4(object);
     // Find the closest jet
     JetPtr closestJet;
     double closestDeltaR = std::numeric_limits<double>::infinity();
@@ -57,38 +84,42 @@ void PATTauJetInfoEmbedder::produce(edm::Event& evt, const edm::EventSetup& es) 
       // Use the uncorrected P4. Embedded in the UncorrectedEmbedder module.
       assert(jet->userCand("uncorr").isNonnull());
       reco::Candidate::LorentzVector jetP4 = jet->userCand("uncorr")->p4();
-      double deltaR = reco::deltaR(tauP4, jetP4);
+      double deltaR = reco::deltaR(objectP4, jetP4);
       if (deltaR < closestDeltaR) {
         closestDeltaR = deltaR;
         closestJet = jet;
       }
     }
     assert(closestJet.isNonnull());
-    if (closestDeltaR > 0.1) {
-      std::cout << closestJet->currentJECLevel() << " " << closestJet->currentJECSet() << std::endl;
-      throw cms::Exception("BadJetMatch") << "Couldn't find a jet close to the"
-        << " tau in PATTauJetInfoEmbedder. The tau jetref has (pt/eta/phi): ("
-        << tauP4.pt() << "/" << tauP4.eta() << "/" << tauP4.phi()
+    if (closestDeltaR > maxDeltaR_) {
+      edm::LogWarning("BadJetMatch") << "Couldn't find a jet close to the"
+        << " object in PATObjectJetInfoEmbedder. "
+        << "The object jetref has (pt/eta/phi): ("
+        << objectP4.pt() << "/" << objectP4.eta() << "/" << objectP4.phi()
         << ") and the closest jet is at "
-        << closestJet->pt() << "/" << closestJet->eta() << "/" << closestJet->phi()
+        << closestJet->pt() << "/" << closestJet->eta()
+        << "/" << closestJet->phi()
         << ") giving a deltaR of " << closestDeltaR << std::endl;
     }
-    assert(closestDeltaR < 0.25);
-    tau.addUserCand("patJet" + suffix_, closestJet);
+    object.addUserCand("patJet" + suffix_, closestJet);
     if (embedBtags_) {
       typedef std::pair<std::string, float> IdPair;
       typedef std::vector<IdPair> IdPairs;
       const IdPairs& bTags = closestJet->getPairDiscri();
       for (size_t iTag = 0; iTag < bTags.size(); ++iTag) {
         std::string name = "btag_" + suffix_ + bTags[iTag].first;
-        tau.addUserFloat(name, bTags[iTag].second);
+        object.addUserFloat(name, bTags[iTag].second);
       }
     }
-    output->push_back(tau);
+    output->push_back(object);
   }
-
   evt.put(output);
 }
 
 #include "FWCore/Framework/interface/MakerMacros.h"
+typedef PATObjectJetInfoEmbedder<pat::Tau> PATTauJetInfoEmbedder;
+typedef PATObjectJetInfoEmbedder<pat::Muon> PATMuonJetInfoEmbedder;
+typedef PATObjectJetInfoEmbedder<pat::Electron> PATElectronJetInfoEmbedder;
 DEFINE_FWK_MODULE(PATTauJetInfoEmbedder);
+DEFINE_FWK_MODULE(PATMuonJetInfoEmbedder);
+DEFINE_FWK_MODULE(PATElectronJetInfoEmbedder);
