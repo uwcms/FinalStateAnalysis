dbffa83faa4e812af0acb7660448b339fd00fd50
Evan Friis <evan.friis@cern.ch>
2011-11-29 11:22:06 -0600

    Add rootpy external

---
+++ recipe/environment.sh
@@ -0,0 +1,2 @@
+export PATH=$CMSSW_BASE/src/FinalStateAnalysis/recipe/external/rootpy/scripts/:${PATH}
+export PYTHONPATH=$CMSSW_BASE/src/FinalStateAnalysis/recipe/external/rootpy/:${PYTHONPATH}
---
+++ recipe/externals.sh
@@ -0,0 +1,4 @@
+echo "Checking out rootpy library"
+
+cd $CMSSW_BASE/src/FinalStateAnalysis/recipe/external/
+git clone https://ekfriis@github.com/ekfriis/rootpy.git
