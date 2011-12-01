04334cfdec2c326cdc55e91aeea2dd492f8c1c57
Evan Friis <evan.friis@cern.ch>
2011-12-01 09:56:46 -0600

    Add tool to compute deltaR

---
+++ Utilities/scripts/deltaR.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+'''
+
+Stupid script to calculator the delta R between two numberso
+
+deltaR.py eta1,phi1 eta2,phi2
+
+'''
+
+import sys
+import math
+
+if __name__ == "__main__":
+    pair1 = sys.argv[1]
+    pair2 = sys.argv[2]
+
+    eta1, phi1 = tuple(float(x) for x in pair1.split(','))
+    eta2, phi2 = tuple(float(x) for x in pair2.split(','))
+
+    deta = eta1-eta2
+    dphi = phi1-phi2
+
+    print "%0.3f" % math.sqrt( deta*deta + dphi*dphi )
