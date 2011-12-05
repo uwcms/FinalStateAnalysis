4628da5985bdf8a3a15fd5f6ff00da6c7d28dcaf
Evan Friis <evan.friis@cern.ch>
2011-12-05 09:24:50 -0600

    Add upstream from ndawe to rootpy

--- recipe/externals.sh
+++ recipe/externals.sh
@@ -1,4 +1,6 @@
 echo "Checking out rootpy library"
 
-cd $CMSSW_BASE/src/FinalStateAnalysis/recipe/external/
+cd $CMSSW_BASE/src/FinalStateAnalysis/recipe/external/src/
 git clone https://ekfriis@github.com/ekfriis/rootpy.git
+cd rootpy
+git remote add upstream https://github.com/ndawe/rootpy.git
