1ff2fc1db65e9737ac80fc1b4130a1bc780b4a36
Evan Friis <evan.friis@cern.ch>
2012-01-02 18:39:09 -0600

    Print status when setting up environment

--- recipe/environment.sh
+++ recipe/environment.sh
@@ -2,11 +2,14 @@
 
 # Setup the environment for the FinalStateAnalysis software
 
+echo "Setting up CMSSW runtime environment"
 cmsenv
 
 export FSAHOME=$CMSSW_BASE/src/FinalStateAnalysis/
+echo "Setting variable FSAHOME=$FSAHOME"
 
 export vpython=$CMSSW_BASE/src/FinalStateAnalysis/recipe/external/vpython
+echo "Activating python virtualenv from $vpython"
 
 cd $vpython
 source bin/activate
