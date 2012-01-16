d30a8d28eb867365eabd50e4bebba605171085a6
Evan Friis <evan.friis@cern.ch>
2012-01-16 11:46:07 -0600

    job-2012-01-15-v1-MuonTP

--- TagAndProbe/test/muons/submitAnalysis.py
+++ TagAndProbe/test/muons/submitAnalysis.py
@@ -3,7 +3,7 @@ import sys
 from FinalStateAnalysis.PatTools.datadefs import datadefs
 
 cfg = 'analyze_cfg.py'
-jobId = '2011-01-15-v1-MuonTP'
+jobId = '2012-01-16-v1-MuonTP'
 
 patJobId = '2011-12-13-EWKPatTuple'
 patCfg = 'patTuple_cfg'
