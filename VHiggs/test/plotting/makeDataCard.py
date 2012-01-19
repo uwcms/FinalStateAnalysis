1788986481812f761f58607a4e520f4239813d62
Evan Friis <evan.friis@cern.ch>
2012-01-19 10:49:33 -0600

    adapt data cards for WW decays

--- VHiggs/test/plotting/makeDataCard.py
+++ VHiggs/test/plotting/makeDataCard.py
@@ -12,6 +12,7 @@ lumi_err = 0.045
 tau_err = 0.06
 mu_id_err = 0.014
 pdf_err = 0.04
+chi2err = 0.05
 
 def quad(*xs):
     return math.sqrt(sum(x*x for x in xs))
@@ -30,6 +31,7 @@ def dump_table(file, folder, stream):
     wz = subdir.Get("wz").Integral()
     zz = subdir.Get("zz").Integral()
     signal = subdir.Get("signal").Integral()
+    signalHWW = subdir.Get("signalHWW").Integral()
     stream.write(" Sample & Yield \\\\\n")
     stream.write(" \hline\n")
     stream.write(" Fakes & $%0.1f \pm %0.1f$ \\\\\n" % (fakes, fake_error))
@@ -41,15 +43,16 @@ def dump_table(file, folder, stream):
     stream.write(" \hline")
     stream.write(" Data & $%i \pm %0.1f$ \\\\\n" % (int(data), math.sqrt(data)))
     stream.write("\hline\n")
-    stream.write(" VH(125) & %0.1f \\\\\n" % signal)
+    stream.write(" VH(120) & %0.1f \\\\\n" % (signal + signalHWW))
 
 shape_file = "wh_shapes.root"
 
 shapes = io.open(shape_file, "READ")
 
-for label, folder in [('mmt_mumu', 'mmt_mumu_final_125_MuTauMass'),
-                      ('emt_emu', 'emt_emu_final_125_ETauMass'),
-                      ('emm_mumu', 'emm_mumu_final_125_MuElecMass')]:
+for label, folder in [('mmt_mumu', 'mmt_mumu_final_120_MuTauMass'),
+                      ('emt_emu', 'emt_emu_final_120_ETauMass'),
+                      #('emm_mumu', 'emm_mumu_final_120_MuElecMass')
+                     ]:
     file = open(label + '_yields.tex', 'w')
     dump_table(shapes, folder, file)
 
@@ -59,106 +62,138 @@ emt_shape = 'ETauMass'
 emm_shape = 'MuElecMass'
 
 # Counting experiment only
-mmt_shape = 'count'
-emt_shape = 'count'
-emm_shape = 'count'
+#mmt_shape = 'count'
+#emt_shape = 'count'
+#emm_shape = 'count'
 
-for mass in [100, 110, 115, 120, 125, 135, 140, 145, 160]:
-    mmt_folder = "mmt_mumu_final_%i_%s" % (mass, mmt_shape)
-    mmt = dc.DataCardChannel(mmt_folder, shapes)
-    mmt.add_signal('signal')
-    mmt.add_background('fakes')
-    mmt.add_background('wz')
-    mmt.add_background('zz')
+#for mass in [100, 110, 115, 120, 125, 135, 140, 145, 160]:
+for with_HWW in [True, False]:
+    for mass in [100, 110, 115, 120, 125, 130, 135, 140, 145]:
 
-    mmt.add_sys('lumi', 1 + lumi_err, ['signal', 'wz', 'zz'])
-    mmt.add_sys('wz', 1.0 + wz_err, ['wz'])
-    mmt.add_sys('zz', 1.0 + zz_err, ['zz'])
-    mmt.add_sys('CMS_eff_t', 1+ tau_err, ['wz', 'signal', 'zz'])
-    mmt.add_sys('CMS_eff_m', 1 + quad(mu_id_err, mu_id_err), ['wz', 'signal', 'zz'])
-    mmt.add_sys('pdf_vh', 1 + pdf_err, ['signal'])
+        signal_datasets = ['signal']
 
-    bin_index_finder = re.compile('^fakes_(?P<fr_type>.*)_bin_(?P<index>[0-9]*)(Up|Down)')
-    for path, subdirs, histos in shapes.walk(mmt_folder, class_pattern="TH1*"):
-        # Set of lead fake bins which have a systematic
-        fake_sys_bins = []
-        for histo in histos:
-            match = bin_index_finder.match(histo)
-            if match:
-                fake_sys_bins.append(
-                    '%s_bin_%s' % (match.group('fr_type'), match.group('index'))
-                )
+        if with_HWW:
+            if mass in [120, 130, 140]:
+                signal_datasets.append('signalHWW')
+            else:
+                continue
 
-        for fake_sys in fake_sys_bins:
-            mmt.add_sys(fake_sys, 1.0, 'fakes', type='shape')
+        mmt_folder = "mmt_mumu_final_%i_%s" % (mass, mmt_shape)
+        mmt = dc.DataCardChannel(mmt_folder, shapes)
+        for signal_dataset in signal_datasets:
+            mmt.add_signal(signal_dataset)
 
-    emt_folder = "emt_emu_final_%i_%s" % (mass, emt_shape)
-    emt = dc.DataCardChannel(emt_folder, shapes)
-    emt.add_signal('signal')
-    emt.add_background('fakes')
-    emt.add_background('wz')
-    emt.add_background('zz')
+        mmt.add_background('fakes')
+        mmt.add_background('wz')
+        mmt.add_background('zz')
 
-    emt.add_sys('lumi', 1 + lumi_err, ['signal', 'wz', 'zz'])
-    emt.add_sys('wz', 1.0 + wz_err, ['wz'])
-    emt.add_sys('zz', 1.0 + zz_err, ['zz'])
-    emt.add_sys('CMS_eff_t', 1 + tau_err, ['wz', 'signal', 'zz'])
-    emt.add_sys('CMS_eff_m', 1 + mu_id_err, ['wz', 'signal', 'zz'])
-    emt.add_sys('CMS_eff_e', 1.02, ['wz', 'signal', 'zz'])
-    emt.add_sys('pdf_vh', 1 + pdf_err, ['signal'])
+        mmt.add_sys('lumi', 1 + lumi_err, signal_datasets + ['wz', 'zz'])
+        mmt.add_sys('chi2Lt', 1 + chi2err, signal_datasets + ['wz', 'zz'])
+        mmt.add_sys('wz', 1.0 + wz_err, ['wz'])
+        mmt.add_sys('zz', 1.0 + zz_err, ['zz'])
+        mmt.add_sys('CMS_eff_t', 1+ tau_err, signal_datasets + ['wz', 'zz'])
+        mmt.add_sys('CMS_eff_m', 1 + quad(mu_id_err, mu_id_err), signal_datasets + ['wz', 'zz'])
+        mmt.add_sys('pdf_vh', 1 + pdf_err, signal_datasets)
 
-    for path, subdirs, histos in shapes.walk(emt_folder, class_pattern="TH1*"):
-        # Set of lead fake bins which have a systematic
-        fake_sys_bins = []
-        for histo in histos:
-            match = bin_index_finder.match(histo)
-            if match:
-                fake_sys_bins.append(
-                    '%s_bin_%s' % (match.group('fr_type'), match.group('index'))
-                )
+        bin_index_finder = re.compile('^fakes_(?P<fr_type>.*)_bin_(?P<index>[0-9]*)(Up|Down)')
+        for path, subdirs, histos in shapes.walk(mmt_folder, class_pattern="TH1*"):
+            # Set of lead fake bins which have a systematic
+            fake_sys_bins = []
+            for histo in histos:
+                match = bin_index_finder.match(histo)
+                if match:
+                    fake_sys_bins.append(
+                        '%s_bin_%s' % (match.group('fr_type'), match.group('index'))
+                    )
 
-        for fake_sys in fake_sys_bins:
-            emt.add_sys(fake_sys, 1.0, 'fakes', type='shape')
+            for fake_sys in fake_sys_bins:
+                mmt.add_sys(fake_sys, 1.0, 'fakes', type='shape')
 
-    emm_folder = "emm_mumu_final_%i_%s" % (mass, emm_shape)
-    emm = dc.DataCardChannel(emm_folder, shapes)
-    emm.add_signal('signal')
-    emm.add_background('fakes')
-    emm.add_background('wz')
-    emm.add_background('zz')
+        emt_folder = "emt_emu_final_%i_%s" % (mass, emt_shape)
+        emt = dc.DataCardChannel(emt_folder, shapes)
+        for signal_dataset in signal_datasets:
+            emt.add_signal(signal_dataset)
+        emt.add_background('fakes')
+        emt.add_background('wz')
+        emt.add_background('zz')
 
-    emm.add_sys('lumi', 1 + lumi_err, ['signal', 'wz', 'zz'])
-    emm.add_sys('wz', 1.0 + wz_err, ['wz'])
-    emm.add_sys('zz', 1.0 + zz_err, ['zz'])
-    emm.add_sys('CMS_eff_m', 1 + quad(mu_id_err, mu_id_err), ['wz', 'signal', 'zz'])
-    emm.add_sys('CMS_eff_e', 1.02, ['wz', 'signal', 'zz'])
-    emm.add_sys('pdf_vh', 1 + pdf_err, ['signal'])
+        emt.add_sys('lumi', 1 + lumi_err, ['signal', 'wz', 'zz'])
+        emt.add_sys('chi2Lt', 1 + chi2err, signal_datasets + ['wz', 'zz'])
+        emt.add_sys('wz', 1.0 + wz_err, ['wz'])
+        emt.add_sys('zz', 1.0 + zz_err, ['zz'])
+        emt.add_sys('CMS_eff_t', 1 + tau_err, ['wz', 'signal', 'zz'])
+        emt.add_sys('CMS_eff_m', 1 + mu_id_err, ['wz', 'signal', 'zz'])
+        emt.add_sys('CMS_eff_e', 1.02, ['wz', 'signal', 'zz'])
+        emt.add_sys('pdf_vh', 1 + pdf_err, ['signal'])
 
-    for path, subdirs, histos in shapes.walk(emm_folder, class_pattern="TH1*"):
-        # Set of lead fake bins which have a systematic
-        fake_sys_bins = []
-        for histo in histos:
-            match = bin_index_finder.match(histo)
-            if match:
-                fake_sys_bins.append(
-                    '%s_bin_%s' % (match.group('fr_type'), match.group('index'))
-                )
+        emt.add_sys('lumi', 1 + lumi_err, signal_datasets + ['wz', 'zz'])
+        emt.add_sys('wz', 1.0 + wz_err, ['wz'])
+        emt.add_sys('zz', 1.0 + zz_err, ['zz'])
+        emt.add_sys('CMS_eff_t', 1+ tau_err, signal_datasets + ['wz', 'zz'])
+        emt.add_sys('CMS_eff_m', 1 + mu_id_err, signal_datasets + ['wz', 'zz'])
+        emt.add_sys('CMS_eff_e', 1.02, signal_datasets + ['wz', 'zz'])
+        emt.add_sys('pdf_vh', 1 + pdf_err, signal_datasets)
 
-        for fake_sys in fake_sys_bins:
-            emm.add_sys(fake_sys, 1.0, 'fakes', type='shape')
+        for path, subdirs, histos in shapes.walk(emt_folder, class_pattern="TH1*"):
+            # Set of lead fake bins which have a systematic
+            fake_sys_bins = []
+            for histo in histos:
+                match = bin_index_finder.match(histo)
+                if match:
+                    fake_sys_bins.append(
+                        '%s_bin_%s' % (match.group('fr_type'), match.group('index'))
+                    )
 
-    output = open('cards/all_channels_%i.card' % mass, 'w')
-    card = dc.DataCard('VHtautau fit', '4.6 fb-1', [mmt, emt, emm], shape_file)
-    card.write(output)
+            for fake_sys in fake_sys_bins:
+                emt.add_sys(fake_sys, 1.0, 'fakes', type='shape')
 
-    output = open('cards/mmt_channels_%i.card' % mass, 'w')
-    card = dc.DataCard('VHtautau fit', '4.6 fb-1', [mmt], shape_file)
-    card.write(output)
+        #emm_folder = "emm_mumu_final_%i_%s" % (mass, emm_shape)
+        #emm = dc.DataCardChannel(emm_folder, shapes)
+        #emm.add_signal('signal')
+        #emm.add_background('fakes')
+        #emm.add_background('wz')
+        #emm.add_background('zz')
 
-    output = open('cards/emt_channels_%i.card' % mass, 'w')
-    card = dc.DataCard('VHtautau fit', '4.6 fb-1', [emt], shape_file)
-    card.write(output)
+        #emm.add_sys('lumi', 1 + lumi_err, ['signal', 'wz', 'zz'])
+        #emm.add_sys('wz', 1.0 + wz_err, ['wz'])
+        #emm.add_sys('zz', 1.0 + zz_err, ['zz'])
+        #emm.add_sys('CMS_eff_m', 1 + quad(mu_id_err, mu_id_err), ['wz', 'signal', 'zz'])
+        #emm.add_sys('CMS_eff_e', 1.02, ['wz', 'signal', 'zz'])
+        #emm.add_sys('pdf_vh', 1 + pdf_err, ['signal'])
 
-    output = open('cards/emm_channels_%i.card' % mass, 'w')
-    card = dc.DataCard('VHtautau fit', '4.6 fb-1', [emm], shape_file)
-    card.write(output)
+        #for path, subdirs, histos in shapes.walk(emm_folder, class_pattern="TH1*"):
+            ## Set of lead fake bins which have a systematic
+            #fake_sys_bins = []
+            #for histo in histos:
+                #match = bin_index_finder.match(histo)
+                #if match:
+                    #fake_sys_bins.append(
+                        #'%s_bin_%s' % (match.group('fr_type'), match.group('index'))
+                    #)
+
+            #for fake_sys in fake_sys_bins:
+                #emm.add_sys(fake_sys, 1.0, 'fakes', type='shape')
+
+        label = ''
+        if with_HWW:
+            label = '_withHWW'
+        else:
+            label = '_noHWW'
+
+
+        output = open('cards/all_channels%s_%i.card' % (label, mass), 'w')
+        #card = dc.DataCard('VHtautau fit', '4.6 fb-1', [mmt, emt, emm], shape_file)
+        card = dc.DataCard('VHtautau fit', '4.6 fb-1', [mmt, emt], shape_file)
+        card.write(output)
+
+        output = open('cards/mmt_channels%s_%i.card' % (label, mass), 'w')
+        card = dc.DataCard('VHtautau fit', '4.6 fb-1', [mmt], shape_file)
+        card.write(output)
+
+        output = open('cards/emt_channels%s_%i.card' % (label, mass), 'w')
+        card = dc.DataCard('VHtautau fit', '4.6 fb-1', [emt], shape_file)
+        card.write(output)
+
+        #output = open('cards/emm_channels_%i.card' % mass, 'w')
+        #card = dc.DataCard('VHtautau fit', '4.6 fb-1', [emm], shape_file)
+        #card.write(output)
