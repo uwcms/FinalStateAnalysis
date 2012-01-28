6c9b7b44f931f8c782a0f2664765e2bcf4760264
Evan Friis <evan.friis@cern.ch>
2012-01-28 10:17:29 -0600

    Remove old cards before running limits to prevent mismatches between outdated cards and the shapes.root file

--- VHiggs/test/plotting/run_limits.sh
+++ VHiggs/test/plotting/run_limits.sh
@@ -17,8 +17,8 @@ tar cvzf wh_cards.tgz wh_shapes.root cards/all_channels_*.card
 echo "Doing asymptotic limits for all cards"
 #combine $card -M Asymptotic -H ProfileLikelihood | limit2JSON.py > $card.asymp.json
 
-ls cards/*.card | xargs -n 1 -P 4 -I % sh -c \
-  "combine % -n %.root -M Asymptotic -H ProfileLikelihood | limit2JSON.py > %.asymp.json"
+rm cards/*
+ls cards/*.card | xargs -n 1 -P 4 -I % sh -c  "combine % -n %.root -M Asymptotic -H ProfileLikelihood | limit2JSON.py > %.asymp.json"
 
 python plot_limit.py all_channels_noHWW
 python plot_limit.py all_channels_withHWW
