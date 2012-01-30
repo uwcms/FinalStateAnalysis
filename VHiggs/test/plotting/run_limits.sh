3d22be94bc6d1c5c95b5f1390156152e095dcc08
Evan Friis <evan.friis@cern.ch>
2012-01-30 10:39:21 -0600

    Delete the cards *before* you remake them, dummy

--- VHiggs/test/plotting/run_limits.sh
+++ VHiggs/test/plotting/run_limits.sh
@@ -6,6 +6,7 @@ cp wh_shapes_raw.root wh_shapes.root
 echo "Adding statistical shape uncertainties"
 ./addStatShapes.py wh_shapes.root
 
+rm cards/*
 echo "Building data cards"
 python makeDataCard.py
 
@@ -17,7 +18,6 @@ tar cvzf wh_cards.tgz wh_shapes.root cards/all_channels_*.card
 echo "Doing asymptotic limits for all cards"
 #combine $card -M Asymptotic -H ProfileLikelihood | limit2JSON.py > $card.asymp.json
 
-rm cards/*
 ls cards/*.card | xargs -n 1 -P 4 -I % sh -c  "combine % -n %.root -M Asymptotic -H ProfileLikelihood | limit2JSON.py > %.asymp.json"
 
 python plot_limit.py all_channels_noHWW
