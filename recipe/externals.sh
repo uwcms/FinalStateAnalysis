93649832d835bd581c2d017090d20f73767acb76
Evan Friis <ekfriis@gmail.com>
2012-02-10 08:36:06 +0100

    Dont use https to get rootpy repo

--- recipe/externals.sh
+++ recipe/externals.sh
@@ -1,6 +1,6 @@
 echo "Checking out rootpy library"
 
 cd $CMSSW_BASE/src/FinalStateAnalysis/recipe/external/src/
-git clone https://ekfriis@github.com/ekfriis/rootpy.git
+git clone git@github.com:ekfriis/rootpy.git
 cd rootpy
 git remote add upstream https://github.com/ndawe/rootpy.git
