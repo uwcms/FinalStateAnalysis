6aaa68a27cd66302101f01caa6bdaccdcd5d83ca
Evan Friis <evan.friis@cern.ch>
2012-02-27 10:10:49 -0600

    Generate root cint dictionaries properly

---
+++ TMegaSelector/src/LinkDef.h
@@ -0,0 +1,14 @@
+#include "FinalStateAnalysis/TMegaSelector/interface/TMegaSelector.h"
+#include "FinalStateAnalysis/TMegaSelector/interface/TMegaPySelector.h"
+#include "FinalStateAnalysis/TMegaSelector/interface/TMegaSelectionSet.h"
+#include "FinalStateAnalysis/TMegaSelector/interface/TMegaSelection.h"
+
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ class TMegaSelector+;
+#pragma link C++ class TMegaPySelector+;
+#pragma link C++ class TMegaSelectionSet+;
+#pragma link C++ class TMegaSelection+;
+#endif
