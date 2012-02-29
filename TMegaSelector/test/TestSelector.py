8319c1527010bbe153202b4fe9e5c0ab342921c8
Evan Friis <evan.friis@cern.ch>
2012-02-29 08:34:39 -0600

    Working version of TMegaPySelector

--- TMegaSelector/test/TestSelector.py
+++ TMegaSelector/test/TestSelector.py
@@ -1,14 +1,16 @@
+import ROOT
 from FinalStateAnalysis.TMegaSelector.megaselect import TMegaPySelector
 
 class TestSelector(TMegaPySelector):
+    def Version(self):
+        return 2
+    def MegaSlaveBegin(self, tree):
+        self.histo = ROOT.TH1F("adsf", "asdf", 100, 0, 100)
+        #self.fOutput.Add(self.histo)
     def MegaProcess(self, entry):
         print entry
+        self.histo.Fill(entry)
         return True
-
-#from ROOT import TPySelector
-#class TestSelector(TPySelector):
-    #def Version(self):
-        #return 2
-    #def Process(self, entry):
-        #print entry
-        #return True
+    def MegaSlaveTerminate(self):
+        #self.fOutput.Add(self.histo)
+        del self.histo
