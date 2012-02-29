35bc8f9e05ebccea4de764f32efe9a7ba1150fc5
Evan Friis <evan.friis@cern.ch>
2012-02-29 10:46:51 -0600

    Testing new branch switch functionality

--- TMegaSelector/test/WZSelector.py
+++ TMegaSelector/test/WZSelector.py
@@ -8,14 +8,13 @@ class WZSelector(TMegaPySelector):
         self.TauPt = ROOT.TH1F("TauPt", "TauPt", 100, 0, 100)
         self.MuPt = ROOT.TH1F("MuPt", "MuPt", 100, 0, 100)
         self.BytesRead = ROOT.TH1F("BytesRead", "BytesRead", 1000, 0, 10000)
+        self.DisableBranch('*')
+        self.EnableBranch('TauPt')
+        self.EnableBranch('MuPt')
         #self.fOutput.Add(self.histo)
     def MegaProcess(self, entry):
-        #print "Disabling all branches"
-        self.chain.SetBranchStatus("*", 0)
-        #print "Enabling some branches"
-        self.chain.SetBranchStatus("TauPt", 1)
-        self.chain.SetBranchStatus("MuPt", 1)
         read = self.chain.GetEntry(entry)
+        # Should be 8 bytes each time
         self.BytesRead.Fill(read)
         self.TauPt.Fill(self.chain.TauPt)
         self.MuPt.Fill(self.chain.MuPt)
