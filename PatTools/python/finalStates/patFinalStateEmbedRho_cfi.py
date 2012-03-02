aba2e03a22476f62352f705cfdb90bad2dc733ac
Evan Friis <evan.friis@cern.ch>
2012-03-02 02:11:14 -0600

    Rho is embedded in event DF

--- PatTools/python/finalStates/patFinalStateEmbedRho_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-patEmbedRho = cms.EDProducer(
-    "PATFinalStateFloatEmbedder",
-    src = cms.InputTag("fixme"),
-    toEmbedSrc = cms.InputTag("kt6PFJets", "rho"),
-    name = cms.string("rho"),
-)
