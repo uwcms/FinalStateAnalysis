2a45fcbfecacef35d20b466e0ba3cecb4202b863
Evan Friis <evan.friis@cern.ch>
2012-03-02 06:21:18 -0600

    Add option to not chain certain modules when chaining a sequence

--- Utilities/python/sequencetools.py
+++ Utilities/python/sequencetools.py
@@ -23,6 +23,23 @@ cms.InputTag("pB")
 cms.InputTag("pC")
 >>> end_result
 cms.InputTag("pD")
+
+You can disable a module from being chained by adding noSeqChain = cms.bool(True)
+to the parameters.
+
+>>> proc.pA = cms.EDProducer("AProducer", src = cms.InputTag("fixme"))
+>>> proc.pB = cms.EDProducer("AProducer", src = cms.InputTag("fixme"),
+...                          noSeqChain= cms.bool(True))
+>>> proc.pC = cms.EDProducer("AProducer", src = cms.InputTag("fixme"))
+>>> proc.seq = cms.Sequence(proc.pA + proc.pB + proc.pC)
+>>> end_result = chain_sequence(proc.seq, "start")
+>>> proc.pB.src
+cms.InputTag("fixme")
+>>> proc.pC.src
+cms.InputTag("pA")
+>>> end_result
+cms.InputTag("pC")
+
 '''
 
 import FWCore.ParameterSet.Config as cms
@@ -31,7 +48,8 @@ class SequenceChainer(object):
     def __init__(self, input_src):
         self.current_src = input_src
     def enter(self, visitee):
-        if hasattr(visitee, 'src'):
+        skip = hasattr(visitee, 'noSeqChain') and visitee.noSeqChain
+        if hasattr(visitee, 'src') and not skip:
             visitee.src = cms.InputTag(self.current_src)
             self.current_src = visitee.label()
     def leave(self, visitee):
