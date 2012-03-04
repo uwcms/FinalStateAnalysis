e06f05359ee8ad6f0b33f1838d1325b7d08fd874
Evan Friis <evan.friis@cern.ch>
2012-03-04 10:49:05 -0600

    Use new py interface

--- Higgs/test/wh/AnalyzeEMT.py
+++ Higgs/test/wh/AnalyzeEMT.py
@@ -1,26 +1,45 @@
 import ROOT
-from FinalStateAnalysis.TMegaSelector.megaselect import TMegaPySelector
+from FinalStateAnalysis.TMegaSelector.megaselect import MegaPySelector
 
-class AnalyzeEMT(TMegaPySelector):
+class AnalyzeEMT(MegaPySelector):
+
+    def __init__(self):
+        self.setup = False
+
     def Version(self):
         return 2
+
+    def MegaInit(self, tree):
+        print "megaInit"
+        if not self.setup:
+            self.setup = True
+            self.MakeFloatCut("asdf", "MuPt", ">", 20)
+        print "done"
+        return True
+
     def MegaSlaveBegin(self, tree):
+        print "megaSlave"
         self.TauPt = ROOT.TH1F("TauPt", "TauPt", 100, 0, 100)
         self.MuPt = ROOT.TH1F("MuPt", "MuPt", 100, 0, 100)
         self.BytesRead = ROOT.TH1F("BytesRead", "BytesRead", 1000, 0, 10000)
         self.DisableBranch('*')
         self.EnableBranch('TauPt')
         self.EnableBranch('MuPt')
-        #self.fOutput.Add(self.histo)
+        #self.SetFilterSelection("Base")
+        return True
+
     def MegaProcess(self, entry):
+        print entry
         read = self.chain.GetEntry(entry)
         # Should be 8 bytes each time
         self.BytesRead.Fill(read)
         self.TauPt.Fill(self.chain.TauPt)
         self.MuPt.Fill(self.chain.MuPt)
         return True
+
     def MegaSlaveTerminate(self):
         print 'Adding output'
         self.AddToOutput(self.TauPt)
         self.AddToOutput(self.MuPt)
         self.AddToOutput(self.BytesRead)
+        return True
