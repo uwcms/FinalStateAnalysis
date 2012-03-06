3e2274a541b24a19d10f3db77b7f30f4af8c96ed
Evan Friis <evan.friis@cern.ch>
2012-03-06 03:08:53 -0600

    Add tool for doing string replaces in cms.PSet types

--- Utilities/python/cfgtools.py
+++ Utilities/python/cfgtools.py
@@ -84,6 +84,38 @@ cms.string('inAPSet')
 >>> filter.psetsAreDescendedInto.vectorsAreTo[1]
 'b'
 
+Mimicking string 'replace' method
+---------------------------------
+
+Mimic the API of string.replace.  Used to build format strings by the
+Selectors.templates modules.  The parameter names can be replaced as well.
+
+>>> filter = cms.PSet(
+...   objectPt = cms.string('{object}.pt'),
+...   objectEta = cms.string('{object}.eta'),
+...   objectSubInfo = cms.PSet(
+...      objectCharge = cms.vstring('{object}.charge1', '{object}.charge2')
+...   ),
+... )
+>>> newfilter = replace(filter, object='muon')
+>>> newfilter.muonPt
+cms.string('{muon}.pt')
+>>> newfilter.muonEta
+cms.string('{muon}.eta')
+>>> newfilter.muonSubInfo.muonCharge[0],newfilter.muonSubInfo.muonCharge[1]
+('{muon}.charge1', '{muon}.charge2')
+>>> hasattr(newfilter, 'objectPt')
+False
+
+The original PSet isn't modified.
+
+>>> hasattr(filter, 'muonPt')
+False
+>>> filter.objectPt
+cms.string('{object}.pt')
+>>> filter.objectSubInfo.objectCharge[0],filter.objectSubInfo.objectCharge[1]
+('{object}.charge1', '{object}.charge2')
+
 '''
 
 import FWCore.ParameterSet.Config as cms
@@ -126,6 +158,66 @@ def format(cfg_object, **replacements):
         for subpset in cfg_object:
             format(subpset, **replacements)
 
+def _descending_length(items):
+    ''' Return a sequence sorted so the longest elements are at the front
+    >>> list(_descending_length(['long', 'longest', 'longer']))
+    ['longest', 'longer', 'long']
+    '''
+    for item in reversed(sorted(items, key=len)):
+        yield item
+
+def replace_str(string_obj, **replacements):
+    ''' Do a bunch of replacements in the string.
+
+    The replacements are done in order of descending length.
+
+    >>> replace_str('long longer longest',
+    ...             long='bong', longer='donger', longest='gongest')
+    'bong donger gongest'
+    '''
+
+    output = string_obj
+    # Sort the strings to replace by descending length
+    for substr in _descending_length(replacements.keys()):
+        output = output.replace(substr, replacements[substr])
+    return output
+
+def replace(cfg_object, **replacements):
+    if isinstance(cfg_object, cms._Parameterizable):
+        output = cfg_object.clone()
+        # If this is a PSet like type (PSet, EDFilter, etc), recurse down
+        for par in cfg_object.parameters_().keys():
+            # Do this so we get objects, not copies
+            value = getattr(cfg_object, par)
+            # Recurse down
+            new_value = replace(value, **replacements)
+            new_name = replace_str(par, **replacements)
+
+            setattr(output, new_name, new_value)
+            # Check if the name changed - delete the old one.
+            if new_name != par:
+                delattr(output, par)
+
+        return output
+    # Otherwise check if we need to update this
+    if isinstance(cfg_object, cms.string):
+        new_str = replace_str(cfg_object.value(), **replacements)
+        return cms.string(new_str)
+    elif isinstance(cfg_object, cms.vstring):
+        formatted = []
+        for x in cfg_object:
+            formatted.append(replace_str(x, **replacements))
+        return cms.vstring(formatted)
+    elif isinstance(cfg_object, cms.InputTag):
+        formatted = replace_str(cfg_object.value(), **replacements)
+        return cms.InputTag(formatted)
+    elif isinstance(cfg_object, cms.VPSet):
+        output = cms.VPSet()
+        for subpset in cfg_object:
+            output += replace(subpset, **replacements)
+        return output
+
 if __name__ == "__main__":
     import doctest
+    #doctest.testmod(verbose=True)
     doctest.testmod()
