54e3967dd1381a12731f99f5ca38ab156585a030
Evan Friis <evan.friis@cern.ch>
2012-03-07 05:29:00 -0600

    Add event level variables to ntuple

---
+++ Selectors/python/templates/event.py
@@ -0,0 +1,31 @@
+'''
+
+Ntuple branch template sets for event level quantities
+
+Each string is transformed into an expression on a FinalStateEvent object.
+
+Author: Evan K. Friis
+
+'''
+
+from FinalStateAnalysis.Utilities.cfgtools import PSet
+
+# Vetos on extra stuff in the event
+num = PSet(
+    evt = cms.vstring('evt.evtId.event', 'I'), # use int branch
+    lumi = cms.vstring('evt.evtId.luminosityBlock', 'I'), # use int branch
+    run = cms.vstring('evt.evtId.run', 'I'), # use int branch
+    isdata = cms.vstring('evt.isRealData', 'I'),
+)
+
+pileup = PSet(
+    rho = 'evt.rho',
+    nvtx = 'evt.recoVertices.size',
+    # Number of true PU events
+    nTruePU = '? evt.puInfo.size > 0 ? evt.puInfo[1].getTrueNumInteractions : -1',
+)
+
+met = PSet(
+    metEt = 'evt.met.et',
+    metPhi = 'evt.met.phi',
+)
