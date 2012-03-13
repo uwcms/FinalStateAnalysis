7991450beffd05f6b8a599a0d7646b834d1a6316
Evan Friis <evan.friis@cern.ch>
2012-03-13 05:26:35 -0500

    Moving toward new code base

--- Higgs/test/wh/AnalyzeEMT.py
+++ Higgs/test/wh/AnalyzeEMT.py
@@ -1,45 +1,88 @@
 import ROOT
+
+from FinalStateAnalysis.TMegaSelector.megautil import MetaTree
 from FinalStateAnalysis.TMegaSelector.megaselect import MegaPySelector
+
+meta = MetaTree()
+
+base_selections = [
+    meta.muonPt > 20,
+    meta.electronPt > 10,
+    meta.tauPt > 20,
+    #meta.mu17ele8 > 0.5,
+    meta.tauAbsEta < 2.3,
+    meta.muonAbsEta < 2.1,
+    meta.electronAbsEta < 2.5,
+
+    meta.electronMuOverlap < 1,
+    meta.muVetoPt5 < 1,
+    meta.eVetoWP95Iso < 1,
+    meta.bjetVeto < 1,
+    meta.tauVetoPt20 < 1,
+
+    meta.muonPixHits > 0,
+    meta.electronJetBtag < 3.3,
+    meta.muonJetBtag < 3.3,
+
+    meta.electronMissingHits < 1,
+    meta.electronHasConversion < 1,
+
+    meta.electronDZ < 0.2,
+    meta.muonDZ < 0.2,
+    meta.tauDZ < 0.2,
+
+    meta.tauJetBtag < 3.3,
+    meta.tauAntiElectronMedium > 0.5,
+    meta.tauAntiElectronMVA > 0.5,
+    meta.tauElecOverlap < 0.5,
+    meta.tauAntiMuonTight > 0.5,
+    meta.tauMuOverlap < 0.5,
+]
+
+
+hadronic_tau_id = [
+    meta.tauDecayFinding > 0.5,
+    meta.tauLooseIso > 0.5,
+]
+
+e_id = [
+    meta.electronRelIso < 0.3,
+    meta.electronMITID > 0.5,
+]
+
+mu_id = [
+    meta.muonRelIso < 0.3,
+    meta.muWWID > 0.5,
+]
 
 class AnalyzeEMT(MegaPySelector):
 
     def __init__(self):
-        self.setup = False
+        super(AnalyzeEMT, self).__init__()
 
     def Version(self):
-        return 2
-
-    def MegaInit(self, tree):
-        print "megaInit"
-        if not self.setup:
-            self.setup = True
-            self.MakeFloatCut("asdf", "MuPt", ">", 20)
-        print "done"
-        return True
+        return 1
 
     def MegaSlaveBegin(self, tree):
-        print "megaSlave"
-        self.TauPt = ROOT.TH1F("TauPt", "TauPt", 100, 0, 100)
-        self.MuPt = ROOT.TH1F("MuPt", "MuPt", 100, 0, 100)
-        self.BytesRead = ROOT.TH1F("BytesRead", "BytesRead", 1000, 0, 10000)
+        # Book histograms
+        self.MuPt = ROOT.TH1F("MuPt", "MuPt", 200, 0, 200)
         self.DisableBranch('*')
-        self.EnableBranch('TauPt')
-        self.EnableBranch('MuPt')
-        #self.SetFilterSelection("Base")
+        for branch in meta.active_branches():
+            self.EnableBranch(branch)
         return True
 
     def MegaProcess(self, entry):
-        print entry
-        read = self.chain.GetEntry(entry)
-        # Should be 8 bytes each time
-        self.BytesRead.Fill(read)
-        self.TauPt.Fill(self.chain.TauPt)
-        self.MuPt.Fill(self.chain.MuPt)
+        tree = self.chain
+        read = tree.GetEntry(entry)
+
+        # Check if we pass the base selection
+        for select in base_selections:
+            if not select(tree):
+                return True
+
+        self.MuPt.Fill(tree.muonPt)
         return True
 
     def MegaSlaveTerminate(self):
-        print 'Adding output'
-        self.AddToOutput(self.TauPt)
         self.AddToOutput(self.MuPt)
-        self.AddToOutput(self.BytesRead)
         return True
