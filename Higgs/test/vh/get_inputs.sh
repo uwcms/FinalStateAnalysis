ad2d2511bda571d51bad1440c98fb670bd794f71
Evan Friis <evan.friis@cern.ch>
2012-03-15 04:35:40 -0500

    Specify where the data should come from

--- Higgs/test/vh/get_inputs.sh
+++ Higgs/test/vh/get_inputs.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 # A stupid script for discovering the input ntuple files.
-# Usage: ./get_inputs.sh jobid
+# Usage: ./get_inputs.sh jobid $hdfs
 # Generates inputs/[jobid]/[sample1].txt
 #           inputs/[jobid]/[sample2].txt
 #           etc
 
-JOBID=$1
+JOBID=$1 
+SOURCE=$2
 
-echo "Finding input files for job: $JOBID"
+echo "Finding input files for job: $JOBID in $SOURCE"
 
 OUTPUTDIR=inputs/$JOBID
 
 mkdir -p $OUTPUTDIR
 
-for sampledir in `ls -d $hdfs/$JOBID/*`
+for sampledir in `ls -d $SOURCE/$JOBID/*`
 do
   sample=`basename $sampledir`
   echo -n "Getting data files for $sample - got "
