7de387d8246e324eadd32c040f14f6de041e4a10
Evan Friis <evan.friis@cern.ch>
2012-03-21 14:59:28 -0500

    Add tool for computing efficiencies

---
+++ StatTools/python/efficiencies.py
@@ -0,0 +1,23 @@
+'''
+
+Interface to use TEfficiency to compute efficiency intervals
+
+'''
+
+import ROOT
+
+
+def efficiency(passed, total, conf=0.682689492137):
+    ''' Use Clopper-Pearson to compute confidence interval
+
+    '''
+
+    eff = passed*1./total
+
+    total = ROOT.TMath.Nint(total)
+    passed = ROOT.TMath.Nint(passed)
+
+    up = ROOT.TEfficiency.ClopperPearson(total, passed, conf, True)
+    down = ROOT.TEfficiency.ClopperPearson(total, passed, conf, False)
+
+    return (eff, eff - down, up - eff)
