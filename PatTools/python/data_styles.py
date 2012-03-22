5c56f1a9d15e0d5e8defc4b491f8c1a1a66d7dfc
Evan Friis <evan.friis@cern.ch>
2012-03-22 09:47:55 -0500

    Add data styles file

---
+++ PatTools/python/data_styles.py
@@ -0,0 +1,14 @@
+
+
+data_styles = {
+    'Zjets' : {
+        'legendstyle' : 'lf',
+        'format' : 'hist',
+        'fillcolor' : 'red',
+    },
+    'Wjets' : {
+        'legendstyle' : 'lf',
+        'format' : 'hist',
+        'fillcolor' : 'blue',
+    },
+}
