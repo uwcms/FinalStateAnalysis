2db2fa49ab6d974067f6ec1ce3b74560cb891d27
Evan Friis <evan.friis@cern.ch>
2012-04-02 14:56:03 -0500

    Improve arugment passing ability in pickEvents script

--- MetaData/scripts/pickEvents.py
+++ MetaData/scripts/pickEvents.py
@@ -24,6 +24,7 @@ Author: Evan K. Friis, UW Madison
 
 '''
 
+from RecoLuminosity.LumiDB import argparse
 import sys
 import os
 import json
@@ -31,18 +32,17 @@ from subprocess import Popen, PIPE, STDOUT
 from FinalStateAnalysis.MetaData.datadefs import datadefs
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        sys.stderr.write("Usage: %s [json_file]\n")
-        sys.exit(1)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('json_file', help="JSON run-lumi-event file")
+    parser.add_argument('--output', default = "{dataset}",
+                        help="Name of output file.  Default: {dataset}")
 
-    filename = sys.argv[1]
+    args = parser.parse_args()
+
+    filename = args.json_file
     if not os.path.exists(filename):
         sys.stderr.write("Input file %s does not exist!\n" % filename)
         sys.exit(2)
-
-    label = ''
-    if len(sys.argv) > 2:
-        label = sys.argv[2]
 
     file = open(filename, 'r')
     events = json.load(file)
@@ -50,7 +50,7 @@ if __name__ == "__main__":
     for dataset, runevts in events.iteritems():
         real_dataset = datadefs[dataset]['datasetpath']
         command = ['fsaPickEvents.py']
-        command.append('--output=%s' % (dataset + label))
+        command.append('--output=%s' % args.output.format(dataset=dataset))
         command.append('--printInteractive')
         command.append(real_dataset)
         sys.stderr.write('Picking events for dataset: %s = %s '
