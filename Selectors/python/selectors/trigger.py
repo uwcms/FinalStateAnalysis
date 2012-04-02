a0135e3abb05a17b7fa41ba1b497359e8ac7e667
Evan Friis <evan.friis@cern.ch>
2012-04-02 05:12:09 -0500

    Remove old ntuple branch maker commands - replaced by templates

--- Selectors/python/selectors/trigger.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-hlt = cms.PSet(
-    name = cms.string("${name}_HLT"),
-    description = cms.string("$nicename HLT requirement [${hlt_path}]"),
-    cut = cms.string('evt.hltResult("${hlt_path}")'),
-    plottable = cms.string('evt.hltResult("${hlt_path}")'),
-    invert = cms.bool(False),
-)
