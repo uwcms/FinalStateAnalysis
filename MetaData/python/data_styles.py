a2611f1b16cfcbbe2e8e673f3a3e5fc3acf6f2b7
Evan Friis <evan.friis@cern.ch>
2012-04-02 08:08:05 -0500

    Use solarized for plot colors

--- MetaData/python/data_styles.py
+++ MetaData/python/data_styles.py
@@ -1,29 +1,51 @@
+'''
 
+Define styles for the different data samples.
+
+The keys correspond to "logical" samples, defined in data_name_map of
+datadefs.py
+
+The values are dictionaries, which can be passed to rootpy Plottable
+
+http://ndawe.github.com/rootpy/reference/rootpy.plotting.html#rootpy.plotting.core.Plottable
+
+'''
+
+from FinalStateAnalysis.Utilities.solarized import colors
 
 data_styles = {
     'Zjets' : {
         'legendstyle' : 'lf',
         'format' : 'hist',
-        'fillcolor' : 'cornflowerblue',
+        'fillcolor' :colors['yellow'],
     },
     'Wjets' : {
         'legendstyle' : 'lf',
         'format' : 'hist',
-        'fillcolor' : 'blue',
+        'fillcolor' : colors['green'],
     },
     'QCDMu' : {
         'legendstyle' : 'lf',
         'format' : 'hist',
-        'fillcolor' : 'crimson',
+        'fillcolor' : colors['magenta'],
     },
     'ttjets' : {
         'legendstyle' : 'lf',
         'format' : 'hist',
-        'fillcolor' : 'darkseagreen',
+        'fillcolor' : colors['orange'],
+    },
+    'WZ_pythia' : {
+        'legendstyle' : 'lf',
+        'format' : 'hist',
+        'fillcolor' : colors['blue'],
+    },
+    'ZZ' : {
+        'legendstyle' : 'lf',
+        'format' : 'hist',
+        'fillcolor' : colors['cyan'],
     },
     'data_DoubleMu' : {
         'legendstyle' : 'pe',
         'format' : 'pe',
-        'fillcolor' : 'blue',
     },
 }
