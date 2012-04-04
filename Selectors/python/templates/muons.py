f63fdb296ac18bc979b8ceb023eb891af8868d5b
Evan Friis <evan.friis@cern.ch>
2012-04-04 10:39:33 -0500

    More muon ID variables

--- Selectors/python/templates/muons.py
+++ Selectors/python/templates/muons.py
@@ -26,12 +26,16 @@ id = PSet(
         "-0.5*{object}.userIso(0),0.0))"
         "/{object}.pt()"
     ),
+    objectIsGlobal = '{object}.isGlobalMuon',
+    objectIsTracker = '{object}.isTrackerMuon',
 )
 
 # Information about the associated track
 tracking = PSet(
     objectPixHits = '? {object}.combinedMuon.isNonnull ? '
         '{object}.combinedMuon.hitPattern.numberOfValidPixelHits :-1',
+    objectGlbTrkHits = '? {object}.globalTrack.isNonnull ? '
+        '{object}.globalTrack.hitPattern.numberOfHits :-1',
     objectNormTrkChi2 = "? {object}.combinedMuon.isNonnull ? "
         "{object}.combinedMuon.chi2/{object}.combinedMuon.ndof : 1e99",
     objectD0 = '{object}.dB("PV3D")',
