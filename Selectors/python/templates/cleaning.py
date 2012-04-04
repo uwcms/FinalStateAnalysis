2e008b47b4c210980482f1acb41ce6ad7dc83c47
Evan Friis <evan.friis@cern.ch>
2012-04-04 11:48:38 -0500

    Add more vetoes to converge w/ abdollah

--- Selectors/python/templates/cleaning.py
+++ Selectors/python/templates/cleaning.py
@@ -16,8 +16,10 @@ from FinalStateAnalysis.Utilities.cfgtools import PSet
 # Vetos on extra stuff in the event
 vetos = PSet(
     muVetoPt5 = 'extras("extMuons", "pt > 5 & abs(eta) < 2.1").size()',
+    muGlbIsoVetoPt10 = 'extras("extMuons", "isGlobalMuon & isTrackerMuon & pt > 10 & abs(eta) < 2.1 & (chargedHadronIso + max(photonIso + neutralHadronIso - 0.5*userIso(0), 0))/pt < 0.25").size()',
     tauVetoPt20 = 'extras("extTaus", "pt > 20 & abs(eta) < 2.5 & tauID(\'decayModeFinding\') & tauID(\'byLooseCombinedIsolationDeltaBetaCorr\')").size()',
     eVetoWP95Iso = 'extras("extElecs", "pt > 10 & abs(eta) < 2.5 & userFloat(\'wp95\') > 0.5 & (chargedHadronIso + max(photonIso + neutralHadronIso - 0.5*userIso(0), 0))/pt < 0.3").size()',
+    eVetoCicTightIso = 'extras("extElecs", "pt > 10 & abs(eta) < 2.5 &  test_bit(electronID(\'cicTight\'), 0) > 0.5 & (chargedHadronIso + max(photonIso + neutralHadronIso - 0.5*userIso(0), 0))/pt < 0.25").size()',
     bjetVeto = 'extras("extJets", "pt > 20 & abs(eta) < 2.5 & bDiscriminator(\'\') > 3.3").size()',
 )
 
