96c2598a1e9ebde0e4442a5fe7d9c083beacb951
Evan Friis <evan.friis@cern.ch>
2012-04-05 02:19:59 -0500

    more info on our UW ZH events

--- Higgs/test/vh/debug_uw_events.sh
+++ Higgs/test/vh/debug_uw_events.sh
@@ -15,7 +15,8 @@ echo "Debugging MMET"
 
 $megadebug AnalyzeMMET.py uw_events_ntuple.txt /emmt/final/Ntuple \
   unique base_selections+   hadronic_tau_id e_id+ \
-  --events 163334,499,286336207
+  --events 163334,499,286336207  
+  #--events 166699,462,499978000 
 
 #echo "Debugging MMME"
 
@@ -23,11 +24,11 @@ $megadebug AnalyzeMMET.py uw_events_ntuple.txt /emmt/final/Ntuple \
   #unique+ base_selections+  e_id m3_id  \
   #--events 179889,195,291267852 177184,10,12646620
 
-#echo "Debugging EETT"
+echo "Debugging EETT"
 
-#$megadebug AnalyzeEETT.py uw_events_ntuple.txt /eett/final/Ntuple \
-  #unique- base_selections+  hadronic_t1_id hadronic_t2_id  \
-  #--events 178100,335,455090581
+$megadebug AnalyzeEETT.py uw_events_ntuple.txt /eett/final/Ntuple \
+  unique- base_selections+  hadronic_t1_id hadronic_t2_id  \
+  --events 178871,535,721402028
 
 #echo "Debugging EEET"
 
@@ -35,4 +36,9 @@ $megadebug AnalyzeMMET.py uw_events_ntuple.txt /emmt/final/Ntuple \
   #unique- base_selections+  e3_id  hadronic_tau_id \
   #--events 172252,40,47105541 177718,469,736335702
 
-## Don't need to do MMTT
+echo "Debugging MMTT"
+
+$megadebug AnalyzeMMTT.py uw_events_ntuple.txt /mmtt/final/Ntuple \
+  unique- base_selections+  t1_id t2_id \
+  --events 163334,499,286336207  
+  #--events 177790,119,157798505 
