9f545c944da5d0d423ce67f8ee969b7a466ad9f5
Evan Friis <evan.friis@cern.ch>
2012-04-09 05:56:58 -0500

    Don't touch the input files if they haven't changed

--- Higgs/test/vh/get_inputs.sh
+++ Higgs/test/vh/get_inputs.sh
@@ -19,7 +19,17 @@ for sampledir in `ls -d $SOURCE/$JOBID/*`
 do
   sample=`basename $sampledir`
   echo -n "Getting data files for $sample - got "
-  ls $sampledir/*/*root | sed "s|^|file:|" | root_file_check.py > $OUTPUTDIR/${sample}.txt
-  echo -n `cat $OUTPUTDIR/${sample}.txt | wc -l `
-  echo " files"
+  ls $sampledir/*/*root | sed "s|^|file:|" | root_file_check.py > $OUTPUTDIR/${sample}.tmp.txt
+  echo -n `cat $OUTPUTDIR/${sample}.tmp.txt | wc -l `
+  echo -n " files, "
+  # Only update the file if it's changed
+  cmp -s $OUTPUTDIR/${sample}.tmp.txt $OUTPUTDIR/${sample}.txt 
+  if [ $? -eq 1 ]; then
+    echo "new files found."
+    mv $OUTPUTDIR/${sample}.tmp.txt $OUTPUTDIR/${sample}.txt 
+  else
+    echo "no new files found."
+    rm $OUTPUTDIR/${sample}.tmp.txt 
+  fi
+
 done
