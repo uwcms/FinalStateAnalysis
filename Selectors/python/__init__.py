7007d26ab01741fbe30a0e70a7c7797e3a02df47
Evan Friis <evan.friis@cern.ch>
2012-04-17 12:31:14 -0500

    Explicitly put python package helpers in version control

---
+++ Selectors/python/__init__.py
@@ -0,0 +1,3 @@
+#Automatically created by SCRAM
+import os
+__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/FinalStateAnalysis/Selectors/',1)[0])+'/cfipython/slc5_amd64_gcc434/FinalStateAnalysis/Selectors')
---
+++ Selectors/python/templates/__init__.py
@@ -0,0 +1,16 @@
+'''
+
+Clearing house for ntuple branch templates
+
+Author: Evan K. Friis, UW Madison
+
+'''
+
+import candidates
+import electrons
+import muons
+import taus
+import event
+import topology
+import cleaning
+import trigger
