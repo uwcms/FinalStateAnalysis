1151ca3f8e0cca8f4af666d04e1211c293765bba
Evan Friis <evan.friis@cern.ch>
2012-04-29 08:54:04 -0500

    If PU distribution is unknown, use Fall 11 (S6)

--- DataAlgos/data/pileup_distributions.py
+++ DataAlgos/data/pileup_distributions.py
@@ -15,6 +15,8 @@ import FWCore.ParameterSet.Config as cms
 pileup_distributions = cms.PSet(
     # MC distributions
     S6 = cms.FileInPath("FinalStateAnalysis/DataAlgos/data/pu/fall11_mc_truth.root"),
+    # HACK
+    unknown = cms.FileInPath("FinalStateAnalysis/DataAlgos/data/pu/fall11_mc_truth.root"),
     # Data distributions
     data2011A = cms.FileInPath("FinalStateAnalysis/DataAlgos/data/pu/allData_2011A_pileupTruth_v2.root"),
     data2011B = cms.FileInPath("FinalStateAnalysis/DataAlgos/data/pu/allData_2011B_pileupTruth_v2.root"),
