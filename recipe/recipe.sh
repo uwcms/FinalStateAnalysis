c42eda026d6524cd992bf1f8969ea1fbf0f6db9c
Evan Friis <evan.friis@cern.ch>
2012-05-03 11:52:28 -0500

    Update to new tau pog recipe

--- recipe/recipe.sh
+++ recipe/recipe.sh
@@ -6,6 +6,15 @@ cd $CMSSW_BASE/src
 echo "Checking out extra packages"
 addpkg -z -f FinalStateAnalysis/recipe/tags
 
+echo "Checking out Tau POG recipe"
+addpkg DataFormats/TauReco CMSSW_5_2_4 # yes, this is correct
+addpkg RecoTauTag/TauTagTools CMSSW_5_2_4
+cvs co -r V01-04-14 RecoTauTag/RecoTau
+cvs co -r V01-04-01 RecoTauTag/Configuration
+cvs co -r V00-04-01 CondFormats/EgammaObjects
+addpkg -z PhysicsTools/PatAlgos 
+cvs up -r 1.53 PhysicsTools/PatAlgos/python/tools/tauTools.py
+
 # Add and patch to way speed up trigger matching
 echo "Applying pat trigger matching speedup"
 addpkg -z DataFormats/PatCandidates 
@@ -14,11 +23,6 @@ patch -N -p0 < FinalStateAnalysis/recipe/patches/V06-04-16_DataFormats_PatCandid
 echo "Adding 2D expression histogram feature"
 addpkg -z CommonTools/Utils 
 patch -N -p0 < FinalStateAnalysis/recipe/patches/V00-04-02_CommonTools_Utils_Add2DHistoFeature.patch
-
-echo "Checking out pat support for new tau discriminators"
-addpkg -z PhysicsTools/PatAlgos 
-# Add Mike's muon discriminant
-cvs co -r 1.47 PhysicsTools/PatAlgos/python/tools/tauTools.py
 
 # Add support for PU Jet ID
 # See https://twiki.cern.ch/twiki/bin/view/CMS/PileupJetID
