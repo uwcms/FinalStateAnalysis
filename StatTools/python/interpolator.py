c76fa6ed535bead4e8bab914f1b517e2b7d6e384
Evan Friis <evan.friis@cern.ch>
2012-05-14 10:45:10 -0500

    Fix logger syntax errors

--- StatTools/python/interpolator.py
+++ StatTools/python/interpolator.py
@@ -17,7 +17,7 @@ log = logging.getLogger('interpolate')
 def interpolate_card(output_stream,
                      lowcardfile, lowmass,
                      highcardfile, highmass,
-                     target, *processes):
+                     target, processes):
     ''' Interpolate between two data cards
 
     The output card (with mass=target) is written to output_stream.
@@ -26,9 +26,9 @@ def interpolate_card(output_stream,
     the format string {mass}
     '''
 
-    log.info("Parsing", lowcardfile, "m:", lowmass)
+    log.info("Parsing %s m:%i", lowcardfile, lowmass)
     lowcard = read_card(lowcardfile)
-    log.info("Parsing", highcardfile, "m:", highmass)
+    log.info("Parsing %s m:%i", highcardfile, highmass)
     highcard = read_card(highcardfile)
 
     assert(lowcard.bins == highcard.bins)
@@ -36,7 +36,8 @@ def interpolate_card(output_stream,
     newcard = copy.deepcopy(lowcard)
 
     for process in processes:
-        log.info("Interpolating %s process" % process)
+        targetlabel = process.format(mass=target)
+        log.info("Interpolating %s process" % targetlabel)
         lowlabel = process.format(mass=lowmass)
         highlabel = process.format(mass=highmass)
 
@@ -49,8 +50,8 @@ def interpolate_card(output_stream,
                 highmass, high_yield,
                 target
             )
-            log.info("in bin", bin, "low yield is", low_yield,
-                    "high yield is", high_yield, "=> target is ", target_yield)
+            #log.info("in bin", bin, "low yield is", low_yield,
+                    #"high yield is", high_yield, "=> target is ", target_yield)
             # We use the same label as lowlabel, and replace it later
             newcard.exp[bin][lowlabel] = target_yield
 
