b518705b8c2178ad913b6d37213365ca76d13ce6
Evan Friis <evan.friis@cern.ch>
2012-05-17 05:03:01 -0500

    Add recipe for building Higgs CS and Width tool in MetaData

---
+++ recipe/install_HCSaW.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Installs the HCSaW package into MetaData
+
+pushd $CMSSW_BASE/src
+cvs co -P -d HCSaW UserCode/Snowball/Higgs
+
+mkdir -p FinalStateAnalysis/MetaData/interface/
+mkdir -p FinalStateAnalysis/MetaData/src/
+mkdir -p FinalStateAnalysis/MetaData/data/HCSaW
+
+cp -v HCSaW/*/include/*.h FinalStateAnalysis/MetaData/interface/
+cp -v HCSaW/*/src/*.cc FinalStateAnalysis/MetaData/src/
+cp -v HCSaW/*/txtFiles/*txt FinalStateAnalysis/MetaData/data/HCSaW
+
+pushd FinalStateAnalysis/MetaData/
+patch -p0 < ../recipe/patches/HCSaW_sanity.patch
+popd
+popd
