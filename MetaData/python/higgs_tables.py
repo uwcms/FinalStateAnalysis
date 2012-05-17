0d986a22408542f8c8a5a218d04f80ebc72a10e2
Evan Friis <evan.friis@cern.ch>
2012-05-17 07:06:29 -0500

    Add access to xsection errors

--- MetaData/python/higgs_tables.py
+++ MetaData/python/higgs_tables.py
@@ -14,6 +14,26 @@ Getting a cross section
 >>> abs(wh_cs - 0.6561) < 1e-6
 True
 
+Getting all info about a cross section
+--------------------------------------
+
+The following data are returned in a dictionary::
+
+* xsec
+* +             (the total error up)
+* -             (the total error down)
+* scale+
+* scale-
+* pdf+
+* pdf-
+
+The errors are returned as relative deviations from the mean.
+
+>>> wh_cs_info = cross_section_info('WH', 120.0, 7)
+>>> # Check the value from the Yellow Report (it's reported in %)
+>>> "%0.1f" % (wh_cs_info['scale-']*100)
+'-0.7'
+
 Getting a BR
 ------------
 
@@ -57,11 +77,21 @@ _br_map = {
 }
 
 
-def cross_section(process, mass, sqrts, spline=True):
+def cross_section_info(process, mass, sqrts, spline=True):
     if isinstance(process, basestring):
         process = _xsec_map[process]
-    return _sm.HiggsCS(process, mass, sqrts, spline)
+    output = {}
+    output['xsec'] = _sm.HiggsCS(process, mass, sqrts, spline)
+    output['+'] = _sm.HiggsCSErrPlus(process, mass, sqrts)
+    output['-'] = _sm.HiggsCSErrMinus(process, mass, sqrts)
+    output['scale+'] = _sm.HiggsCSscaleErrPlus(process, mass, sqrts)
+    output['scale-'] = _sm.HiggsCSscaleErrMinus(process, mass, sqrts)
+    output['pdf+'] = _sm.HiggsCSpdfErrPlus(process, mass, sqrts)
+    output['pdf-'] = _sm.HiggsCSpdfErrMinus(process, mass, sqrts)
+    return output
 
+def cross_section(process, mass, sqrts, spline=True):
+    return cross_section_info(process, mass, sqrts, spline)['xsec']
 
 def branching_ratio(process, mass, spline=True):
     if isinstance(process, basestring):
