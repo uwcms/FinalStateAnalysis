cfd06101743c23d9cb308cf5dd52f1d3fae0c753
Evan Friis <evan.friis@cern.ch>
2012-05-18 06:01:12 -0500

    Improve crash message when wrong dataset used

--- PatTools/python/electrons/patElectronEmbedCalibratedGsf_cfi.py
+++ PatTools/python/electrons/patElectronEmbedCalibratedGsf_cfi.py
@@ -28,8 +28,12 @@ def validate_egamma_calib_config(process):
     data_datasets = set(['Prompt', 'ReReco', 'Jan16ReReco'])
     mc_datasets = set(['Summer11', 'Fall11'])
 
-    if isMC:
-        assert(dataset in mc_datasets)
-    else:
-        assert(dataset in data_datasets)
+    if isMC and dataset not in mc_datasets:
+        raise ValueError(
+            "Dataset %s is not in the list of valid MC datasets: %s" %
+            (dataset, repr(mc_datasets)))
+    elif dataset not in data_datasets:
+        raise ValueError(
+            "Dataset %s is not in the list of valid data datasets: %s" %
+            (dataset, repr(data_datasets)))
 
