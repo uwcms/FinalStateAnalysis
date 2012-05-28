dc59a94e512bb0fb74a904b1cc996e0e9296f31d
Evan Friis <evan.friis@cern.ch>
2012-05-28 04:50:11 -0500

    Add 8TeV data, up to run 194479

--- MetaData/python/data8TeV.py
+++ MetaData/python/data8TeV.py
@@ -9,7 +9,7 @@ Author: Evan K. Friis, UW Madison
 from datacommon import square, cube, quad, picobarns, br_w_leptons
 
 # Figure this out later.
-data_name_map = None
+data_name_map = {}
 
 datadefs = {
     'WplusJets_madgraph' : {
@@ -85,3 +85,53 @@ for mass in [115, 120, 125, 130, 140, 145, 150, 155, 160]:
         'xsec': -999,
         'responsible' : 'Evan',
     }
+
+# Add data files
+def build_data_set(pd, analyses, who):
+    subsample_dict = {
+        'data_%s_Run2012A_PromptReco_v1' % pd : {
+            'datasetpath' : "/%s/Run2012A-PromptReco-v1/RECO",
+            'lumi_mask' : "FinalStateAnalysis/RecoTools/data/masks/Cert_190456-194479_8TeV_PromptReco_Collisions12_JSON.txt",
+            'firstRun' : 190450,
+            'lastRun' : 193686,
+            'analyses' : analyses,
+            'responsible' : who,
+        },
+        'data_%s_Run2012B_PromptReco_v1_a' % pd : {
+            'datasetpath' : "/%s/Run2012B-PromptReco-v1/RECO",
+            'lumi_mask' : "FinalStateAnalysis/RecoTools/data/masks/Cert_190456-194479_8TeV_PromptReco_Collisions12_JSON.txt",
+            'firstRun' : 193752,
+            'lastRun' : 194479,
+            'analyses' : analyses,
+            'responsible' : who,
+        },
+    }
+    sample_dict = {
+        'data_%s' % pd : subsample_dict.keys()
+    }
+    return subsample_dict, sample_dict
+
+# Build all the PDs we use
+data_DoubleMu, list_DoubleMu = build_data_set('DoubleMu', ['VH', 'Mu'], 'Ian')
+datadefs.update(data_DoubleMu)
+data_name_map.update(list_DoubleMu)
+
+data_MuEG, list_MuEG = build_data_set('MuEG', ['VH', 'HTT', 'Mu'], 'Evan')
+datadefs.update(data_MuEG)
+data_name_map.update(list_MuEG)
+
+data_DoubleE, list_DoubleE = build_data_set('DoubleElectron', ['VH',], 'Ian')
+datadefs.update(data_DoubleE)
+data_name_map.update(list_DoubleE)
+
+data_SingleMu, list_SingleMu = build_data_set('SingleMu', ['Tau', 'Mu'], 'Maria')
+datadefs.update(data_SingleMu)
+data_name_map.update(list_SingleMu)
+
+data_SingleElectron, list_SingleElectron = build_data_set('SingleElectron', ['Tau', 'E', 'Wjets'], 'Maria')
+datadefs.update(data_SingleElectron)
+data_name_map.update(list_SingleElectron)
+
+data_TauPlusX, list_TauPlusX = build_data_set('TauPlusX', ['HTT', ], 'Josh')
+datadefs.update(data_TauPlusX)
+data_name_map.update(list_TauPlusX)
