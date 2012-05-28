27332d0f378c51b81c8d0d06e7473e0b9b7c48b1
Evan Friis <evan.friis@cern.ch>
2012-05-28 04:45:39 -0500

    Add 8TeV lumi masks

--- RecoTools/data/masks/update.sh
+++ RecoTools/data/masks/update.sh
@@ -10,3 +10,8 @@ base=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/
 rsync -v -a --exclude="*CMSSW*" --exclude="*/" --exclude="*/*" --include="Cert*.txt" --exclude="*" $base/Collisions11/7TeV/Prompt/ .
 # Get 2011 reprocessing jsons
 rsync -v -a --exclude="*CMSSW*" --exclude="*/" --exclude="*/*" --include="Cert*.txt" --exclude="*" $base/Collisions11/7TeV/Reprocessing/ .
+
+# Get 2012 prompt jsons
+rsync -v -a --exclude="*CMSSW*" --exclude="*/" --exclude="*/*" --include="Cert*.txt" --exclude="*" $base/Collisions12/8TeV/Prompt/ .
+# Get 2012 reprocessing jsons
+rsync -v -a --exclude="*CMSSW*" --exclude="*/" --exclude="*/*" --include="Cert*.txt" --exclude="*" $base/Collisions12/8TeV/Reprocessing/ .
