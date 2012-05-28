28cfb311028aed1ec8a9eb8b0186c4331cf59116
Evan Friis <evan.friis@cern.ch>
2012-05-28 09:48:45 -0500

    Add function to get the current commit hash

--- Utilities/python/version.py
+++ Utilities/python/version.py
@@ -5,9 +5,17 @@ Stupid library to get CMSSW version
 '''
 
 import os
+import subprocess
 
 def cmssw_version():
     return os.getenv('CMSSW_VERSION')
 
 def cmssw_major_version():
     return int(os.getenv('CMSSW_VERSION').split('_')[1])
+
+def fsa_version():
+    ''' Get commit hash of FSA '''
+    result = subprocess.Popen(
+        ['git', 'log', '-1', '--format=%h'],
+        stdout=subprocess.PIPE).communicate()[0]
+    return result.strip()
