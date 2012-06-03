3ec691adfd29ec5ba9ca255b447ec44a1eefb252
Evan Friis <evan.friis@cern.ch>
2012-06-03 05:27:25 -0500

    Disable electron veto as it's currently broken :(

--- Selectors/python/templates/cleaning.py
+++ Selectors/python/templates/cleaning.py
@@ -18,8 +18,9 @@ vetos = PSet(
     muVetoPt5 = 'vetoMuons(0.3, "pt > 5 & abs(eta) < 2.4").size()',
     muGlbIsoVetoPt10 = 'vetoMuons(0.3, "isGlobalMuon & isTrackerMuon & pt > 10 & abs(eta) < 2.4 & (userIso(0) + max(photonIso + neutralHadronIso - 0.5*pfPUChargedHadrons, 0))/pt < 0.3").size()',
     tauVetoPt20 = 'vetoTaus(0.3, "pt > 20 & abs(eta) < 2.5 & tauID(\'decayModeFinding\') & tauID(\'byLooseIsolationMVA\')").size()',
-    eVetoWP95Iso = 'vetoElectrons(0.3, "pt > 10 & abs(eta) < 2.5 & userFloat(\'wp95\') > 0.5 & (userIso(0) + max(userIso(1) + neutralHadronIso - 0.5*userIso(2), 0))/pt < 0.3").size()',
-    eVetoCicTightIso = 'vetoElectrons(0.3, "pt > 10 & abs(eta) < 2.5 &  test_bit(electronID(\'cicTight\'), 0) > 0.5 & (userIso(0) + max(userIso(1) + neutralHadronIso - 0.5*userIso(2), 0))/pt < 0.3").size()',
+    # Electrons embedding currently broken
+    #eVetoWP95Iso = 'vetoElectrons(0.3, "pt > 10 & abs(eta) < 2.5 & userFloat(\'wp95\') > 0.5 & (userIso(0) + max(userIso(1) + neutralHadronIso - 0.5*userIso(2), 0))/pt < 0.3").size()',
+    #eVetoCicTightIso = 'vetoElectrons(0.3, "pt > 10 & abs(eta) < 2.5 &  test_bit(electronID(\'cicTight\'), 0) > 0.5 & (userIso(0) + max(userIso(1) + neutralHadronIso - 0.5*userIso(2), 0))/pt < 0.3").size()',
     bjetVeto = 'vetoJets(0.4, "pt > 20 & abs(eta) < 2.5 & bDiscriminator(\'\') > 3.3").size()',
     bjetCSVVeto = 'vetoJets(0.4, "pt > 20 & abs(eta) < 2.5 & bDiscriminator(\'combinedSecondaryVertexBJetTagsAOD\') > 0.679").size()',
     jetVeto20 = 'vetoJets(0.4, "pt > 20 & abs(eta) < 5.0 & userInt(\'fullIdLoose\')").size()',
@@ -28,6 +29,7 @@ vetos = PSet(
 
 overlaps = PSet(
     objectMuOverlap = 'overlapMuons({object_idx}, "pt > 5").size()',
-    objectElecOverlap = 'overlapElectrons({object_idx}, "pt > 10").size()',
-    objectCiCTightElecOverlap = 'overlapElectrons({object_idx}, "pt > 10 & test_bit(electronID(\'cicTight\'), 0)").size()',
+    # Electrons embedding currently broken
+    #objectElecOverlap = 'overlapElectrons({object_idx}, "pt > 10").size()',
+    #objectCiCTightElecOverlap = 'overlapElectrons({object_idx}, "pt > 10 & test_bit(electronID(\'cicTight\'), 0)").size()',
 )
