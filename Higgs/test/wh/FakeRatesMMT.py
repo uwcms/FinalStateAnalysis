576d053f376ec9eba5324e55a970381d1eb7e8fc
Evan Friis <evan.friis@cern.ch>
2012-06-07 14:57:33 -0500

    Make a more reasonable example

--- Higgs/test/wh/FakeRatesMMT.py
+++ Higgs/test/wh/FakeRatesMMT.py
@@ -17,12 +17,27 @@ class FakeRatesMMT(MegaBase):
         self.out = outfile
 
     def begin(self):
-        self.out.cd()
-        self.tPt = self.book('plots', 'tPt', 'Tau Pt', 100, 0, 100)
+        # Book histograms
+        # Denominator tau PT histogram
+        self.tPt = self.book('plots', 'tPtAll', 'Tau Pt', 100, 0, 100)
+        # Numerator (passing MVA) histogram
+        self.tPtLooseMVA = self.book('plots', 'tPtLooseMVA', 'Tau Pt', 100, 0, 100)
+        # Numerator (passing regular iso) histogram
+        self.tPtLooseIso = self.book('plots', 'tPtLooseIso', 'Tau Pt', 100, 0, 100)
 
     def process(self):
-        for row in self.tree:
+        # Analyze data.  Select events with a good Z.
+        for row in self.tree.where(
+            'm1RelPFIsoDB < 0.25 && '
+            'm2RelPFIsoDB < 0.25 && '
+            'abs(m1_m2_Mass-91.2) < 10'):
+            # Fill denominator
             self.tPt.Fill(row.tPt)
+            # Check if passes tau ID, if so, fill numerator
+            if row.tLooseMVAIso:
+                self.tPtLooseMVA.Fill(row.tPt)
+            if row.tLooseIso:
+                self.tPtLooseIso.Fill(row.tPt)
 
     def finish(self):
         self.write_histos()
