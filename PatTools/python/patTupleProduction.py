c636d648e31f61377034142e3fee3cb49f0b152d
Evan Friis <evan.friis@cern.ch>
2012-06-11 12:56:54 -0500

    Update keep statements for embedded samples

--- PatTools/python/patTupleProduction.py
+++ PatTools/python/patTupleProduction.py
@@ -43,7 +43,6 @@ def configurePatTuple(process, isMC=True, **kwargs):
         # Debug
         #'*_generalTracks_*_*',
         #'*_offlinePrimaryVertices_*_*',
-
         '*_electronGsfTracks_*_*',
         '*_offlinePrimaryVertices*_*_*',
         '*_ak5GenJets_*_*',
@@ -57,7 +56,8 @@ def configurePatTuple(process, isMC=True, **kwargs):
         '*_kt6PFJets_rho_*',
         '*_kt6PFJetsCentralNeutral_rho_*', #for zz muons
         # for Zmumu -> embedded samples
-        '*_generator_weight_*',
+        '*_generator_weight_*', # 2k11
+        "GenFilterInfo_generator_minVisPtFilter_*", #2k12
         '*_genDaughters_*_*',
     ]
     # Define our patTuple production sequence
