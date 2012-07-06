83a9451824762f0ea928628ef76291d373d39c1a
Evan Friis <evan.friis@cern.ch>
2012-07-06 10:09:48 -0500

    More features, shape output for WH plots

--- Higgs/test/wh/WHPlotterBase.py
+++ Higgs/test/wh/WHPlotterBase.py
@@ -2,27 +2,53 @@
 
 Base class to do WH plotting.
 
+Author: Evan K. Friis, UW
+
+Takes as input a set of ROOT [files] with analysis histgrams, and the corresponding
+lumicalc.sum [lumifiles] that hve the effective lumi for each sample.
+
+If [blind] is true, data in the p1p2p3 region will not be plotted.
+
 '''
 
 import os
+import rootpy.plotting.views as views
 import rootpy.plotting as plotting
 from FinalStateAnalysis.MetaData.data_views import data_views
+from FinalStateAnalysis.MetaData.data_styles import data_styles
+from FinalStateAnalysis.PlotTools.BlindView import BlindView
+
+
+def rebin_view(x, rebin):
+    ''' Make a view which rebins histograms '''
+    rebinner = lambda x: x.Rebin(rebin)
+    output = views.FunctorView(x, rebinner)
+    return output
 
 class WHPlotterBase(object):
-    def __init__(self, files, lumifiles, outputdir):
+    def __init__(self, files, lumifiles, outputdir, blind=True):
         self.outputdir = outputdir
         self.views = data_views(files, lumifiles)
         self.canvas = plotting.Canvas(name='adsf', title='asdf')
         self.canvas.cd()
-        self.all_mc_stack = plotting.views.StackView(
-            self.views['Zjets_M50']['view'],
-            self.views['WplusJets_madgraph']['view'],
-            self.views['TTplusJets_madgraph']['view'],
-            self.views['WZJetsTo3LNu']['view'],
-            #self.views['ZZJetsTo4L_pythia']['view'],
-        )
+        if blind:
+            # Don't look at the SS all pass region
+            self.views['data']['view'] = BlindView(
+                self.views['data']['view'], "ss/p1p2p3/.*")
         self.data = self.views['data']['view']
         self.keep = []
+
+    def make_stack(self, rebin=1):
+        ''' Make a stack of the MC histograms '''
+        all_mc_stack = views.StackView(
+            rebin_view(self.views['Zjets_M50']['view'], rebin),
+            rebin_view(self.views['WplusJets_madgraph']['view'], rebin),
+            rebin_view(self.views['TTplusJets_madgraph']['view'], rebin),
+            rebin_view(self.views['WZJetsTo3LNu']['view'], rebin),
+            rebin_view(self.views['ZZJetsTo4L_pythia']['view'], rebin),
+        )
+        return all_mc_stack
+
 
     def save(self, filename):
         self.canvas.SaveAs(os.path.join(self.outputdir, filename) + '.png')
@@ -31,25 +57,89 @@ class WHPlotterBase(object):
 
     # Plot a single sample
     def plot(self, sample, path, drawopt='', styler=None):
+
         view = self.views[sample]['view']
         histo = view.Get(path)
         if styler:
             styler(histo)
-
         histo.Draw(drawopt)
         self.keep.append(histo)
+        return True
 
-    def plot_mc_vs_data(self, folder, variable):
+    def plot_mc_vs_data(self, folder, variable, rebin=1):
         path = os.path.join(folder, variable)
-        mc_stack = self.all_mc_stack.Get(path)
-        data = self.data.Get(path)
-        print data.Integral()
+        mc_stack = self.make_stack(rebin).Get(path)
+        data = rebin_view(self.data, rebin).Get(path)
         mc_stack.Draw()
-        data.Draw('same')
         self.keep.append(mc_stack)
+        data.Draw('same')
         self.keep.append(data)
 
-    def make_plots(self):
-        ''' Generic function to draw all plots '''
-        pass
-        #self.plot_mc_vs_data('
+    def make_signal_views(self, rebin):
+        ''' Make signal views with FR background estimation '''
+
+        #vh_view = views.SubdirectoryView(
+            #rebin_view(self.views['VH120']['view'], rebin),
+            #'ss/p1p2p3/'
+        #)
+        wz_view = views.SubdirectoryView(
+            rebin_view(self.views['WZJetsTo3LNu']['view'], rebin),
+            'ss/p1p2p3/'
+        )
+        zz_view = views.SubdirectoryView(
+            rebin_view(self.views['ZZJetsTo4L_pythia']['view'], rebin),
+            'ss/p1p2p3/'
+        )
+        all_data_view = rebin_view(self.views['data']['view'], rebin)
+        data_view = views.SubdirectoryView(all_data_view, 'ss/p1p2p3/')
+
+        # View of weighted obj1-fails data
+        obj1_view = views.SubdirectoryView(all_data_view, 'ss/f1p2p3/w1')
+        # View of weighted obj2-fails data
+        obj2_view = views.SubdirectoryView(all_data_view, 'ss/p1f2p3/w2')
+        # View of weighted obj1&2-fails data
+        obj12_view = views.SubdirectoryView(all_data_view, 'ss/f1f2p3/w12')
+        subtract_obj12_view = views.ScaleView(obj12_view, -1)
+        # Corrected fake view
+        fakes_view = views.SumView(obj1_view, obj2_view, subtract_obj12_view)
+        fakes_view = views.StyleView(fakes_view, **data_styles['Zjets*'])
+
+        output = {
+            #'vh120' : vh120_view,
+            'wz' : wz_view,
+            'zz' : zz_view,
+            'data' : data_view,
+            'obj1' : obj1_view,
+            'obj2' : obj2_view,
+            'fakes' : fakes_view
+        }
+
+        return output
+
+    def write_shapes(self, variable, rebin, outdir):
+        ''' Write final shape histos for [variable] into a TDirectory [outputdir] '''
+        sig_view = self.make_signal_views(rebin)
+        outdir.cd()
+        wz = sig_view['wz'].Get(variable)
+        zz = sig_view['zz'].Get(variable)
+        obs = sig_view['data'].Get(variable)
+        fakes = sig_view['fakes'].Get(variable)
+        wz.SetName('wz')
+        zz.SetName('zz')
+        obs.SetName('data_obs')
+        fakes.SetName('fakes')
+        wz.Write()
+        zz.Write()
+        obs.Write()
+        fakes.Write()
+
+    def plot_final(self, variable, rebin=1):
+        sig_view = self.make_signal_views(rebin)
+        stack = views.StackView(
+            sig_view['wz'],
+            sig_view['zz'],
+            sig_view['fakes'],
+        )
+        histo = stack.Get(variable)
+        histo.Draw()
+        self.keep.append(histo)
--- Higgs/test/wh/WHPlotterMMT.py
+++ Higgs/test/wh/WHPlotterMMT.py
@@ -7,19 +7,22 @@ Plot the MMT channel
 import glob
 import logging
 import WHPlotterBase
+import ROOT
 import sys
 
-logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 
 class WHPlotterMMT(WHPlotterBase.WHPlotterBase):
     def __init__(self, files, lumifiles, outputdir):
         super(WHPlotterMMT, self).__init__(files, lumifiles, outputdir)
 
 if __name__ == "__main__":
-    jobid = '2012-07-04-7TeV-Higgs'
+    jobid = '2012-07-05-7TeV-Higgs'
     samples = [
-        'Zjets_M50', 'WplusJets_madgraph',
+        'Zjets_M50',
+        'WplusJets_madgraph',
         'WZJetsTo3LNu',
+        'ZZ*',
         'TTplusJets_madgraph',
         "data_DoubleMu*",
     ]
@@ -32,20 +35,37 @@ if __name__ == "__main__":
         lumifiles.extend(glob.glob('inputs/%s/%s.lumicalc.sum' % (jobid, x)))
 
     plotter = WHPlotterMMT(files, lumifiles, 'results/plots/mmt')
+
+    ###########################################################################
+    ##  Zmm control plots #####################################################
+    ###########################################################################
+
+    # Control Z->mumu + jet region
     plotter.plot_mc_vs_data('os/p1p2f3', 'm1m2Mass')
     plotter.save('mcdata-os-p1p2f3-m1m2Mass')
+
     plotter.plot_mc_vs_data('os/p1p2f3/w3', 'm1m2Mass')
     plotter.save('mcdata-os-p1p2f3-w3-m1m2Mass')
 
     plotter.plot_mc_vs_data('os/p1f2p3', 'm1m2Mass')
     plotter.save('mcdata-os-p1f2p3-m1m2Mass')
 
+    # Check PU variables
     plotter.plot_mc_vs_data('os/p1p2f3', 'rho')
     plotter.save('mcdata-os-p1p2f3-rho')
 
     plotter.plot_mc_vs_data('os/p1p2f3', 'nvtx')
     plotter.save('mcdata-os-p1p2f3-nvtx')
 
+    # Lower stat but closer to signal region
+    plotter.plot_mc_vs_data('os/p1p2p3', 'rho')
+    plotter.save('mcdata-os-p1p2p3-rho')
+
+    plotter.plot_mc_vs_data('os/p1p2p3', 'nvtx')
+    plotter.save('mcdata-os-p1p2p3-nvtx')
+
+
+    # Make Z->mumu + tau jet control
     def make_styler(color, format=None):
         def unsuck(x):
             x.SetFillStyle(0)
@@ -57,5 +77,52 @@ if __name__ == "__main__":
     plotter.plot('data', 'os/p1p2f3/w3/m1m2Mass',  'hist', styler=make_styler(2, 'hist'))
     plotter.plot('data', 'os/p1p2p3/m1m2Mass', 'same', styler=make_styler(1))
     plotter.save('zmm-os-fr-control')
+
+    plotter.plot('data', 'os/p1p2p3/prescale', styler=make_styler(1))
+    plotter.save('zmm-os-prescale-check')
+
     plotter.plot('Zjets_M50', 'os/p1p2f3/weight')
     plotter.save('zmm-mc-event-weights')
+    # Check MC weights
+    plotter.plot('Zjets_M50', 'os/p1p2f3/weight_nopu')
+    plotter.save('zmm-mc-event-weight_nopu')
+
+
+    ###########################################################################
+    ##  FR sideband MC-vs-Data ################################################
+    ###########################################################################
+
+    plotter.plot_mc_vs_data('ss/p1f2p3', 'm1Pt', 5)
+    plotter.save('mcdata-ss-p1f2p3-m1Pt')
+
+    plotter.plot_mc_vs_data('ss/p1f2p3/w2', 'm1Pt', 5)
+    plotter.save('mcdata-ss-p1f2p3-w2-m1Pt')
+
+    plotter.plot_mc_vs_data('ss/p1f2p3', 'm1Pt', 5)
+    plotter.save('mcdata-ss-p1f2p3-m1Pt')
+
+    plotter.plot_mc_vs_data('ss/p1f2p3/w2', 'm1Pt', 5)
+    plotter.save('mcdata-ss-p1f2p3-w2-m1Pt')
+
+
+    ###########################################################################
+    ##  Signal region plots    ################################################
+    ###########################################################################
+
+    plotter.plot_final('m1Pt', 10)
+    plotter.save('final-m1Pt')
+
+    plotter.plot_final('m2Pt', 10)
+    plotter.save('final-m2Pt')
+
+
+    ###########################################################################
+    ##  Making shape file     #################################################
+    ###########################################################################
+
+    shape_file = ROOT.TFile('mmt_shapes.root', 'RECREATE')
+    shape_dir = shape_file.mkdir('mmt')
+    plotter.write_shapes('m1Pt', 10, shape_dir)
+    shape_file.Close()
+
+
