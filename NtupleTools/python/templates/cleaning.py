97b0f6327fa494152e043dae34329797d4de8107
Evan Friis <evan.friis@cern.ch>
2012-07-07 04:27:51 -0500

    Synch e-veto ID with H2tau

--- NtupleTools/python/templates/cleaning.py
+++ NtupleTools/python/templates/cleaning.py
@@ -19,8 +19,8 @@ vetos = PSet(
     muGlbIsoVetoPt10 = 'vetoMuons(0.3, "isGlobalMuon & isTrackerMuon & pt > 10 & abs(eta) < 2.4 & (userIso(0) + max(photonIso + neutralHadronIso - 0.5*puChargedHadronIso, 0))/pt < 0.3").size()',
     tauVetoPt20 = 'vetoTaus(0.3, "pt > 20 & abs(eta) < 2.5 & tauID(\'decayModeFinding\') & tauID(\'byLooseIsolationMVA\')").size()',
     # Electrons embedding currently broken
-    #eVetoWP95Iso = 'vetoElectrons(0.3, "pt > 10 & abs(eta) < 2.5 & userFloat(\'wp95\') > 0.5 & (userIso(0) + max(userIso(1) + neutralHadronIso - 0.5*userIso(2), 0))/pt < 0.3").size()',
-    #eVetoCicTightIso = 'vetoElectrons(0.3, "pt > 10 & abs(eta) < 2.5 &  test_bit(electronID(\'cicTight\'), 0) > 0.5 & (userIso(0) + max(userIso(1) + neutralHadronIso - 0.5*userIso(2), 0))/pt < 0.3").size()',
+    eVetoMVAIso = 'vetoElectrons(0.3, "pt > 10 & abs(eta) < 2.5 & userInt(\'mvaidwp\') > 0.5 & (userIso(0) + max(userIso(1) + neutralHadronIso - 0.5*userIso(2), 0))/pt < 0.3").size()',
+    eVetoCicTightIso = 'vetoElectrons(0.3, "pt > 10 & abs(eta) < 2.5 &  test_bit(electronID(\'cicTight\'), 0) > 0.5 & (userIso(0) + max(userIso(1) + neutralHadronIso - 0.5*userIso(2), 0))/pt < 0.3").size()',
     bjetVeto = 'vetoJets(0.4, "pt > 20 & abs(eta) < 2.5 & bDiscriminator(\'\') > 3.3").size()',
     bjetCSVVeto = 'vetoJets(0.4, "pt > 20 & abs(eta) < 2.5 & bDiscriminator(\'combinedSecondaryVertexBJetTagsAOD\') > 0.679").size()',
     jetVeto20 = 'vetoJets(0.4, "pt > 20 & abs(eta) < 5.0 & userInt(\'fullIdLoose\')").size()',
@@ -29,7 +29,6 @@ vetos = PSet(
 
 overlaps = PSet(
     objectMuOverlap = 'overlapMuons({object_idx}, 0.3, "pt > 5").size()',
-    # Electrons embedding currently broken
-    #objectElecOverlap = 'overlapElectrons({object_idx}, "pt > 10").size()',
-    #objectCiCTightElecOverlap = 'overlapElectrons({object_idx}, "pt > 10 & test_bit(electronID(\'cicTight\'), 0)").size()',
+    objectElecOverlap = 'overlapElectrons({object_idx}, "pt > 10").size()',
+    objectCiCTightElecOverlap = 'overlapElectrons({object_idx}, "pt > 10 & test_bit(electronID(\'cicTight\'), 0)").size()',
 )
