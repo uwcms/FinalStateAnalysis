75bec4c9168e64772ce459555f95fc868473e121
Evan Friis <evan.friis@cern.ch>
2012-07-07 05:05:33 -0500

    Use 0.4 cones for all cleaning/vetos

--- NtupleTools/python/templates/cleaning.py
+++ NtupleTools/python/templates/cleaning.py
@@ -15,12 +15,12 @@ from FinalStateAnalysis.Utilities.cfgtools import PSet
 
 # Vetos on extra stuff in the event
 vetos = PSet(
-    muVetoPt5 = 'vetoMuons(0.3, "pt > 5 & abs(eta) < 2.4").size()',
-    muGlbIsoVetoPt10 = 'vetoMuons(0.3, "isGlobalMuon & isTrackerMuon & pt > 10 & abs(eta) < 2.4 & (userIso(0) + max(photonIso + neutralHadronIso - 0.5*puChargedHadronIso, 0))/pt < 0.3").size()',
-    tauVetoPt20 = 'vetoTaus(0.3, "pt > 20 & abs(eta) < 2.5 & tauID(\'decayModeFinding\') & tauID(\'byLooseIsolationMVA\')").size()',
+    muVetoPt5 = 'vetoMuons(0.4, "pt > 5 & abs(eta) < 2.4").size()',
+    muGlbIsoVetoPt10 = 'vetoMuons(0.4, "isGlobalMuon & isTrackerMuon & pt > 10 & abs(eta) < 2.4 & (userIso(0) + max(photonIso + neutralHadronIso - 0.5*puChargedHadronIso, 0))/pt < 0.3").size()',
+    tauVetoPt20 = 'vetoTaus(0.4, "pt > 20 & abs(eta) < 2.5 & tauID(\'decayModeFinding\') & tauID(\'byLooseIsolationMVA\')").size()',
     # Electrons embedding currently broken
-    eVetoMVAIso = 'vetoElectrons(0.3, "pt > 10 & abs(eta) < 2.5 & userInt(\'mvaidwp\') > 0.5 & (userIso(0) + max(userIso(1) + neutralHadronIso - 0.5*userIso(2), 0))/pt < 0.3").size()',
-    eVetoCicTightIso = 'vetoElectrons(0.3, "pt > 10 & abs(eta) < 2.5 &  test_bit(electronID(\'cicTight\'), 0) > 0.5 & (userIso(0) + max(userIso(1) + neutralHadronIso - 0.5*userIso(2), 0))/pt < 0.3").size()',
+    eVetoMVAIso = 'vetoElectrons(0.4, "pt > 10 & abs(eta) < 2.5 & userInt(\'mvaidwp\') > 0.5 & (userIso(0) + max(userIso(1) + neutralHadronIso - 0.5*userIso(2), 0))/pt < 0.3").size()',
+    eVetoCicTightIso = 'vetoElectrons(0.4, "pt > 10 & abs(eta) < 2.5 &  test_bit(electronID(\'cicTight\'), 0) > 0.5 & (userIso(0) + max(userIso(1) + neutralHadronIso - 0.5*userIso(2), 0))/pt < 0.3").size()',
     bjetVeto = 'vetoJets(0.4, "pt > 20 & abs(eta) < 2.5 & bDiscriminator(\'\') > 3.3").size()',
     bjetCSVVeto = 'vetoJets(0.4, "pt > 20 & abs(eta) < 2.5 & bDiscriminator(\'combinedSecondaryVertexBJetTagsAOD\') > 0.679").size()',
     jetVeto20 = 'vetoJets(0.4, "pt > 20 & abs(eta) < 5.0 & userInt(\'fullIdLoose\')").size()',
