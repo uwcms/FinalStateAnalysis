eb232bf5754282dc647e45f6166005c00d95e0e0
Evan Friis <evan.friis@cern.ch>
2012-07-08 04:22:54 -0500

    Dont filter dupes in MC?  What is the policy on run-lumis in MC?

--- PlotTools/scripts/discover_ntuples.py
+++ PlotTools/scripts/discover_ntuples.py
@@ -5,7 +5,7 @@
 Given an input directory, find the input ntuple files and put them in one
 .txt filelist for each discovered sample.
 
-Filters out duplicate run-lumis.
+Filter out duplicate run-lumis in data, optionally in MC as well.
 
 Author: Evan K. Friis, UW
 
@@ -27,6 +27,9 @@ if __name__ == "__main__":
     parser.add_argument('outputdir', help='Output directory')
     parser.add_argument('--meta', default='mm/metaInfo',
                         help='Path to a meta tree, default: mm/metaInfo')
+    parser.add_argument('--filtermc', default=False,
+                        help='If true, skip MC files with run-lumis that exist'
+                        ' in other files.  Might not be correct...')
 
     args = parser.parse_args()
     import ROOT
@@ -50,7 +53,6 @@ if __name__ == "__main__":
                 if not tfile:
                     log.warning("Can't open file: %s" % file)
                     flist.write('# corrupt %s\n' % file)
-                    tfile.Close()
                     continue
                 ntuple = tfile.Get(args.meta)
                 if not ntuple:
@@ -60,16 +62,18 @@ if __name__ == "__main__":
                     continue
 
                 # Check to make sure this file doesn't have any lumi dupes
-                has_dupes = False
-                for row in ntuple:
-                    run_lumi = (int(row.run), int(row.lumi))
-                    if run_lumi in run_lumis:
-                        has_dupes = True
-                        break
-                    run_lumis.add(run_lumi)
+                has_dupes = None
+                if 'data' in sample_name or args.filtermc:
+                    for row in ntuple:
+                        run_lumi = (int(row.run), int(row.lumi))
+                        if run_lumi in run_lumis:
+                            has_dupes = run_lumi
+                            break
+                        run_lumis.add(run_lumi)
 
                 if has_dupes:
-                    log.error("Duplicate run lumi found in file: %s", file)
+                    log.error("Duplicate run lumi %s found in file: %s",
+                              repr(has_dupes), file)
                     flist.write('# dupe %s\n' % file)
                     tfile.Close()
                     continue
