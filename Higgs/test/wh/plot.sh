0adaaf8bdfbbf5e2ec0543ced58303ef0e10dda9
Evan Friis <evan.friis@cern.ch>
2012-07-24 12:05:32 -0500

    Update scripts

--- Higgs/test/wh/plot.sh
+++ Higgs/test/wh/plot.sh
@@ -1,25 +1,23 @@
-export jobid='2012-07-22-7TeV-Higgs'
-#rake fakerates
-#rake fits
+#!/bin/bash
+# Plot all of the analysis
+
+set -o nounset
+set -o errexit
+
+export jobid='2012-07-23-7TeV-Higgs'
 python WHPlotterEMT.py
-#python WHPlotterEET.py
 python WHPlotterMMT.py
-DumpMCvsData.py --files results/$jobid/FakeRatesMM/*root --lumifiles inputs/$jobid/*sum  --rebin 1  --outputdir results/$jobid/plots/FakeRatesMM_dump
-DumpMCvsData.py --files results/$jobid/FakeRatesME/*root --lumifiles inputs/$jobid/*sum  --rebin 1  --outputdir results/$jobid/plots/FakeRatesME_dump
-DumpMCvsData.py --files results/$jobid/FakeRatesMMM/*root --lumifiles inputs/$jobid/*sum  --rebin 1  --outputdir results/$jobid/plots/FakeRatesMMM_dump
-#rake cards
-#rake copycards
+python PlotControlZMM.py
+python PlotControlEM.py
+#DumpMCvsData.py --files results/$jobid/FakeRatesMM/*root --lumifiles inputs/$jobid/*sum  --rebin 1  --outputdir results/$jobid/plots/FakeRatesMM_dump
+#DumpMCvsData.py --files results/$jobid/FakeRatesME/*root --lumifiles inputs/$jobid/*sum  --rebin 1  --outputdir results/$jobid/plots/FakeRatesME_dump
+#DumpMCvsData.py --files results/$jobid/FakeRatesMMM/*root --lumifiles inputs/$jobid/*sum  --rebin 1  --outputdir results/$jobid/plots/FakeRatesMMM_dump
 
-export jobid='2012-07-22-8TeV-Higgs'
-#rake fakerates
-#rake fits
+export jobid='2012-07-23-8TeV-Higgs'
 python WHPlotterEMT.py
-#python WHPlotterEET.py
 python WHPlotterMMT.py
-DumpMCvsData.py --files results/$jobid/FakeRatesMM/*root --lumifiles inputs/$jobid/*sum  --rebin 1  --outputdir results/$jobid/plots/FakeRatesMM_dump
-DumpMCvsData.py --files results/$jobid/FakeRatesME/*root --lumifiles inputs/$jobid/*sum  --rebin 1  --outputdir results/$jobid/plots/FakeRatesME_dump
-DumpMCvsData.py --files results/$jobid/FakeRatesMMM/*root --lumifiles inputs/$jobid/*sum  --rebin 1  --outputdir results/$jobid/plots/FakeRatesMMM_dump
-
-#rake cards
-#rake copycards
-
+python PlotControlZMM.py
+python PlotControlEM.py
+#DumpMCvsData.py --files results/$jobid/FakeRatesMM/*root --lumifiles inputs/$jobid/*sum  --rebin 1  --outputdir results/$jobid/plots/FakeRatesMM_dump
+#DumpMCvsData.py --files results/$jobid/FakeRatesME/*root --lumifiles inputs/$jobid/*sum  --rebin 1  --outputdir results/$jobid/plots/FakeRatesME_dump
+#DumpMCvsData.py --files results/$jobid/FakeRatesMMM/*root --lumifiles inputs/$jobid/*sum  --rebin 1  --outputdir results/$jobid/plots/FakeRatesMMM_dump
--- Higgs/test/wh/run.sh
+++ Higgs/test/wh/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Run all of the analysis
 
 set -o nounset
 set -o errexit
@@ -8,12 +9,13 @@ rake fakerates
 rake fits
 rake mmt
 rake emt
-#rake eet
+rake mmcontrol
+rake emcontrol
 
 export jobid='2012-07-23-8TeV-Higgs'
 rake fakerates
 rake fits
 rake mmt
 rake emt
-#rake eet
-
+rake mmcontrol
+rake emcontrol
