1745062772af47fef2afd3c4a15d23ac4cd2bc03
Evan Friis <ekfriis@gmail.com>
2012-07-30 02:57:55 -0500

    Make username agnostic (take ntuples always from Evan's area on login06), use 7TeV WWW samples in 8TeV

--- Higgs/test/wh/setup.sh
+++ Higgs/test/wh/setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-export jobid=2012-07-24-7TeV-Higgs
-export afile=`find $s/data/$jobid | grep root | head -n 1`
+export datasrc=/scratch/efriis/data/
+export jobid=2012-07-23-7TeV-Higgs
+export jobid7TeV=$jobid
+export afile=`find $datasrc/$jobid | grep root | head -n 1`
 
 rake "make_wrapper[$afile, eet/final/Ntuple, EETauTree]"
 rake "make_wrapper[$afile, emt/final/Ntuple, EMuTauTree]"
@@ -13,10 +15,16 @@ rake "make_wrapper[$afile, ee/final/Ntuple, EETree]"
 
 ls *pyx | sed "s|pyx|so|" | xargs rake 
 
-
-rake "meta:getinputs[$jobid, $s/data]"
+rake "meta:getinputs[$jobid, $datasrc]"
 rake "meta:getmeta[inputs/$jobid, mm/metaInfo, 7]"
 
-export jobid=2012-07-24-8TeV-Higgs
-rake "meta:getinputs[$jobid, $s/data]"
+export jobid=2012-07-23-8TeV-Higgs
+rake "meta:getinputs[$jobid, $datasrc]"
+# Use the 7TeV WH samples for 8TeV
+pushd inputs/$jobid/
+# Symlink the list of input files and the counts of the number of events.
+# For the effectively lumis, we have to recompute using the 8 TeV x-section.
+ls ../../inputs/$jobid7TeV/WH_*HWW* | grep -v lumicalc | xargs -n 1 ln -s 
+popd
 rake "meta:getmeta[inputs/$jobid, mm/metaInfo, 8]"
+
