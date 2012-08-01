6bccdf440312c7a8447e44db7a27d004f866c837
Evan Friis <ekfriis@gmail.com>
2012-08-01 10:31:36 -0500

    Add mu + jet skim

---
+++ RecoTools/test/muPlusJetSkim_cfg.py
@@ -0,0 +1,39 @@
+'''
+
+Select ~good reconstructed mu+jet events.
+
+Muon is isolated, pt > 20
+
+At least one jet, not on top of the muon, with pt > 15.
+
+'''
+
+process = cms.Process("SKIM")
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(5000)
+)
+process.source = cms.Source(
+    "PoolSource",
+    fileNames = cms.untracked.vstring(
+        '/store/data/Run2011B/TauPlusX/AOD/PromptReco-v1/000/175/832/9E5E5FA4-35DD-E011-9C06-003048D2C020.root'
+    )
+)
+
+process.load("FinalStateAnalysis.RecoTools.triggerSkims_cfi")
+
+process.muPlusJetSkim = cms.Path(process.muPlusJetSeq)
+
+process.out = cms.OutputModule(
+    "PoolOutputModule",
+    fileName = cms.untracked.string('skim.root'),
+    outputCommands = cms.untracked.vstring('keep *_*_*_*',
+                                           'drop *_*_*_SKIM'),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents=cms.vstring("muPlusJetSkim")
+    )
+)
+
+process.e = cms.EndPath(process.out)
+
+process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
