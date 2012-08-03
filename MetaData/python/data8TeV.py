8374080013da699e649e0aaa48592f19e763b8de
tsarangi <tapas.sarangi@cern.ch>
2012-08-03 05:11:07 -0500

    added signal WH_HBB dataset info to datadefs

--- MetaData/python/data8TeV.py
+++ MetaData/python/data8TeV.py
@@ -190,17 +190,23 @@ datadefs = {
         'pu' : 'data',
         'responsible' : 'Evan',
         },
-    ############################################################################
-    #### Signal datasets                    ####################################
-    ############################################################################
-    'WH_WToLNu_HToBB_M-125' : {
-   'datasetpath' :"/WH_WToLNu_HToBB_M-125_8TeV-powheg-herwigpp/Summer12-PU_S7_START52_V9-v1/AODSIM",
-   'pu' : 'S7',
-   'x_sec' : 999,
-   'analyses' : ['VH', 'HBB'],
-   'responsible' : 'Isobel',
-   },       
 }
+
+############################################################################
+#### Signal datasets                    ####################################
+############################################################################
+# Add HToBB
+for mass in range(110, 140, 5):
+   ver=1
+   if mass==120 :
+      ver=2
+   datadefs['WH_WToLNu_HToBB_M-%i' % mass]= {
+      'datasetpath' :'/WH_WToLNu_HToBB_M-%i_8TeV-powheg-herwigpp/Summer12-PU_S7_START52_V9-v%i/AODSIM' % (mass, ver), 
+      'pu' : 'S7',
+      'x_sec' : -999,
+      'analyses' : ['VH', 'HBB'],
+      'responsible' : 'Tapas',
+   }       
 
 # Add GGH H2Tau samples
 for mass in range(110, 165, 5):
