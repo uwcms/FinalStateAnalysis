eb1c52570c80e5be88fab672f2a6d31758e17986
tsarangi <tapas.sarangi@cern.ch>
2012-08-28 11:43:24 -0500

    Commit json_DCSONLY.txt

--- RecoTools/data/masks/update.sh
+++ RecoTools/data/masks/update.sh
@@ -10,8 +10,12 @@ base=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/
 rsync -v -a --exclude="*CMSSW*" --exclude="*/" --exclude="*/*" --include="Cert*.txt" --exclude="*" $base/Collisions11/7TeV/Prompt/ .
 # Get 2011 reprocessing jsons
 rsync -v -a --exclude="*CMSSW*" --exclude="*/" --exclude="*/*" --include="Cert*.txt" --exclude="*" $base/Collisions11/7TeV/Reprocessing/ .
+# Get 2011 DCSOnly jsons
+rsync -v -a --exclude="*CMSSW*" --exclude="*/" --exclude="*/*" --include="*json*DCSOnly*.txt" --exclude="*" $base/Collisions11/7TeV/DCSOnly/ .
 
 # Get 2012 prompt jsons
 rsync -v -a --exclude="*CMSSW*" --exclude="*/" --exclude="*/*" --include="Cert*.txt" --exclude="*" $base/Collisions12/8TeV/Prompt/ .
 # Get 2012 reprocessing jsons
 rsync -v -a --exclude="*CMSSW*" --exclude="*/" --exclude="*/*" --include="Cert*.txt" --exclude="*" $base/Collisions12/8TeV/Reprocessing/ .
+# Get 2012 DCSOnly jsons
+rsync -v -a --exclude="*CMSSW*" --exclude="*/" --exclude="*/*" --include="*json*DCSOnly*.txt" --exclude="*" $base/Collisions12/8TeV/DCSOnly/ .
