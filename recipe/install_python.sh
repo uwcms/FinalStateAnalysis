21da46679fa4fa427dc4c82bc0ae09952b17b308
Evan Friis <ekfriis@gmail.com>
2012-09-11 07:52:17 -0500

    Make rootpy a submodule, improve and document python installation script

--- recipe/install_python.sh
+++ recipe/install_python.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-cmsenv
+# 
+# This script sets up a python "virtualenv" [1], allowing us to install some 
+# useful python modules.
+# 
+# Author: Evan K. Friis, UW Madison
+# 
+# Installed modules:
+#   > yolk - tools for managing installed packages
+#   > ipython - better interactive python
+#   > termcolor - utility for coloring the consol
+#   > uncertainties - awesome error propagation
+#   > progressbar - cool progress bars
+#   > cython - used for mixing C and python
+#   > argparse - improved argument parsing
+#   > rootpy - pythonic bindings for PyROOT 
 
 export recipe=$CMSSW_BASE/src/FinalStateAnalysis/recipe
 export vpython=$CMSSW_BASE/src/FinalStateAnalysis/recipe/external/vpython
@@ -13,8 +27,6 @@ echo "Activating virtual python environment"
 cd $vpython
 source bin/activate
 
-echo "Installing rootpy"
-pip install git+https://github.com/uwcms/rootpy.git
 echo "Installing yolk"
 pip install -U yolk
 echo "Installing ipython"
@@ -27,4 +39,9 @@ echo "Install progressbar"
 pip install -U progressbar
 echo "Install cython"
 pip install -U cython
+echo "Installing argparse"
+pip install -U argparse
+
+echo "Installing rootpy"
+pip install -e $recipe/external/src/rootpy
 
