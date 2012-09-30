a0aec410d6ac7e8ada3b3dbcb019a6fa89b68e6c
Evan Friis <ekfriis@gmail.com>
2012-09-30 09:22:11 -0500

    Add ability to use variable width bins

--- PlotTools/python/RebinView.py
+++ PlotTools/python/RebinView.py
@@ -6,6 +6,7 @@ Author: Evan K. Friis, UW Madison
 
 '''
 
+import array
 import rootpy.plotting.views as views
 
 class RebinView(views._FolderView):
@@ -18,7 +19,24 @@ class RebinView(views._FolderView):
         self.binning = binning
         super(RebinView, self).__init__(dir)
 
+    @staticmethod
+    def rebin(histogram, binning):
+        ''' Rebin a histogram
+
+        [binning] can be either an integer, or a list/tuple for variable bin
+        sizes.
+
+        '''
+        # Just merging bins
+        if isinstance(binning, int):
+            histogram.Rebin(binning)
+            return histogram
+        # Fancy variable size bins
+        bin_array = array.array('d', binning)
+        new_histo = histogram.Rebin(len(binning)-1, histogram.GetName() + 'rebin', bin_array)
+        return new_histo
+        #import pdb; pdb.set_trace()
+
     def apply_view(self, object):
         object = object.Clone()
-        object.Rebin(self.binning)
-        return object
+        return self.rebin(object, self.binning)
