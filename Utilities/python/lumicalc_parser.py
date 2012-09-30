dbc314b8743f75d1b5d18a4fdaa53c3dfe35fb62
Evan Friis <ekfriis@gmail.com>
2012-09-30 05:15:35 -0500

    Move algorithm part of lumicalc parser into lumitools

--- Utilities/python/lumicalc_parser.py
+++ Utilities/python/lumicalc_parser.py
@@ -3,18 +3,18 @@
 
 Parses the CSV output of lumi calc to get the total luminosity.
 
+Prints the recorded int. lumi. (in pb-1) to STDOUT
+
+Usage:
+
+    lumicalc_parser.py lumiCalc_output.csv
+
+Author: Evan K. Friis, UW Madison
+
 '''
 
-import csv
-
-def get_total_lumi(csv_filename):
-    file = open(csv_filename, 'r')
-    reader = csv.DictReader(file)
-    output = 0.
-    for row in reader:
-        output += float(row['Recorded(/ub)'])
-    return output/1e6
+from FinalStateAnalysis.Utilities.lumitools import parse_lumicalc_output
 
 if __name__ == "__main__":
     import sys
-    print get_total_lumi(sys.argv[1])
+    print parse_lumicalc_output(sys.argv[1])
