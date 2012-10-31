795cf658167009cd691d8e625979c35e1b78e007
Lindsey Gray <lindsey.gray@gmail.com>
2012-10-31 16:08:45 +0100

    all combinatorics done, wooo

--- PatTools/plugins/PATPairFinalStateBuilder.cc
+++ PatTools/plugins/PATPairFinalStateBuilder.cc
@@ -4,14 +4,22 @@
 typedef PATPairFinalStateBuilderT<PATElecElecFinalState> PATElecElecFinalStateProducer;
 typedef PATPairFinalStateBuilderT<PATElecMuFinalState> PATElecMuFinalStateProducer;
 typedef PATPairFinalStateBuilderT<PATElecTauFinalState> PATElecTauFinalStateProducer;
+typedef PATPairFinalStateBuilderT<PATElecPhoFinalState> PATElecPhoFinalStateProducer;
 typedef PATPairFinalStateBuilderT<PATMuMuFinalState> PATMuMuFinalStateProducer;
 typedef PATPairFinalStateBuilderT<PATMuTauFinalState> PATMuTauFinalStateProducer;
+typedef PATPairFinalStateBuilderT<PATMuPhoFinalState> PATMuPhoFinalStateProducer;
 typedef PATPairFinalStateBuilderT<PATTauTauFinalState> PATTauTauFinalStateProducer;
+typedef PATPairFinalStateBuilderT<PATTauPhoFinalState> PATTauPhoFinalStateProducer;
+typedef PATPairFinalStateBuilderT<PATPhoPhoFinalState> PATPhoPhoFinalStateProducer;
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 DEFINE_FWK_MODULE(PATElecElecFinalStateProducer);
 DEFINE_FWK_MODULE(PATElecMuFinalStateProducer);
 DEFINE_FWK_MODULE(PATElecTauFinalStateProducer);
+DEFINE_FWK_MODULE(PATElecPhoFinalStateProducer);
 DEFINE_FWK_MODULE(PATMuMuFinalStateProducer);
 DEFINE_FWK_MODULE(PATMuTauFinalStateProducer);
+DEFINE_FWK_MODULE(PATMuPhoFinalStateProducer);
 DEFINE_FWK_MODULE(PATTauTauFinalStateProducer);
+DEFINE_FWK_MODULE(PATTauPhoFinalStateProducer);
+DEFINE_FWK_MODULE(PATPhoPhoFinalStateProducer);
