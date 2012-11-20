da59725ff6842d2f165d6029b9ca01effbb83704
Evan Friis <ekfriis@gmail.com>
2012-11-20 09:20:52 -0600

    Define electron working point cuts using super cluster eta.
    Should fix #51.

--- PatTools/python/electrons/patElectronMVAWPEmbedding_cfi.py
+++ PatTools/python/electrons/patElectronMVAWPEmbedding_cfi.py
@@ -13,27 +13,27 @@ patElectronMVAIDWPEmbedding = cms.EDProducer(
     userIntLabel = cms.string('mvaidwp'),
     categories = cms.VPSet(
         cms.PSet(
-            category = cms.string('pt < 20 & abs(eta) < 0.8'),
+            category = cms.string('pt < 20 & abs(superCluster.eta) < 0.8'),
             cut = cms.string('electronID("mvaNonTrigV0") > 0.925')
         ),
         cms.PSet(
-            category = cms.string('pt < 20 & abs(eta) >= 0.8 & abs(eta) < 1.479'),
+            category = cms.string('pt < 20 & abs(superCluster.eta) >= 0.8 & abs(superCluster.eta) < 1.479'),
             cut = cms.string('electronID("mvaNonTrigV0") > 0.915')
         ),
         cms.PSet(
-            category = cms.string('pt < 20 & abs(eta) >= 1.479'),
+            category = cms.string('pt < 20 & abs(superCluster.eta) >= 1.479'),
             cut = cms.string('electronID("mvaNonTrigV0") > 0.965')
         ),
         cms.PSet(
-            category = cms.string('pt >= 20 & abs(eta) < 0.8'),
+            category = cms.string('pt >= 20 & abs(superCluster.eta) < 0.8'),
             cut = cms.string('electronID("mvaNonTrigV0") > 0.905')
         ),
         cms.PSet(
-            category = cms.string('pt >= 20 & abs(eta) >= 0.8 & abs(eta) < 1.479'),
+            category = cms.string('pt >= 20 & abs(superCluster.eta) >= 0.8 & abs(superCluster.eta) < 1.479'),
             cut = cms.string('electronID("mvaNonTrigV0") > 0.955')
         ),
         cms.PSet(
-            category = cms.string('pt >= 20 & abs(eta) >= 1.479'),
+            category = cms.string('pt >= 20 & abs(superCluster.eta) >= 1.479'),
             cut = cms.string('electronID("mvaNonTrigV0") > 0.975')
         ),
     )
