f99f77f780b61e4f07bfe996290b18afae9e2993
Evan Friis <ekfriis@gmail.com>
2012-11-20 09:53:55 -0600

    PEP8

--- PatTools/python/electrons/patElectronMVAWPEmbedding_cfi.py
+++ PatTools/python/electrons/patElectronMVAWPEmbedding_cfi.py
@@ -2,39 +2,47 @@
 
 Embed working point cuts into pat::Electrons
 
-See: https://twiki.cern.ch/twiki/bin/view/CMS/HiggsToTauTauWorking2012#Object_ID_AN1
+See:
+
+https://twiki.cern.ch/twiki/bin/view/CMS/HiggsToTauTauWorking2012#Object_ID_AN1
 
 '''
 import FWCore.ParameterSet.Config as cms
 
 patElectronMVAIDWPEmbedding = cms.EDProducer(
     "PATElectronWorkingPointEmbedder",
-    src = cms.InputTag("fixme"),
-    userIntLabel = cms.string('mvaidwp'),
-    categories = cms.VPSet(
+    src=cms.InputTag("fixme"),
+    userIntLabel=cms.string('mvaidwp'),
+    categories=cms.VPSet(
         cms.PSet(
-            category = cms.string('pt < 20 & abs(superCluster.eta) < 0.8'),
-            cut = cms.string('electronID("mvaNonTrigV0") > 0.925')
+            category=cms.string('pt < 20 & abs(superCluster.eta) < 0.8'),
+            cut=cms.string('electronID("mvaNonTrigV0") > 0.925')
         ),
         cms.PSet(
-            category = cms.string('pt < 20 & abs(superCluster.eta) >= 0.8 & abs(superCluster.eta) < 1.479'),
-            cut = cms.string('electronID("mvaNonTrigV0") > 0.915')
+            category=cms.string(
+                'pt < 20 & '
+                'abs(superCluster.eta) >= 0.8 &'
+                'abs(superCluster.eta) < 1.479'),
+            cut=cms.string('electronID("mvaNonTrigV0") > 0.915')
         ),
         cms.PSet(
-            category = cms.string('pt < 20 & abs(superCluster.eta) >= 1.479'),
-            cut = cms.string('electronID("mvaNonTrigV0") > 0.965')
+            category=cms.string('pt < 20 & abs(superCluster.eta) >= 1.479'),
+            cut=cms.string('electronID("mvaNonTrigV0") > 0.965')
         ),
         cms.PSet(
-            category = cms.string('pt >= 20 & abs(superCluster.eta) < 0.8'),
-            cut = cms.string('electronID("mvaNonTrigV0") > 0.905')
+            category=cms.string('pt >= 20 & abs(superCluster.eta) < 0.8'),
+            cut=cms.string('electronID("mvaNonTrigV0") > 0.905')
         ),
         cms.PSet(
-            category = cms.string('pt >= 20 & abs(superCluster.eta) >= 0.8 & abs(superCluster.eta) < 1.479'),
-            cut = cms.string('electronID("mvaNonTrigV0") > 0.955')
+            category=cms.string(
+                'pt >= 20 & '
+                'abs(superCluster.eta) >= 0.8 & '
+                'abs(superCluster.eta) < 1.479'),
+            cut=cms.string('electronID("mvaNonTrigV0") > 0.955')
         ),
         cms.PSet(
-            category = cms.string('pt >= 20 & abs(superCluster.eta) >= 1.479'),
-            cut = cms.string('electronID("mvaNonTrigV0") > 0.975')
+            category=cms.string('pt >= 20 & abs(superCluster.eta) >= 1.479'),
+            cut=cms.string('electronID("mvaNonTrigV0") > 0.975')
         ),
     )
 )
