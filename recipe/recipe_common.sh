uwcms/FinalStateAnalysis

commit 2d270fe98f22b167404b35535d936983ccd18f89

Small cleanup, add MVA MET recipe to PAT tuple

recipe/recipe_common.sh
@@ -36,15 +36,10 @@ EOF
   # PU Jet ID
   cvs co -r V00-02-09 -d CMGTools/External UserCode/CMG/CMGTools/External
 
-  # Add Electron ID MVA  
-  pushd EGamma/EGammaAnalysisTools/data
-  cat download.url | xargs wget
+  # MVA MET
+  pushd $CMSSW_BASE/src/FinalStateAnalysis/recipe/
+  ./recipe_mvamet.sh
   popd
-
-  # Add muon effective area code
-  cvs co -r V00-00-10 -d Muon/MuonAnalysisTools UserCode/sixie/Muon/MuonAnalysisTools 
-  # Remove trainings we don't use
-  rm Muon/MuonAnalysisTools/data/*xml
 
 else
   cat > $CMSSW_BASE/src/FinalStateAnalysis/PatTools/interface/PATProductionFlag.h << EOF 

recipe/recipe_mvamet.sh
@@ -1,0 +1,27 @@
+#!/bin/bash
+
+# Install the MVA MET
+
+set -o errexit
+set -o nounset
+
+: ${CMSSW_BASE:?"CMSSW_BASE is not set!  Run cmsenv before recipe.sh"}
+
+# Check CMSSW version
+MAJOR_VERSION=`echo $CMSSW_VERSION | sed "s|CMSSW_\([0-9]\)_.*|\1|"`
+MINOR_VERSION=`echo $CMSSW_VERSION | sed "s|CMSSW_\([0-9]\)_\([0-9]\)_.*|\2|"`
+
+if [ "$MAJOR_VERSION" -eq "5" ]; then
+  # Add MVA MET
+  cvs co -r METPU_5_3_X_v2 JetMETCorrections/METPUSubtraction
+  pushd $CMSSW_BASE/src/JetMETCorrections/METPUSubtraction/test/
+  ./setup.sh
+  popd
+  cvs up -r 1.6 PhysicsTools/PatAlgos/plugins/PATMHTProducer.h
+else
+  cvs co -r METPU_4_2_X JetMETCorrections/METPUSubtraction
+  pushd $CMSSW_BASE/src/JetMETCorrections/METPUSubtraction/test/
+  ./setup42.sh
+  popd
+  cvs up -r 1.6 PhysicsTools/PatAlgos/plugins/PATMHTProducer.h
+fi

commit df174f2266b33e7e3848a664af83b71f995baab7

Fix regression in EG ID recipe
Fixes bug introduced in 2d270fe98f22b167404b35535d936983ccd18f89

recipe/recipe_common.sh
@@ -33,6 +33,12 @@ then
 #define ENABLE_PAT_PROD
 EOF
 
+  # Add Electron ID MVA - the tags get checked out in 42X/52X/53X specific
+  # scripts
+  pushd $CMSSW_BASE/src/EGamma/EGammaAnalysisTools/data
+  cat download.url | xargs wget
+  popd
+
   # PU Jet ID
   cvs co -r V00-02-09 -d CMGTools/External UserCode/CMG/CMGTools/External
 