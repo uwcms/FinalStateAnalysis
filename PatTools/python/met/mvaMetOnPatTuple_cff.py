b813afc559e036b6fb239b6b4f8eaf03c166494d
Evan Friis <ekfriis@gmail.com>
2012-11-22 06:05:12 -0600

    Flake8 + docs

--- PatTools/python/met/mvaMetOnPatTuple_cff.py
+++ PatTools/python/met/mvaMetOnPatTuple_cff.py
@@ -1,30 +1,45 @@
+'''
+
+Build a version of the MVA MET recipe which runs only on objects in
+the UW PAT tuple.
+
+Author: Evan K. Friis, UW Madison
+
+'''
+
+import FWCore.ParameterSet.Config as cms
+
 try:
     from JetMETCorrections.METPUSubtraction.mvaPFMET_leptons_cff import \
-            calibratedAK5PFJetsForPFMEtMVA, pfMEtMVA, \
-            isomuons, isoelectrons, isotaus
+        calibratedAK5PFJetsForPFMEtMVA, pfMEtMVA, \
+        isomuons, isoelectrons
 
     # Modify muons
     muon_cut = isomuons.cut
     isomuons = cms.EDFilter(
         "PATMuonSelector",
-        src = cms.InputTag("cleanPatMuons"),
-        cut = muon_cut,
-        filter = cms.bool(False)
+        src=cms.InputTag("cleanPatMuons"),
+        cut=muon_cut,
+        filter=cms.bool(False)
     )
     # Modify electrons
     e_cut = isoelectrons.cut
     isoelectrons = cms.EDFilter(
         "PATMuonSelector",
-        src = cms.InputTag("cleanPatElectrons"),
-        cut = e_cut,
-        filter = cms.bool(False)
+        src=cms.InputTag("cleanPatElectrons"),
+        cut=e_cut,
+        filter=cms.bool(False)
     )
     # Modify taus
-    isotaus = EDFilter(
+    isotaus = cms.EDFilter(
         "PATTauSelector",
-        src = cms.InputTag("selectedPatTaus"),
-        cut = cms.string('pt > 19 && abs(eta) < 2.3 && tauID("decayModeFinding") && tauID("byIsolationMVAraw") > 0.7 && tauID("againstElectronLoose") && tauID("againstMuonLoose")'),
-        filter = cms.bool(False)
+        src=cms.InputTag("selectedPatTaus"),
+        cut=cms.string(
+            'pt > 19 && abs(eta) < 2.3 && '
+            'tauID("decayModeFinding") && '
+            'tauID("byIsolationMVAraw") > 0.7 && '
+            'tauID("againstElectronLoose") && tauID("againstMuonLoose")'),
+        filter=cms.bool(False)
     )
 
     pfMEtMVAsequence = cms.Sequence(
@@ -35,4 +50,5 @@ try:
 except ImportError:
     import sys
     sys.stderr.write(
-        "Warning: MVA MET dependencies not installed => MVA MET is disabled.\n")
+        "Warning: MVA MET dependencies not installed => MVA MET disabled.\n")
+    pfMEtMVAsequence = cms.Sequence()
