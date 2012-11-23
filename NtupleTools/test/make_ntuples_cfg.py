8d6fdedb80f842ac677d18009ccf09db9c396573
Evan Friis <ekfriis@gmail.com>
2012-11-23 06:44:51 -0600

    PEP8

--- NtupleTools/test/make_ntuples_cfg.py
+++ NtupleTools/test/make_ntuples_cfg.py
@@ -22,7 +22,7 @@ process = cms.Process("TrileptonNtuple")
 
 import FinalStateAnalysis.Utilities.TauVarParsing as TauVarParsing
 options = TauVarParsing.TauVarParsing(
-    skipEvents=0, # For debugging
+    skipEvents=0,  # Start at an event offset (for debugging)
     puScenario='S4',
     saveSkim=0,
     reportEvery=100,
@@ -42,14 +42,13 @@ options = TauVarParsing.TauVarParsing(
     noPhotons=0,  # If one, don't assume that photons are in the PAT tuples.
 )
 
-options.outputFile="ntuplize.root"
+options.outputFile = "ntuplize.root"
 options.parseArguments()
 
 process.source = cms.Source(
     "PoolSource",
-    fileNames = cms.untracked.vstring(options.inputFiles),
-    skipEvents = cms.untracked.uint32(options.skipEvents),
-    #lumisToProcess = options.buildPoolSourceLumiMask()
+    fileNames=cms.untracked.vstring(options.inputFiles),
+    skipEvents=cms.untracked.uint32(options.skipEvents),
 )
 
 
@@ -63,31 +62,29 @@ if options.lumiMask:
     process.source.lumisToProcess = options.buildPoolSourceLumiMask()
 
 process.TFileService = cms.Service(
-    "TFileService", fileName = cms.string(options.outputFile)
+    "TFileService", fileName=cms.string(options.outputFile)
 )
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(options.maxEvents))
+    input=cms.untracked.int32(options.maxEvents))
 
-process.schedule = cms.Schedule(
-)
+process.schedule = cms.Schedule()
 
 # Check if we want to rerun creation of the FSA objects
 if options.rerunFSA:
     print "Rebuilding FS composite objects"
     # Drop the input ones, just to make sure we aren't screwing anything up
-    # fixme
     process.buildFSASeq = cms.Sequence()
     from FinalStateAnalysis.PatTools.patFinalStateProducers \
-            import produce_final_states
+        import produce_final_states
     # Which collections are used to build the final states
     fs_daughter_inputs = {
-        'electrons' : 'cleanPatElectrons',
-        'muons' : 'cleanPatMuons',
-        'taus' : 'cleanPatTaus',
-        'photons' : 'cleanPatPhotons',
-        'jets' : 'selectedPatJets',
-        'met' : 'systematicsMET',
+        'electrons': 'cleanPatElectrons',
+        'muons': 'cleanPatMuons',
+        'taus': 'cleanPatTaus',
+        'photons': 'cleanPatPhotons',
+        'jets': 'selectedPatJets',
+        'met': 'systematicsMET',
     }
     # Eventually, set buildFSAEvent to False, currently working around bug
     # in pat tuples.
@@ -99,7 +96,7 @@ if options.rerunFSA:
     process.patFinalStateEventProducer.forbidMissing = cms.bool(False)
     process.schedule.append(process.buildFSAPath)
     # Drop the old stuff.
-    process.source.inputCommands=cms.untracked.vstring(
+    process.source.inputCommands = cms.untracked.vstring(
         'keep *',
         'drop PATFinalStatesOwned_finalState*_*_*',
         'drop *_patFinalStateEvent*_*_*'
@@ -107,50 +104,50 @@ if options.rerunFSA:
 
 from FinalStateAnalysis.NtupleTools.tnp_ntuples_cfi import add_tnp_ntuples
 from FinalStateAnalysis.NtupleTools.h2tau_ntuples_cfi import add_h2tau_ntuples
-from FinalStateAnalysis.NtupleTools.trilepton_ntuples_cfi import add_trilepton_ntuples
-from FinalStateAnalysis.NtupleTools.lepton_photon_ntuples_cfi import add_leptonphoton_ntuples
+from FinalStateAnalysis.NtupleTools.trilepton_ntuples_cfi \
+    import add_trilepton_ntuples
+from FinalStateAnalysis.NtupleTools.lepton_photon_ntuples_cfi \
+    import add_leptonphoton_ntuples
 from FinalStateAnalysis.NtupleTools.quad_ntuples_cfi import add_quad_ntuples
 
 if options.makeH2Tau:
-    add_h2tau_ntuples(process, process.schedule,
-                      event_view = options.eventView)
+    add_h2tau_ntuples(process, process.schedule, event_view=options.eventView)
 
 if options.makeTNP:
-    add_tnp_ntuples(process, process.schedule,
-                    event_view = options.eventView)
+    add_tnp_ntuples(process, process.schedule, event_view=options.eventView)
 
 if options.makeTrilepton:
-    add_trilepton_ntuples(process, process.schedule,
-                          event_view = options.eventView)
+    add_trilepton_ntuples(
+        process, process.schedule, event_view=options.eventView)
 
 if options.makeQuad:
     add_quad_ntuples(process, process.schedule,
                      do_zz=False, do_zh=True,
-                     oevent_view = ptions.event_view)
+                     event_view=options.event_view)
 
 if options.make4L:
     add_quad_ntuples(process, process.schedule,
                      do_zh=False, do_zz=True,
-                     event_view = options.eventView)
+                     event_view=options.eventView)
 
 if options.makeHZG:
     add_trilepton_ntuples(process, process.schedule,
-                          do_trileptons=False, do_photons = True,
-                          event_view = options.eventView)
+                          do_trileptons=False, do_photons=True,
+                          event_view=options.eventView)
 
 if options.makeTGC:
     add_leptonphoton_ntuples(process, process.schedule,
                              options.eventView)
     add_trilepton_ntuples(process, process.schedule,
-                          do_trileptons=False, do_photons = True,
-                          event_view = options.eventView)
+                          do_trileptons=False, do_photons=True,
+                          event_view=options.eventView)
 if options.makeQuartic:
     add_trilepton_ntuples(process, process.schedule,
-                          do_trileptons=True, do_photons = True,
-                          event_view = options.eventView)
+                          do_trileptons=True, do_photons=True,
+                          event_view=options.eventView)
     add_quad_ntuples(process, process.schedule,
                      do_zh=False, do_zz=False, do_zgg=True,
-                     event_view = options.eventView)
+                     event_view=options.eventView)
 
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
@@ -158,11 +155,11 @@ process.MessageLogger.cerr.FwkReport.reportEvery = options.reportEvery
 process.MessageLogger.categories.append('FSAEventMissingProduct')
 # Don't go nuts if there are a lot of missing products.
 process.MessageLogger.cerr.FSAEventMissingProduct = cms.untracked.PSet(
-    limit = cms.untracked.int32(10)
+    limit=cms.untracked.int32(10)
 )
 
 if options.verbose:
-    process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
+    process.options = cms.untracked.PSet(wantSummary=cms.untracked.bool(True))
 if options.passThru:
     set_passthru(process)
 
