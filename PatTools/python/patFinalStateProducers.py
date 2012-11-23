eaab21febaf9929739b3a73f7c228ba5e70545ba
Evan Friis <ekfriis@gmail.com>
2012-11-23 06:23:25 -0600

    PEP8

--- PatTools/python/patFinalStateProducers.py
+++ PatTools/python/patFinalStateProducers.py
@@ -23,15 +23,17 @@ from FinalStateAnalysis.Utilities.cfgtools import chain_sequence
 import PhysicsTools.PatAlgos.tools.helpers as helpers
 import itertools
 
+
 def _subsort(iterables):
     for iterable in iterables:
         yield tuple(sorted(iterable))
 
+
 def _combinatorics(items, n):
     ''' Build unique combination of items
     >>> items = ['Ananas', 'Abricot', 'Bouef']
-    >>> list(_combinatorics(items, 2))
-    [('Ananas', 'Ananas'), ('Ananas', 'Abricot'), ('Ananas', 'Bouef'), ('Abricot', 'Abricot'), ('Abricot', 'Bouef'), ('Bouef', 'Bouef')]
+    >>> list(_combinatorics(items, 2))[0:3]
+    [('Ananas', 'Ananas'), ('Ananas', 'Abricot'), ('Ananas', 'Bouef')]
     '''
     indices = range(len(items))
     combinatorics = set(_subsort(itertools.product(indices, repeat=n)))
@@ -39,8 +41,9 @@ def _combinatorics(items, n):
         yield tuple(items[x] for x in index_set)
 
 
-def produce_final_states(process, collections, output_commands, sequence, puTag,
-                         buildFSAEvent=True, noTracks=False):
+def produce_final_states(process, collections, output_commands,
+                         sequence, puTag, buildFSAEvent=True,
+                         noTracks=False, noPhotons=False):
 
     muonsrc = collections['muons']
     esrc = collections['electrons']
@@ -50,8 +53,9 @@ def produce_final_states(process, collections, output_commands, sequence, puTag,
     phosrc = collections['photons']
 
     # Build the PATFinalStateEventObject
-    if buildFSAEvent == True:
-        process.load("FinalStateAnalysis.PatTools.finalStates.patFinalStateEventProducer_cfi")
+    if buildFSAEvent:
+        process.load("FinalStateAnalysis.PatTools."
+                     "finalStates.patFinalStateEventProducer_cfi")
         process.patFinalStateEventProducer.electronSrc = cms.InputTag(esrc)
         process.patFinalStateEventProducer.muonSrc = cms.InputTag(muonsrc)
         process.patFinalStateEventProducer.tauSrc = cms.InputTag(tausrc)
@@ -68,53 +72,56 @@ def produce_final_states(process, collections, output_commands, sequence, puTag,
     # so the combinatorics don't blow up
     process.muonsForFinalStates = cms.EDFilter(
         "PATMuonRefSelector",
-        src = cms.InputTag(muonsrc),
-        cut = cms.string('pt > 4 & (isGlobalMuon | isTrackerMuon)'),
-        filter = cms.bool(False),
+        src=cms.InputTag(muonsrc),
+        cut=cms.string('pt > 4 & (isGlobalMuon | isTrackerMuon)'),
+        filter=cms.bool(False),
     )
 
     process.electronsForFinalStates = cms.EDFilter(
         "PATElectronRefSelector",
-        src = cms.InputTag(esrc),
-        cut = cms.string('abs(eta) < 2.5 & pt > 7'),
-        filter = cms.bool(False),
+        src=cms.InputTag(esrc),
+        cut=cms.string('abs(eta) < 2.5 & pt > 7'),
+        filter=cms.bool(False),
     )
 
     process.photonsForFinalStates = cms.EDFilter(
         "PATPhotonRefSelector",
-        src = cms.InputTag(phosrc),
-        cut = cms.string('abs(eta) < 2.5 & pt > 10'),
-        filter = cms.bool(False),
+        src=cms.InputTag(phosrc),
+        cut=cms.string('abs(eta) < 2.5 & pt > 10'),
+        filter=cms.bool(False),
     )
 
     # Require that the PT of the jet (either corrected jet or tau)
     # to be greater than 17
     process.tausForFinalStates = cms.EDFilter(
         "PATTauRefSelector",
-        src = cms.InputTag(tausrc),
-        cut = cms.string('abs(eta) < 2.5 & pt > 17 & tauID("decayModeFinding")'),
-        filter = cms.bool(False),
+        src=cms.InputTag(tausrc),
+        cut=cms.string('abs(eta) < 2.5 & pt > 17 & tauID("decayModeFinding")'),
+        filter=cms.bool(False),
     )
 
     process.selectObjectsForFinalStates = cms.Sequence(
         process.muonsForFinalStates
         + process.electronsForFinalStates
         + process.tausForFinalStates
-        + process.photonsForFinalStates
     )
+    if not noPhotons:
+        process.selectObjectsForFinalStates += process.photonsForFinalStates
 
     sequence += process.selectObjectsForFinalStates
 
     # Now build all combinatorics for E/Mu/Tau/Photon
     object_types = [('Elec', cms.InputTag("electronsForFinalStates")),
                     ('Mu', cms.InputTag("muonsForFinalStates")),
-                    ('Tau', cms.InputTag("tausForFinalStates")),
-                    ('Pho', cms.InputTag("photonsForFinalStates"))]
+                    ('Tau', cms.InputTag("tausForFinalStates"))]
+
+    if not noPhotons:
+        object_types.append(('Pho', cms.InputTag("photonsForFinalStates")))
 
     process.buildDiObjects = cms.Sequence()
 
-    process.load(
-        "FinalStateAnalysis.PatTools.finalStates.patFinalStatesEmbedExtraCollections_cfi")
+    process.load("FinalStateAnalysis.PatTools."
+                 "finalStates.patFinalStatesEmbedExtraCollections_cfi")
     # If we don't have tracks, don't fit the FS vertices
     if noTracks:
         process.patFinalStateVertexFitter.enable = False
@@ -126,29 +133,27 @@ def produce_final_states(process, collections, output_commands, sequence, puTag,
             continue
 
         # Define some basic selections for building combinations
-        cuts = ['smallestDeltaR() > 0.3'] # basic x-cleaning
+        cuts = ['smallestDeltaR() > 0.3']  # basic x-cleaning
 
         producer = cms.EDProducer(
             "PAT%s%sFinalStateProducer" % (diobject[0][0], diobject[1][0]),
-            evtSrc = cms.InputTag("patFinalStateEventProducer"),
-            leg1Src = diobject[0][1],
-            leg2Src = diobject[1][1],
+            evtSrc=cms.InputTag("patFinalStateEventProducer"),
+            leg1Src=diobject[0][1],
+            leg2Src=diobject[1][1],
             # X-cleaning
-            cut = cms.string(' & '.join(cuts))
+            cut=cms.string(' & '.join(cuts))
         )
         producer_name = "finalState%s%s" % (diobject[0][0], diobject[1][0])
         setattr(process, producer_name + "Raw", producer)
         process.buildDiObjects += producer
         # Embed the other collections
-        embedder_seq = helpers.cloneProcessingSnippet(process,
-            process.patFinalStatesEmbedObjects, producer_name)
+        embedder_seq = helpers.cloneProcessingSnippet(
+            process, process.patFinalStatesEmbedObjects, producer_name)
         process.buildDiObjects += embedder_seq
         # Do some trickery so the final module has a nice output name
         final_module_name = chain_sequence(embedder_seq, producer_name + "Raw")
         final_module = cms.EDProducer(
-            "PATFinalStateCopier",
-            src = final_module_name
-        )
+            "PATFinalStateCopier", src=final_module_name)
         setattr(process, producer_name, final_module)
         process.buildDiObjects += final_module
         setattr(process, producer_name, final_module)
@@ -164,17 +169,17 @@ def produce_final_states(process, collections, output_commands, sequence, puTag,
             continue
 
         # Define some basic selections for building combinations
-        cuts = ['smallestDeltaR() > 0.3'] # basic x-cleaning
+        cuts = ['smallestDeltaR() > 0.3']  # basic x-cleaning
 
         producer = cms.EDProducer(
             "PAT%s%s%sFinalStateProducer" %
             (triobject[0][0], triobject[1][0], triobject[2][0]),
-            evtSrc = cms.InputTag("patFinalStateEventProducer"),
-            leg1Src = triobject[0][1],
-            leg2Src = triobject[1][1],
-            leg3Src = triobject[2][1],
+            evtSrc=cms.InputTag("patFinalStateEventProducer"),
+            leg1Src=triobject[0][1],
+            leg2Src=triobject[1][1],
+            leg3Src=triobject[2][1],
             # X-cleaning
-            cut = cms.string(' & '.join(cuts))
+            cut=cms.string(' & '.join(cuts))
         )
         producer_name = "finalState%s%s%s" % (
             triobject[0][0], triobject[1][0], triobject[2][0])
@@ -183,15 +188,13 @@ def produce_final_states(process, collections, output_commands, sequence, puTag,
         setattr(process, producer_name + "Raw", producer)
         process.buildTriObjects += producer
         # Embed the other collections
-        embedder_seq = helpers.cloneProcessingSnippet(process,
-            process.patFinalStatesEmbedObjects, producer_name)
+        embedder_seq = helpers.cloneProcessingSnippet(
+            process, process.patFinalStatesEmbedObjects, producer_name)
         process.buildTriObjects += embedder_seq
         # Do some trickery so the final module has a nice output name
         final_module_name = chain_sequence(embedder_seq, producer_name + "Raw")
         final_module = cms.EDProducer(
-            "PATFinalStateCopier",
-            src = final_module_name
-        )
+            "PATFinalStateCopier", src=final_module_name)
         setattr(process, producer_name, final_module)
         process.buildTriObjects += final_module
         output_commands.append("*_%s_*_*" % producer_name)
@@ -205,19 +208,19 @@ def produce_final_states(process, collections, output_commands, sequence, puTag,
             continue
 
         # Define some basic selections for building combinations
-        cuts = ['smallestDeltaR() > 0.3'] # basic x-cleaning
+        cuts = ['smallestDeltaR() > 0.3']  # basic x-cleaning
 
         producer = cms.EDProducer(
             "PAT%s%s%s%sFinalStateProducer" %
             (quadobject[0][0], quadobject[1][0], quadobject[2][0],
              quadobject[3][0]),
-            evtSrc = cms.InputTag("patFinalStateEventProducer"),
-            leg1Src = quadobject[0][1],
-            leg2Src = quadobject[1][1],
-            leg3Src = quadobject[2][1],
-            leg4Src = quadobject[3][1],
+            evtSrc=cms.InputTag("patFinalStateEventProducer"),
+            leg1Src=quadobject[0][1],
+            leg2Src=quadobject[1][1],
+            leg3Src=quadobject[2][1],
+            leg4Src=quadobject[3][1],
             # X-cleaning
-            cut = cms.string(' & '.join(cuts))
+            cut=cms.string(' & '.join(cuts))
         )
         producer_name = "finalState%s%s%s%s" % (
             quadobject[0][0], quadobject[1][0], quadobject[2][0],
@@ -228,15 +231,13 @@ def produce_final_states(process, collections, output_commands, sequence, puTag,
         setattr(process, producer_name + "Raw", producer)
         process.buildQuadObjects += producer
         # Embed the other collections
-        embedder_seq = helpers.cloneProcessingSnippet(process,
-            process.patFinalStatesEmbedObjects, producer_name)
+        embedder_seq = helpers.cloneProcessingSnippet(
+            process, process.patFinalStatesEmbedObjects, producer_name)
         process.buildQuadObjects += embedder_seq
         # Do some trickery so the final module has a nice output name
         final_module_name = chain_sequence(embedder_seq, producer_name + "Raw")
         final_module = cms.EDProducer(
-            "PATFinalStateCopier",
-            src = final_module_name
-        )
+            "PATFinalStateCopier", src=final_module_name)
         setattr(process, producer_name, final_module)
         process.buildQuadObjects += final_module
         output_commands.append("*_%s_*_*" % producer_name)
