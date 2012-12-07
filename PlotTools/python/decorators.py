e40a42abae3c9c08a1282b06c8d662beeae86f67
Mauro Verzetti <mverzett@cern.ch>
2012-12-07 08:09:48 -0600

    decorators library

---
+++ PlotTools/python/decorators.py
@@ -0,0 +1,22 @@
+from functools import update_wrapper
+
+def decorator(d):
+    "Make function d a decorator that wraps function fn"
+    return lambda fn: update_wrapper(d(fn),fn)
+
+decorator = decorator(decorator)
+
+@decorator
+def memo(fn):
+    "Decorato to memoize (cache) results of a function"
+    cache = {}
+    def _f(*args):
+        try: #check if we have it in cache
+            return cache[args]
+        except KeyError: #No, we don't
+            cache[args] = result = fn(*args)
+            return result
+        except TypeError: #Actually, the args cannot even be a key of dict (like lists)
+            return fn(*args)
+    return _f
+        
