9f5860950c83c3e434cb1d1db080ab36c4388318
Lindsey Gray <lindsey.gray@gmail.com>
2012-12-11 06:27:11 -0600

    make MultiCandFinalState work

--- DataFormats/interface/PATMultiCandFinalState.h
+++ DataFormats/interface/PATMultiCandFinalState.h
@@ -31,7 +31,8 @@ class PATMultiCandFinalState : public PATFinalState {
         size_t i, const std::string& label) const;
 
   private:
-    std::vector<reco::CandidatePtr> cands_;
+    //reco::CandidateBaseRefVector cands_; // try new way later
+    std::vector<reco::CandidatePtr> cands_; // old way
 };
 
 #endif /* end of include guard: FinalStateAnalysis_DataFormats_PATMultiCandFinalState_h */
--- DataFormats/src/PATMultiCandFinalState.cc
+++ DataFormats/src/PATMultiCandFinalState.cc
@@ -1,5 +1,11 @@
 #include "FinalStateAnalysis/DataFormats/interface/PATMultiCandFinalState.h"
 
+#include "DataFormats/PatCandidates/interface/Electron.h"
+#include "DataFormats/PatCandidates/interface/Muon.h"
+#include "DataFormats/PatCandidates/interface/Tau.h"
+#include "DataFormats/PatCandidates/interface/Photon.h"
+#include "DataFormats/PatCandidates/interface/Jet.h"
+#include "DataFormats/PatCandidates/interface/MET.h"
 
 PATMultiCandFinalState::PATMultiCandFinalState():PATFinalState(){}
 
@@ -8,15 +14,16 @@ PATMultiCandFinalState::PATMultiCandFinalState(
     const edm::Ptr<PATFinalStateEvent>& evt):
   PATFinalState(0, reco::Candidate::LorentzVector(), evt),
   cands_(cands) {
-    // Setup p4 in base class
-    int charge = 0;
-    reco::Candidate::LorentzVector totalP4;
-    for (size_t i = 0; i < cands_.size(); ++i) {
-      charge += cands_[i]->charge();
-      totalP4 += cands_[i]->p4();
-    }
-    this->setCharge(charge);
-    this->setP4(totalP4);
+  
+  // Setup p4 in base class
+  int charge = 0;
+  reco::Candidate::LorentzVector totalP4;
+  for (size_t i = 0; i < cands_.size(); ++i) {
+    charge += cands_[i]->charge();
+    totalP4 += cands_[i]->p4();
+  }
+  this->setCharge(charge);
+  this->setP4(totalP4);
 }
 
 PATMultiCandFinalState* PATMultiCandFinalState::clone() const {
@@ -44,14 +51,58 @@ size_t PATMultiCandFinalState::numberOfDaughters() const {
 
 reco::CandidatePtr PATMultiCandFinalState::daughterUserCandUnsafe(size_t i,
     const std::string& tag) const {
-  throw cms::Exception("NotImplemented") <<
-    "The daughterUserCand functionality is not implemented for instances of" <<
-    " PATMultiCandFinalState." << std::endl;
+  reco::CandidatePtr theCand = cands_.at(i); // will throw OOB exception
+
+  if( dynamic_cast<const pat::Electron*>(theCand.get()) ) {
+    edm::Ptr<pat::Electron> asEle(theCand);
+    return asEle->userCand(tag); 
+  } else if ( dynamic_cast<const pat::Muon*>(theCand.get()) ) {
+    edm::Ptr<pat::Muon> asMuon(theCand);
+    return asMuon->userCand(tag); 
+  } else if ( dynamic_cast<const pat::Tau*>(theCand.get()) ) {
+    edm::Ptr<pat::Tau> asTau(theCand);
+    return asTau->userCand(tag); 
+  } else if ( dynamic_cast<const pat::Photon*>(theCand.get()) ) {
+    edm::Ptr<pat::Photon> asPho(theCand);
+    return asPho->userCand(tag); 
+  } else if ( dynamic_cast<const pat::Jet*>(theCand.get()) ) {
+    edm::Ptr<pat::Jet> asJet(theCand);
+    return asJet->userCand(tag); 
+  } else if ( dynamic_cast<const pat::MET*>(theCand.get()) ) {
+    edm::Ptr<pat::MET> asMET(theCand);
+    return asMET->userCand(tag); 
+  } else {
+    throw cms::Exception("Uncastable") 
+      << "The edm::Ptr at index " << i 
+      << "is not castable to a PAT Object." << std::endl;
+  }
 }
 
 const reco::CandidatePtrVector& PATMultiCandFinalState::daughterOverlaps(
     size_t i, const std::string& label) const {
-  throw cms::Exception("NotImplemented") <<
-    "The daughterOverlaps functionality is not implemented for instances of" <<
-    " PATMultiCandFinalState." << std::endl;
+  reco::CandidatePtr theCand = cands_.at(i); // will throw OOB exception
+
+  if( dynamic_cast<const pat::Electron*>(theCand.get()) ) {
+    edm::Ptr<pat::Electron> asEle(theCand);
+    return asEle->overlaps(label); 
+  } else if ( dynamic_cast<const pat::Muon*>(theCand.get()) ) {
+    edm::Ptr<pat::Muon> asMuon(theCand);
+    return asMuon->overlaps(label); 
+  } else if ( dynamic_cast<const pat::Tau*>(theCand.get()) ) {
+    edm::Ptr<pat::Tau> asTau(theCand);
+    return asTau->overlaps(label); 
+  } else if ( dynamic_cast<const pat::Photon*>(theCand.get()) ) {
+    edm::Ptr<pat::Photon> asPho(theCand);
+    return asPho->overlaps(label); 
+  } else if ( dynamic_cast<const pat::Jet*>(theCand.get()) ) {
+    edm::Ptr<pat::Jet> asJet(theCand);
+    return asJet->overlaps(label); 
+  } else if ( dynamic_cast<const pat::MET*>(theCand.get()) ) {
+    edm::Ptr<pat::MET> asMET(theCand);
+    return asMET->overlaps(label); 
+  } else {
+    throw cms::Exception("Uncastable") 
+      << "The edm::Ptr at index " << i 
+      << "is not castable to a PAT Object." << std::endl;
+  }
 }
