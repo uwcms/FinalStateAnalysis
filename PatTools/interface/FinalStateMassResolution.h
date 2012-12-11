7f7e4668012caab85e1932a46b156ca587da01d5
Lindsey Gray <lindsey.gray@gmail.com>
2012-12-11 12:43:07 -0600

    work on per-event errors

---
+++ PatTools/interface/FinalStateMassResolution.h
@@ -0,0 +1,73 @@
+/*
+ *
+ * This has been shamelessly stolen and ported to FSA from:
+ * http://cmssw.cvs.cern.ch/cgi-bin/cmssw.cgi/UserCode/CJLST/ZZAnalysis/AnalysisStep/src/CompositeCandMassResolution.cc?revision=1.2&view=markup
+ * Original Authors: Boris Mangano, Stefano Argiro, Christina Botta
+ * Port (and parts for reco::Photon) by: L. Gray (FNAL)
+ *
+ */
+
+#ifndef PatTools_FinalStateMassResolution_h
+#define PatTools_FinalStateMassResolution_h
+
+namespace reco { 
+  class Candidate; 
+  class Muon; 
+  class GsfElectron; 
+  class Track; 
+  class PFCandidate; 
+  class LeafCandidate; 
+}
+namespace edm { class EventSetup; }
+
+#include <vector>
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "MagneticField/Engine/interface/MagneticField.h"
+#include <TMatrixDSym.h>
+
+/* Mutuated from 
+   /UserCode/Mangano/WWAnalysis/AnalysisStep/src/CompositeCandMassResolution.cc
+
+ */
+
+class FinalStateMassResolution  {
+    public:
+        FinalStateMassResolution() {}
+        ~FinalStateMassResolution() {}
+        void init(const edm::EventSetup &iSetup);
+        double getMassResolution(const reco::Candidate &c) const ;
+        double getMassResolutionWithComponents(const reco::Candidate &c, 
+					    std::vector<double> &errI) const;
+    private:
+        void   fillP3Covariance(const reco::Candidate &c, 
+				TMatrixDSym &bigCov, 
+				int offset) const ;
+        void   fillP3Covariance(const reco::GsfElectron &c, 
+				TMatrixDSym &bigCov, 
+				int offset) const ;
+        void   fillP3Covariance(const reco::Muon &c, 
+				TMatrixDSym &bigCov, 
+				int offset) const ;
+        void   fillP3Covariance(const reco::PFCandidate &c, 
+				TMatrixDSym &bigCov, 
+				int offset) const ;
+        void   fillP3Covariance(const reco::Candidate &c, 
+				const reco::Track &t, 
+				TMatrixDSym &bigCov, 
+				int offset) const ;
+	void   fillP3Covariance(const reco::LeafCandidate &c, 
+				TMatrixDSym &bigCov, 
+				int offset) const ;
+
+        edm::ESHandle<MagneticField> magfield_;
+
+        // 1 if this is a lead, recursive number of leafs if composite
+        void getLeaves(const reco::Candidate &c, 
+		       std::vector<const reco::Candidate *> &out) const ;
+
+        double getMassResolution_(const reco::Candidate &c, 
+				  std::vector<double> &errI, 
+				  bool doComponents) const;
+};
+
+#endif
