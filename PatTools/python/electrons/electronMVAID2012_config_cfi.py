572fe6b7b32298748b9590c8a0ec103f45c3184b
Lindsey Gray <lindsey.gray@gmail.com>
2012-12-28 09:41:39 -0600

    fix path names

--- PatTools/python/electrons/electronMVAID2012_config_cfi.py
+++ PatTools/python/electrons/electronMVAID2012_config_cfi.py
@@ -1,20 +1,23 @@
 import FWCore.ParameterSet.Config as cms
 
-mvaTrigIDISOV0 = cms.EDFilter("ElectronIdMVAProducer",
-                              verbose = cms.untracked.bool(False),
-                              vertexTag = cms.InputTag('offlinePrimaryVertices'),
-                              electronTag = cms.InputTag('gsfElectrons'),
-                              reducedEBRecHitCollection = cms.InputTag('reducedEcalRecHitsEB'),
-                              reducedEERecHitCollection = cms.InputTag('reducedEcalRecHitsEE'),
-                              method = cms.string("BDT"),
-                              mvaWeightFile = cms.vstring(
-                     "FinalStateAnalysis/PatTools/data/ElectronIDMVA_Trig_V4_EtaBin0LowPt_V4_BDTG.weights.xml.gz",
-                     "FinalStateAnalysis/PatTools/data/ElectronIDMVA_Trig_V4_EtaBin1LowPt_V4_BDTG.weights.xml.gz",
-                     "FinalStateAnalysis/PatTools/data/ElectronIDMVA_Trig_V4_EtaBin2LowPt_V4_BDTG.weights.xml.gz",
-                     "FinalStateAnalysis/PatTools/data/ElectronIDMVA_Trig_V4_EtaBin0HighPt_V4_BDTG.weights.xml.gz",
-                     "FinalStateAnalysis/PatTools/data/ElectronIDMVA_Trig_V4_EtaBin1HighPt_V4_BDTG.weights.xml.gz",
-                     "FinalStateAnalysis/PatTools/data/ElectronIDMVA_Trig_V4_EtaBin2HighPt_V4_BDTG.weights.xml.gz"
-                            ),
-                            isISISO=cms.untracked.bool(True),
-                            Trig = cms.bool(True),
+weightFileBase = "FinalStateAnalysis/PatTools/data/ElectronMVAWeights/"
+weightFiles = ["ElectronIDMVA_Trig_V4_EtaBin0LowPt_V4_BDTG.weights.xml.gz",
+               "ElectronIDMVA_Trig_V4_EtaBin1LowPt_V4_BDTG.weights.xml.gz",
+               "ElectronIDMVA_Trig_V4_EtaBin2LowPt_V4_BDTG.weights.xml.gz",
+               "ElectronIDMVA_Trig_V4_EtaBin0HighPt_V4_BDTG.weights.xml.gz",
+               "ElectronIDMVA_Trig_V4_EtaBin1HighPt_V4_BDTG.weights.xml.gz",
+               "ElectronIDMVA_Trig_V4_EtaBin2HighPt_V4_BDTG.weights.xml.gz"]
+fullNames = [weightFileBase+file for file in weightFiles]
+
+mvaTrigIDISOV0 = cms.EDFilter(
+    "ElectronIdMVAProducer",
+    verbose = cms.untracked.bool(False),
+    vertexTag = cms.InputTag('offlinePrimaryVertices'),
+    electronTag = cms.InputTag('gsfElectrons'),
+    reducedEBRecHitCollection = cms.InputTag('reducedEcalRecHitsEB'),
+    reducedEERecHitCollection = cms.InputTag('reducedEcalRecHitsEE'),
+    method = cms.string("BDT"),
+    mvaWeightFile = cms.vstring(fullNames),
+    isISISO=cms.untracked.bool(True),
+    Trig = cms.bool(True),
 )
