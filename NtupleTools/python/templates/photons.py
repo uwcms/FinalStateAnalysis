71b20d3bd09a4a7fe17fbb83e62fc80208fa742c
Lindsey Gray <lindsey.gray@gmail.com>
2012-12-28 13:18:08 -0600

    string parser syntax error

--- NtupleTools/python/templates/photons.py
+++ NtupleTools/python/templates/photons.py
@@ -61,7 +61,7 @@ id = PSet(
     # gen matching (photons can be matched to many object types)
     objectPdgId = '? ({object}.genParticleRef().isNonnull && {object}.genParticleRef().isAvailable) ? {object}.getParticleRef().pdgId() : -999',
     objectGenMotherPdgId = '? (getDaughterGenParticleMotherSmart({object_idx}).isAvailable && getDaughterGenParticleMotherSmart({object_idx}).isNonnull) ? getDaughterGenParticleMotherSmart({object_idx}).pdgId() : -999',
-    objectGenGrandMotherPdgId = '? (getDaughterGenParticleMotherSmart({object_idx}).isAvailable && getDaughterGenParticleMotherSmart({object_idx}).isNonnull && getDaughterGenParticleMotherSmart({object_idx}).mother()) ? getDaughterGenParticleMotherSmart({object_idx}).mother().pdgId() : -999',
+    objectGenGrandMotherPdgId = '? (getDaughterGenParticleMotherSmart({object_idx}).isAvailable && getDaughterGenParticleMotherSmart({object_idx}).isNonnull && (getDaughterGenParticleMotherSmart({object_idx}).mother() != 0)) ? getDaughterGenParticleMotherSmart({object_idx}).mother().pdgId() : -999',
     objectComesFromHiggs = 'comesFromHiggs({object_idx})',
     objectGenEnergy = '? ({object}.genParticleRef().isNonnull && {object}.genParticleRef().isAvailable) ? {object}.genParticleRef().energy() : -999',
 )
