06c8fce5db04876fc9bd3b05731e8d5e8319e07b
Mauro Verzetti <mverzett@cern.ch>
2013-01-16 08:56:35 -0600

    added function to load weights from TH2

--- StatTools/python/RooFunctorFromWS.py
+++ StatTools/python/RooFunctorFromWS.py
@@ -42,5 +42,27 @@ def build_roofunctor(filename, wsname, functionname, var='x'):
     ws = file.Get(wsname)
     return RooFunctorFromWS(ws, functionname, var)
 
+def make_corrector_from_th2(filename, path):
+    tfile = ROOT.TFile.Open(filename)
+    if not tfile:
+        raise IOError("Can't open file: %s" % filename)
+    hist  = tfile.Get(path).Clone()
+    #print hist
+    binsx = hist.GetNbinsX()
+    binsy = hist.GetNbinsY()
+    def refFun(xval,yval):
+        #print hist
+        xbin = hist.GetXaxis().FindBin(xval)
+        xbin = (xbin if xbin <= binsx else binsx ) if xbin >= 1 else 1 #Compute underflow and overflow as first and last bin
+        ybin = hist.GetYaxis().FindBin(yval)
+        ybin = (ybin if ybin <= binsy else binsy ) if ybin >= 1 else 1 #Compute underflow and overflow as first and last bin
+        prob = hist.GetBinContent(xbin,ybin)
+        try:
+            return prob 
+        except ZeroDivisionError:
+            raise ZeroDivisionError(" catched trying to return weight for (%.3f,%.3f) ==> (%i,%i) bin out of (%i,%i). Prob: %.3f. Hist: %s : %s. " % (xval, yval, xbin, ybin, binsx, binsy , prob, filename, path))
+    return refFun
+
+
 if __name__ == "__main__":
     import doctest; doctest.testmod()
