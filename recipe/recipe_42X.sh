5d68b1447acd77b978e474d1de4023a6e695d1a7
Evan Friis <ekfriis@gmail.com>
2013-02-13 08:56:03 -0600

    Always checkout the full PAT recipe, even when PATPROD=0
    Otherwise you get segfaults when using things like PATMuonSelector.

--- recipe/recipe_42X.sh
+++ recipe/recipe_42X.sh
@@ -6,6 +6,19 @@ pushd $CMSSW_BASE/src
 
 # Always need these
 addpkg DataFormats/PatCandidates  V06-04-19-05
+# PAT RECIPE V08-06-58 IAR 27.Sep.2012
+# patch in 44X electrons so regression BS works
+cvs up -r 1.44 DataFormats/PatCandidates/interface/Electron.h
+cvs up -r 1.33 DataFormats/PatCandidates/src/Electron.cc
+addpkg PhysicsTools/PatAlgos V08-06-58
+addpkg PhysicsTools/PatUtils V03-09-18
+addpkg CommonTools/ParticleFlow B4_2_X_V00-03-05
+addpkg PhysicsTools/SelectorUtils V00-03-24
+addpkg PhysicsTools/UtilAlgos V08-02-14 
+# Remove this junky MHT package, we don't need it and it causes
+# link errors with the MVAMET
+rm -f PhysicsTools/PatAlgos/plugins/PATMHTProducer*
+
 
 if [ "$LIMITS" = "1" ]
 then
@@ -17,20 +30,6 @@ fi
 
 if [ "$PATPROD" = "1" ]
 then
-
-  # PAT RECIPE V08-06-58 IAR 27.Sep.2012
-  #addpkg DataFormats/PatCandidates  V06-04-19-05
-  # patch in 44X electrons so regression BS works
-  cvs up -r 1.44 DataFormats/PatCandidates/interface/Electron.h
-  cvs up -r 1.33 DataFormats/PatCandidates/src/Electron.cc
-  addpkg PhysicsTools/PatAlgos V08-06-58
-  addpkg PhysicsTools/PatUtils V03-09-18
-  addpkg CommonTools/ParticleFlow B4_2_X_V00-03-05
-  addpkg PhysicsTools/SelectorUtils V00-03-24
-  addpkg PhysicsTools/UtilAlgos V08-02-14 
-  # Remove this junky MHT package, we don't need it and it causes
-  # link errors with the MVAMET
-  rm -f PhysicsTools/PatAlgos/plugins/PATMHTProducer*
 
   #Update to calculate Single Tower H/E in 42X
   #https://twiki.cern.ch/twiki/bin/view/CMS/HoverE2012
