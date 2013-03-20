8cdd1c60f22e71ed797dc0a54e189bc062821b01
Austin Belknap <dabelknap@wisc.edu>
2013-03-20 09:19:19 -0500

    Added RelPFIsoRho for muons

--- NtupleTools/python/templates/muons.py
+++ NtupleTools/python/templates/muons.py
@@ -37,6 +37,13 @@ id = PSet(
         "-0.5*{object}.puChargedHadronIso,0.0))"
         "/{object}.pt()"
     ),
+    objectRelPFIsoRho = cms.string(
+        '({object}.chargedHadronIso()'
+        '+max(0.0,{object}.neutralHadronIso()'
+        '+{object}.photonIso()'
+        '-{object}.userFloat("zzRho2012")*{object}.userFloat("ea_comb_iso04_kt6PFJCNth05")))'
+        '/{object}.pt()'
+    ),
 
     ## objectRelPFIsoDBZhLike = cms.string(
     ##     "({object}.chargedHadronIso()"
