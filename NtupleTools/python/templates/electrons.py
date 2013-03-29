b0653c6c150906d80ef513157c7b86fc5cc1fee0
Austin Belknap <dabelknap@wisc.edu>
2013-03-29 11:05:38 -0500

    Added FSR isolation corrections to lepton templates

--- NtupleTools/python/templates/electrons.py
+++ NtupleTools/python/templates/electrons.py
@@ -43,6 +43,13 @@ id = PSet(
         '-{object}.userFloat("zzRho2012")*{object}.userFloat("ea_comb_Data2012_iso04_kt6PFJ")))'
         '/{object}.pt()'
     ),
+    objectRelPFIsoRhoFSR = cms.string(
+        '({object}.chargedHadronIso()'
+        '+max(0.0,{object}.neutralHadronIso()'
+        '+{object}.photonIso() - userFloat("leg{object_idx}fsrIsoCorr")'
+        '-{object}.userFloat("zzRho2012")*{object}.userFloat("ea_comb_Data2012_iso04_kt6PFJ")))'
+        '/{object}.pt()'
+    ),
     objectPFChargedIso = cms.string('{object}.userIsolation("PfChargedHadronIso")'),
     objectPFNeutralIso = cms.string('{object}.userIsolation("PfNeutralHadronIso")'),
     objectPFPhotonIso  = cms.string('{object}.userIsolation("PfGammaIso")'),
