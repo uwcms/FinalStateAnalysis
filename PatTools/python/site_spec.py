0245d0a887a5705c942859e9061a4ac31d72415e
Lindsey Gray <lindsey.gray@gmail.com>
2013-04-09 06:26:39 -0500

    proper submit dir for FNAL

--- PatTools/python/site_spec.py
+++ PatTools/python/site_spec.py
@@ -11,7 +11,7 @@ output_dir_srm  = 'cmssrm.hep.wisc.edu:8443/srm/v2/server?SFN='
 if 'fnal.gov' in _host_name:
     output_dir_srm  = 'cmssrm.fnal.gov:8443/srm/managerv2?SFN='
     output_dir_root = '/11/store/user/%s'%_log_name
-    submit_dir_root = '/storage/local/data1/condor/execute'
+    submit_dir_root = '/storage/local/data1/condor/execute/%'_log_name
 
 if 'hep.wisc.edu' in _host_name and 'lgray' in _log_name:
     output_dir_srm  = 'cmssrm.fnal.gov:8443/srm/managerv2?SFN='
