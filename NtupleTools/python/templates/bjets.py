c561cd86de0e0d1a5d4e2d9e1390f51e9017c8f6
maria <cepeda@cern.ch>
2013-05-22 05:17:16 -0500

    Test

---
+++ NtupleTools/python/templates/bjets.py
@@ -0,0 +1,38 @@
+'''
+
+Ntuple branch template sets for tau objects.
+
+Each string is transformed into an expression on a FinalStateEvent object.
+
+{object} should be replaced by an expression which evaluates to a pat::Muon
+i.e. daughter(1) or somesuch.
+
+Author: Evan K. Friis
+
+'''
+
+from FinalStateAnalysis.Utilities.cfgtools import PSet
+
+btagging = PSet(
+	#Btagging
+        objectSSVHEBtag = '{object}.bDiscriminator("simpleSecondaryVertexHighEffBJetTags")',
+	objectSSVHPBtag = '{object}.bDiscriminator("simpleSecondaryVertexHighPurBJetTags")',
+        objectCSVBtag ='{object}.bDiscriminator("combinedSecondaryVertexBJetTags")',
+
+	#Custom Btagging, based on SV
+        objectSVMassBtag ='{object}.userFloat("mass_SSV")',
+	objectSVChargeBtag ='{object}.userFloat("chargeSSV")',
+	objectSVNtracksBtag ='{object}.userFloat("nTracksSSV")',
+        objectNSVsBtag ='{object}.userFloat("nTracksSSV")',
+	objectSVFlightDistanceBtag ='{object}.userFloat("flightDistance")',
+        objectSVErrFlightDistanceBtag ='{object}.userFloat("errorFlightDistance")',
+
+	#Custom Btagging, based on Muons-In-Jets
+	objectMuonInJetPtBtag ='{object}.userFloat("MuonInJetPt")',
+        objectMuonInJetPtRelBtag ='{object}.userFloat("MuonInJetPtRel")',
+
+	#Flavour
+	objectsJetFlavour ='{object}.partonFlavour()',
+)
+
+
