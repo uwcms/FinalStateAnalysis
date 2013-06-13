face98e7e3469d453640c7ac56aaac110f8a5378
Evan Friis <ekfriis@gmail.com>
2013-06-13 10:16:38 -0500

    Remove no-PAT protection, we use nuke_pat now

--- PatTools/python/jets/patJetPUId_cfi.py
+++ PatTools/python/jets/patJetPUId_cfi.py
@@ -9,14 +9,10 @@ See: https://twiki.cern.ch/twiki/bin/view/CMS/PileupJetID
 import FWCore.ParameterSet.Config as cms
 import sys
 
-try:
-    from RecoJets.JetProducers.PileupJetID_cfi import pileupJetIdProducer
-    # The path to the residuals must exist, even if we don't use it.
-    pileupJetIdProducer.residualsTxt = \
+from RecoJets.JetProducers.PileupJetID_cfi import pileupJetIdProducer
+# The path to the residuals must exist, even if we don't use it.
+pileupJetIdProducer.residualsTxt = \
         "FinalStateAnalysis/PatTools/data/readme.txt"
-except ImportError:
-    sys.stderr.write(__file__ +
-                     ": PU Jet ID dependency not installed, will not run!\n")
 
 # Module to embed the IDs
 patJetsPUID = cms.EDProducer(
