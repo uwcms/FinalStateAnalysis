6ec8620e05cfcb4e26683beeb2e617dc8a0d631d
Mauro Verzetti <mverzett@cern.ch>
2013-10-28 04:45:09 -0500

    allow regexes, add fitting tools etc..

--- PlotTools/python/Plotter.py
+++ PlotTools/python/Plotter.py
@@ -7,11 +7,14 @@ Author: Evan K. Friis, UW
 '''
 
 import fnmatch
+import re
 import os
 import rootpy.plotting.views as views
 import rootpy.plotting as plotting
 from FinalStateAnalysis.MetaData.data_views import data_views
 from FinalStateAnalysis.PlotTools.RebinView import RebinView
+from FinalStateAnalysis.Utilities.struct import struct
+import FinalStateAnalysis.Utilities.prettyjson as prettyjson
 import ROOT
 
 _original_draw = plotting.Legend.Draw
@@ -56,12 +59,51 @@ class Plotter(object):
             'ZZ*',
             'WW*',
         ]
+        file_to_map = filter(lambda x: x.startswith('data_'), self.views.keys())[0]
+        if not file_to_map: #no data here!
+            file_to_map = self.views.keys()[0]
+        #from pdb import set_trace; set_trace()
+        self.file_dir_structure = Plotter.map_dir_structure( self.views[file_to_map]['file'] )
+
+    @staticmethod
+    def map_dir_structure(directory, dirName=''):
+        objects = [(i.GetName(), i.GetClassName()) for i in directory.GetListOfKeys()]
+        ret     = []
+        for keyname, keyclass in objects:
+            if keyclass.startswith('TDirectory'):
+                subdirName = os.path.join(dirName,keyname)
+                ret.append(subdirName)
+                ret.extend(Plotter.map_dir_structure(directory.Get(keyname), subdirName))
+        return ret
 
     @staticmethod
     def rebin_view(x, rebin):
         ''' Make a view which rebins histograms '''
         output = RebinView(x, rebin)
         return output
+
+    @staticmethod
+    def parse_formula(fcn_string, pars_string):
+        pars       = []
+        formula    = fcn_string
+        for par_num, match in enumerate(re.finditer("(?P<name>\w+)(?P<boundaries>\[[^\]]+\]),? ?", pars_string)):
+            par        = struct()
+            par.num    = par_num
+            par.name   = match.group('name')
+            par.bounds = eval( match.group('boundaries') )
+            formula    = formula.replace(par.name, '[%i]' % par.num)
+            pars.append(par)
+
+        ret = ROOT.TF1('ret', formula, 0, 200)
+        for par in pars:
+            ret.SetParName(par.num, par.name)
+            if len(par.bounds) == 1:
+                ret.FixParameter(par.num, par.bounds[0])
+            else:
+                ret.SetParameter(par.num, par.bounds[0])
+                ret.SetParLimits(par.num, par.bounds[1], par.bounds[2])
+        return ret
+
 
     def get_view(self, sample_pattern, key_name='view'):
         ''' Get a view which matches a pattern like "Zjets*"
@@ -79,7 +121,7 @@ class Plotter(object):
         raise KeyError("I can't find a view that matches %s, I have: %s" % (
             sample_pattern, " ".join(self.views.keys())))
 
-    def make_stack(self, rebin=1, preprocess=None):
+    def make_stack(self, rebin=1, preprocess=None, folder=''):
         ''' Make a stack of the MC histograms '''
         
         mc_views = []
@@ -87,6 +129,8 @@ class Plotter(object):
             mc_view = self.get_view(x)
             if preprocess:
                 mc_view = preprocess(mc_view)
+            if folder:
+                mc_view = self.get_wild_dir(mc_view, folder)
             mc_views.append(
                 self.rebin_view(mc_view, rebin)
                 )
@@ -140,7 +184,11 @@ class Plotter(object):
         self.lower_pad.Draw()
         self.lower_pad.cd()
         
-        mc_hist    = sum(mc_stack.GetHists())
+        mc_hist    = None
+        if isinstance(mc_stack, plotting.HistStack):
+            mc_hist = sum(mc_stack.GetHists())
+        else:
+            mc_hist = mc_stack
         data_clone = data_hist.Clone()
         if not x_range:
             nbins = data_clone.GetNbinsX()
@@ -152,11 +200,64 @@ class Plotter(object):
         
         data_clone.Divide(mc_hist)
         data_clone.Draw()
-        data_clone.GetYaxis().SetRangeUser(1-ratio_range, 1+ratio_range)
+        if ratio_range:
+            data_clone.GetYaxis().SetRangeUser(1-ratio_range, 1+ratio_range)
         ref_function.Draw('same')
         self.keep.append(data_clone)
         self.keep.append(ref_function)
-        self.pad.cd()        
+        self.pad.cd()
+        return data_clone
+
+    def fit_shape(self, histo, model, x_range, fitopt='IRMENS'):
+        tf1 = self.parse_formula(*model) 
+        tf1.SetRange(*x_range)
+        tf1.SetLineColor(ROOT.EColor.kAzure)
+        tf1.SetLineWidth(3)
+        result = histo.Fit(tf1, fitopt) #WL
+        # "WL" Use Loglikelihood method and bin contents are not integer,
+        #               i.e. histogram is weighted (must have Sumw2() set)
+        # "Q"  Quiet mode (minimum printing)
+        # "E"  Perform better Errors estimation using Minos technique
+        # "M"  More. Improve fit results.
+        #      It uses the IMPROVE command of TMinuit (see TMinuit::mnimpr).
+        #      This algorithm attempts to improve the found local minimum by searching for a
+        #      better one.
+        # "R"  Use the Range specified in the function range
+        # "N"  Do not store the graphics function, do not draw
+        # "S"  The result of the fit is returned in the TFitResultPtr
+        numpoints = tf1.GetNpx() #number of points in which the func is evaluated
+        func_hist = plotting.Hist(numpoints, *x_range)
+        (ROOT.TVirtualFitter.GetFitter()).GetConfidenceIntervals(func_hist)
+        func_hist.linewidth = 0
+        func_hist.fillcolor = ROOT.EColor.kAzure - 9
+        func_hist.fillstyle = 3013
+        func_hist.markersize = 0
+        func_hist.Draw('same e3')
+        tf1.Draw('same')
+        self.keep.extend([
+            tf1,
+            func_hist
+        ])
+        return tf1
+
+    def make_text_box(self, text, position='top-right'):
+        look_up_positions = {
+            'top-right'    : (0.73, 0.67, 0.96, 0.92),
+            'top-left'     : (0.16, 0.67, 0.39, 0.92),
+            'bottom-right' : (0.73, 0.15, 0.96, 0.4),
+            'bottom-left'  : (0.16, 0.15, 0.39, 0.4),
+            }
+        p = look_up_positions[position] if isinstance(position, str) \
+                         else position
+        stat_box = ROOT.TPaveText(p[0], p[1], p[2], p[3], 'NDC')
+        for line in text.split('\n'):
+            print line
+            stat_box.AddText(line)
+        
+        #Set some graphics options not to suck
+        stat_box.SetFillColor(0)
+        stat_box.SetBorderSize(1)
+        return stat_box
 
     def reset(self):
         '''hard graphic reset'''
@@ -171,7 +272,7 @@ class Plotter(object):
         self.pad.cd()
         self.lower_pad = None
 
-    def save(self, filename, dotc=False, dotroot=False, verbose=False):
+    def save(self, filename, dotc=False, dotroot=False, json=False, verbose=False):
         ''' Save the current canvas contents to [filename] '''
         self.pad.Draw()
         self.canvas.Update()
@@ -183,6 +284,17 @@ class Plotter(object):
         self.canvas.SaveAs(os.path.join(self.outputdir, filename) + '.pdf')
         if dotc:
             self.canvas.SaveAs(os.path.join(self.outputdir, filename) + '.C')
+        if json:
+            jdict = {}
+            for obj in self.keep:
+                if isinstance(obj, ROOT.TH1):
+                    jdict[obj.GetTitle()] = [obj.GetBinContent(1), obj.GetBinError(1)] 
+                if isinstance(obj, ROOT.THStack):
+                    jdict['hist_stack'] = {}
+                    for i in obj.GetHists():
+                        jdict['hist_stack'][i.GetTitle()] = [i.GetBinContent(1), i.GetBinError(1)]
+            with open(os.path.join(self.outputdir, filename) + '.json', 'w') as jout:
+                jout.write(prettyjson.dumps(jdict))
         if dotroot:
             outfile = ROOT.TFile.Open(os.path.join(self.outputdir, filename) + '.root', 'recreate')
             outfile.cd()
@@ -243,12 +355,34 @@ class Plotter(object):
             1.2*max(histo1.GetMaximum(), histo2.GetMaximum()))
         self.keep.append( (histo1, histo2) )
 
+    def expand_path(self, pattern):
+        #because fnmatch does not treat / properly and may generate LOTS of problems
+        if any((i in pattern) for i in ['*', '?']):
+            repattern = re.compile('^'+pattern.replace('*','[^/]*').replace('?','[^/]')+'$')
+            return [i for i in self.file_dir_structure if repattern.match(i)]
+        else:
+            return [pattern]
+
+    def get_wild_dir(self, view, path):
+        'equivalent of SubdirectoryView, but with unix-style wildcards'
+        paths = self.expand_path(path)
+        return views.SumView(
+            *[ views.SubdirectoryView(view, i) for i in paths]
+            )
+
+    def get_wild_path(self, view, path):
+        'gets a FULL path with wildcards in it. By full it is intended till the histogram to be picked'
+        base_name = os.path.basename(path)
+        dir_name  = os.path.dirname(path)
+        return self.get_wild_dir(view, dir_name).Get(base_name)
+
     def plot_mc_vs_data(self, folder, variable, rebin=1, xaxis='',
                         leftside=True, xrange=None, preprocess=None,
                         show_ratio=False, ratio_range=0.2):
         ''' Compare Monte Carlo to data '''
-        path = os.path.join(folder, variable)
-        mc_stack = self.make_stack(rebin, preprocess).Get(path)
+        #path = os.path.join(folder, variable)
+        mc_stack_view = self.make_stack(rebin, preprocess, folder)
+        mc_stack = mc_stack_view.Get(variable)
         mc_stack.Draw()
         mc_stack.GetHistogram().GetXaxis().SetTitle(xaxis)
         if xrange:
@@ -259,14 +393,17 @@ class Plotter(object):
         data_view = self.get_view('data')
         if preprocess:
             data_view = preprocess( data_view )
-        data_view = self.rebin_view(data_view, rebin)
-        data = data_view.Get(path)
+        data_view = self.get_wild_dir(
+            self.rebin_view(data_view, rebin),
+            folder
+            )
+        data = data_view.Get(variable)
         data.Draw('same')
         self.keep.append(data)
         # Make sure we can see everything
         if data.GetMaximum() > mc_stack.GetMaximum():
             mc_stack.SetMaximum(1.2*data.GetMaximum())
         # Add legend
-        self.add_legend([data, mc_stack], leftside, entries=5)
+        self.add_legend([data, mc_stack], leftside, entries=len(mc_stack.GetHists())+1)
         if show_ratio:
             self.add_ratio_plot(data, mc_stack, xrange, ratio_range=0.2)
