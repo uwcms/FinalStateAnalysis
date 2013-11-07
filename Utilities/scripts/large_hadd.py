1525f9313eebc5b15d9d891df37a04e1f697f01d
Austin Belknap <dabelknap@wisc.edu>
2013-11-07 08:13:27 -0600

    Add doc strings and remove intermediate files

--- Utilities/scripts/large_hadd.py
+++ Utilities/scripts/large_hadd.py
@@ -5,11 +5,18 @@ import os
 import argparse
 
 def hadd(outfile, infiles):
+    """This makes a simple call to hadd"""
     cmd = "hadd " + outfile + " " + " ".join(infiles)
     os.system(cmd)
 
 
 def batch_hadd(outfile, infiles, n_files=200):
+    """
+    This runs hadd on smaller chunks of files into intermediate files. Then
+    hadd is run to merge the intermidate files into the final file.
+    """
+
+    # split the list of input files into sublists
     split_infiles = [infiles[x:x+n_files]
                      for x in xrange(0, len(infiles), n_files)]
 
@@ -18,15 +25,18 @@ def batch_hadd(outfile, infiles, n_files=200):
     user_name = os.environ['USER']
 
     for i in xrange(len(split_infiles)):
+        # Use unique names for the intermediate files to avoid collisions
         intm_file = ("/tmp/tmp_" + user_name + "_" + str(i) + "_" +
                      os.path.basename(outfile))
         print intm_file
         intermediate_files.append(intm_file)
         hadd(intm_file, split_infiles[i])
 
+    # merge the intermediate files together
     hadd(outfile, intermediate_files)
 
-    #os.system("rm " + " ".join(intermediate_files))
+    # remove the intermediate files
+    os.system("rm " + " ".join(intermediate_files))
 
 
 def parse_command_line(argv):
