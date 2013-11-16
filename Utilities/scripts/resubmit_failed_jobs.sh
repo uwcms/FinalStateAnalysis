4790acd3c58a22177be345d459cd1ccdfdb9f4a2
Austin Belknap <dabelknap@wisc.edu>
2013-11-16 12:42:01 -0600

    Add resubmit failed jobs script

---
+++ Utilities/scripts/resubmit_failed_jobs.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ $# -lt 1 ]
+then
+    echo "Usage: resubmit_failed_jobs.sh JOB_ID"
+    exit 1
+fi
+
+JOBID=$1
+SCRATCH_PATH=/scratch/$USER/$JOBID
+
+ls $SCRATCH_PATH/*/dags/* | xargs grep -lir ERR | sed -e "s|status|rescue001|" | xargs -I{} -n 1 farmoutAnalysisJobs --rescue-dag-file={}
