50e59d311dba0e76001fdfb6ce9282adf970e60f
Mauro Verzetti <mverzett@cern.ch>
2013-12-09 05:04:50 -0600

    refactored the code. Same effect but now you can apply the static method directly to the histogram

--- PlotTools/python/MedianView.py
+++ PlotTools/python/MedianView.py
@@ -10,21 +10,26 @@ class MedianView(object):
         self.lowv  = lowv
         self.centv = views.ScaleView( views.SumView(lowv, self.highv) , 0.5) if not centv else centv
 
-    def Get(self, path):
-        central_hist = self.centv.Get(path)
-        high_hist    = self.highv.Get(path) if self.highv else None
-        low_hist     = self.lowv.Get(path)  if self.lowv  else None
+    @staticmethod
+    def apply_view(central_hist, high_hist, low_hist=None):
         ret_hist = central_hist.Clone()
-        
         for bin in range(1, ret_hist.GetNbinsX() + 1):
             error = quad(
                 central_hist.GetBinError(bin),
-                (high_hist.GetBinContent(bin) - central_hist.GetBinContent(bin))
+                abs(high_hist.GetBinContent(bin) - central_hist.GetBinContent(bin))
             ) if high_hist else \
             quad(
                 central_hist.GetBinError(bin),
-                (central_hist.GetBinContent(bin) - low_hist.GetBinContent(bin))
+                abs(central_hist.GetBinContent(bin) - low_hist.GetBinContent(bin))
             ) 
             
             ret_hist.SetBinError(bin, error)
         return ret_hist
+        
+
+    def Get(self, path):
+        central_hist = self.centv.Get(path)
+        high_hist    = self.highv.Get(path) if self.highv else None
+        low_hist     = self.lowv.Get(path)  if self.lowv  else None
+
+        return self.apply_view(central_hist, high_hist, low_hist)
