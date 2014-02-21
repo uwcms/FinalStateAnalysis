68eef0ba5608e44c5024675f67758a7334afec25
Edward Laird <elaird@gmail.com>
2014-02-21 04:35:26 -0600

    string comparison

--- recipeGIT/recipe_mvamet.sh
+++ recipeGIT/recipe_mvamet.sh
@@ -6,14 +6,9 @@ set -o errexit
 set -o nounset
 
 : ${CMSSW_BASE:?"CMSSW_BASE is not set! Run cmsenv before recipe.sh"}
-
-# Check CMSSW version
-MAJOR_VERSION=`echo $CMSSW_VERSION | sed "s|CMSSW_\([0-9]\)_.*|\1|"`
-MINOR_VERSION=`echo $CMSSW_VERSION | sed "s|CMSSW_\([0-9]\)_\([0-9]\)_.*|\2|"`
-
 pushd $CMSSW_BASE/src
 
-if [ "$MAJOR_VERSION" -eq "5" ]; then
+if [ "$CMSSW_VERSION" == "CMSSW_5_3_14" ]; then
   # https://hypernews.cern.ch/HyperNews/CMS/get/met/333.html
   git cms-addpkg PhysicsTools/PatAlgos
   git cms-merge-topic cms-analysis-tools:5_3_14-updateSelectorUtils
@@ -21,7 +16,7 @@ if [ "$MAJOR_VERSION" -eq "5" ]; then
   git cms-merge-topic -u TaiSakuma:53X-met-131120-01
   git-cms-merge-topic -u cms-met:53X-MVaNoPuMET-20131217-01
 else
-  echo "Sorry, 42X not ported yet. For 7 TeV, we should move to the rereco"
+  echo "MVAMET recipe not available for releases other than 5_3_14."
 fi
 
 popd
