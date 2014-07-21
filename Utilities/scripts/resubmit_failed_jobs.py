9ba7ee88b67c9833a998dd04ab975a2d13a2b547
Austin Belknap <dabelknap@wisc.edu>
2014-07-21 10:54:18 -0500

    Add comments

--- Utilities/scripts/resubmit_failed_jobs.py
+++ Utilities/scripts/resubmit_failed_jobs.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+"""
+Allows the resubmission of failed Condor jobs provided they have a rescue dag
+file.
+
+Author: D. Austin Belknap, UW-Madison
+"""
 
 import os
 import re
@@ -8,6 +14,10 @@ import argparse
 
 
 def submit_jobid(jobid):
+    """
+    Scan through the samples of a given job id, and check the dag status file
+    for failed jobs. If any, submit the rescue dag files to farmoutAnalysisJobs
+    """
     user = os.environ['USER']
     scratch = '/nfs_scratch'
 
@@ -16,32 +26,37 @@ def submit_jobid(jobid):
     samples = glob.glob('%s/*' % path)
 
     for s in samples:
+        # FSA ntuples and PAT tuples use a different naming convention for the
+        # status dag files. Try both.
         status_dag1 = '%s/dags/dag.status' % s
         status_dag2 = '%s/dags/dag.dag.status' % s
+
+        # look for failed jobs
         errors = []
         try:
             with open(status_dag1, 'r') as dagfile:
-                errors = [True and re.search('ERROR', line) for line in dagfile]
+                errors = [re.search('ERROR', line) for line in dagfile]
         except IOError:
             try:
                 with open(status_dag2, 'r') as dagfile:
-                    errors = [True and re.search('ERROR', line) for line in dagfile]
+                    errors = [re.search('ERROR', line) for line in dagfile]
             except IOError:
                 print "Skipping: %s" % s
                 continue
 
+        # if there are any errors, submit the rescue dag files
         if any(errors):
             rescue_dags = glob.glob('%s/dags/*dag.rescue[0-9][0-9][1-9]' % s)
             for dag in rescue_dags:
                 cmd = 'farmoutAnalysisJobs --rescue-dag-file=%s' % dag
-                print cmd
                 os.system(cmd)
 
 
 def parse_command_line(argv):
-    parser = argparse.ArgumentParser(description='')
+    parser = argparse.ArgumentParser(description='Resubmit failed Condor jobs')
 
-    parser.add_argument('jobid', type=str, help='')
+    parser.add_argument('jobid', type=str, help='Provide the FSA job ID the
+            original jobs were run with')
 
     args = parser.parse_args(argv)
 
