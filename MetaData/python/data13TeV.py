700609791c41ad8d56fcf5130ab710abf6ef9bed
Nate Woods <nwoods@hep.wisc.edu>
2014-08-29 10:36:35 -0500

    Add more ZZ samples

--- MetaData/python/data13TeV.py
+++ MetaData/python/data13TeV.py
@@ -2,46 +2,60 @@
 Data definitions for 13 TeV samples.
 '''
 
-from datacommon import square, cube, quad, picobarns, br_w_leptons, br_z_leptons, query_cli
+from datacommon import square, cube, quad, picobarns, br_w_leptons, br_z_leptons, query_cli, picobarns
 
 data_name_map = {}
 
 datadefs = {}
 
 datadefs["Zjets_M50"] = {
-    'analyses': [],
+    'analyses': ['4L'],
     'datasetpath': '/DYJetsToLL_M-50_13TeV-pythia6/Spring14miniaod-PU20bx25_POSTLS170_V5-v1/MINIAODSIM',
     'pu': '20bx25',
     'calibrationTarget': 'Spring14miniaod',
-    'x_sec': -999,
+    'x_sec': 6025.2*picobarns,
 }
 datadefs["Wjets"] = {
     'analyses': [],
     'datasetpath': '/WJetsToLNu_13TeV-madgraph-pythia8-tauola/Spring14miniaod-PU20bx25_POSTLS170_V5-v1/MINIAODSIM',
     'pu': '20bx25',
     'calibrationTarget': 'Spring14miniaod',
-    'x_sec': -999,
+    'x_sec': 20508.9*picobarns,
 }
 datadefs["TTjets"] = {
-    'analyses': [],
+    'analyses': ['4L'],
     'datasetpath': '/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/Spring14miniaod-PU20bx25_POSTLS170_V5-v2/MINIAODSIM',
     'pu': '20bx25',
     'calibrationTarget': 'Spring14miniaod',
-    'x_sec': -999,
+    'x_sec': 689.1*picobarns, # might be 809.1, I'm not sure. See https://twiki.cern.ch/twiki/bin/viewauth/CMS/StandardModelCrossSectionsat13TeV
 }
 datadefs["ggHZZ"] = {
-    'analyses': [],
+    'analyses': ['4L'],
     'datasetpath': '/GluGluToHToZZTo4L_M-125_13TeV-powheg-pythia6/Spring14miniaod-PU20bx25_POSTLS170_V5-v1/MINIAODSIM',
     'pu': '20bx25',
     'calibrationTarget': 'Spring14miniaod',
-    'x_sec': -999,
+    'x_sec': 43.62*picobarns*0.00133, # xsec * br. ratio. BR for L=e,mu, might need to use BR with tau? (=0.000294)
 }
 datadefs["VBFHZZ"] = {
-    'analyses': [],
+    'analyses': ['4L'],
     'datasetpath': '/VBF_HToZZTo4L_M-125_13TeV-powheg-pythia6/Spring14miniaod-PU20bx25_POSTLS170_V5-v1/MINIAODSIM',
     'pu': '20bx25',
     'calibrationTarget': 'Spring14miniaod',
-    'x_sec': -999,
+    'x_sec': 3.72*picobarns*0.00133, # xsec * br. ratio. BR for L=e,mu, might need to use BR with tau? (=0.000294)
+}
+datadefs["DYJetsToLL_M-50"] = {
+    'analyses' : ['4L'],
+    'datasetpath' : '/DYJetsToLL_M-50_13TeV-madgraph-pythia8-tauola_v2/Spring14miniaod-PU20bx25_POSTLS170_V5-v1/MINIAODSIM',
+    'pu' : '20bx25',
+    'calibrationTarget' : 'Spring14miniaod',
+    'x_sec' : 6025.2*picobarns,
+}
+datadefs["ZZJetsTo4L"] = {
+    'analyses' : ['4L'],
+    'datasetpath' : '/ZZTo4L_Tune4C_13TeV-powheg-pythia8/Spring14miniaod-PU20bx25_POSTLS170_V5-v1/MINIAODSIM',
+    'pu' : '20bx25',
+    'calibrationTarget' : 'Spring14miniaod',
+    'xsec' : 15.4*picobarns,
 }
 datadefs["Bprime_M1000"] = {
     'analyses': [],
