762c89ea7f27b643a84dd23ef9f5f26a7e26d320
Devin Taylor <dntaylor@wisc.edu>
2014-09-19 05:37:37 -0500

    adding nates smaples back in

--- MetaData/python/data13TeV.py
+++ MetaData/python/data13TeV.py
@@ -9,25 +9,46 @@ data_name_map = {}
 datadefs = {}
 
 datadefs["Zjets_M50-PU20bx25"] = {
-    'analyses': [],
+    'analyses': ['4L'],
     'datasetpath': '/DYJetsToLL_M-50_13TeV-madgraph-pythia8-tauola_v2/Spring14miniaod-PU20bx25_POSTLS170_V5-v1/MINIAODSIM',
     'pu': '20bx25',
     'calibrationTarget': 'Spring14miniaod',
-    'x_sec': -999,
+    'x_sec': 6025.2*picobarns,
+}
+datadefs["Wjets-PU20bx25"] = {
+    'analyses': [],
+    'datasetpath': '/WJetsToLNu_13TeV-madgraph-pythia8-tauola/Spring14miniaod-PU20bx25_POSTLS170_V5-v1/MINIAODSIM',
+    'pu': '20bx25',
+    'calibrationTarget': 'Spring14miniaod',
+    'x_sec': 20508.9*picobarns,
 }
 datadefs["TTjets-PU20bx25"] = {
-    'analyses': [],
+    'analyses': ['4L'],
     'datasetpath': '/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/Spring14miniaod-PU20bx25_POSTLS170_V5-v2/MINIAODSIM',
     'pu': '20bx25',
     'calibrationTarget': 'Spring14miniaod',
-    'x_sec': -999,
+    'x_sec': 689.1*picobarns, # might be 809.1, I'm not sure. See https://twiki.cern.ch/twiki/bin/viewauth/CMS/StandardModelCrossSectionsat13TeV
+}
+datadefs["ggHZZ-PU20bx25"] = {
+    'analyses': ['4L'],
+    'datasetpath': '/GluGluToHToZZTo4L_M-125_13TeV-powheg-pythia6/Spring14miniaod-PU20bx25_POSTLS170_V5-v1/MINIAODSIM',
+    'pu': '20bx25',
+    'calibrationTarget': 'Spring14miniaod',
+    'x_sec': 43.62*picobarns*0.00133, # xsec * br. ratio. BR for L=e,mu, might need to use BR with tau? (=0.000294)
+}
+datadefs["VBFHZZ-PU20bx25"] = {
+    'analyses': ['4L'],
+    'datasetpath': '/VBF_HToZZTo4L_M-125_13TeV-powheg-pythia6/Spring14miniaod-PU20bx25_POSTLS170_V5-v1/MINIAODSIM',
+    'pu': '20bx25',
+    'calibrationTarget': 'Spring14miniaod',
+    'x_sec': 3.72*picobarns*0.00133, # xsec * br. ratio. BR for L=e,mu, might need to use BR with tau? (=0.000294)
 }
 datadefs["ZZTo4L-PU20bx25"] = {
-    'analyses': [],
+    'analyses': ['4L'],
     'datasetpath': '/ZZTo4L_Tune4C_13TeV-powheg-pythia8/Spring14miniaod-PU20bx25_POSTLS170_V5-v1/MINIAODSIM',
     'pu': '20bx25',
     'calibrationTarget': 'Spring14miniaod',
-    'x_sec': -999,
+    'x_sec': 15.4*picobarns,
 }
 datadefs["T_tW-PU20bx25"] = {
     'analyses': [],
