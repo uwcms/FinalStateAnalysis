69ba30215ab45a3939d20ec341d27cb48cf27696
Austin Belknap <austin.belknap@gmail.com>
2014-11-22 22:07:07 -0600

    Update SRM server path

--- PatTools/python/site_spec.py
+++ PatTools/python/site_spec.py
@@ -6,7 +6,7 @@ _log_name  = os.environ['LOGNAME']
 
 submit_dir_root = '/nfs_scratch/%s'%_log_name
 output_dir_root = '/hdfs/store/user/%s'%_log_name
-output_dir_srm  = 'cmssrm.hep.wisc.edu:8443/srm/v2/server?SFN='
+output_dir_srm  = 'cmssrm2.hep.wisc.edu:8443/srm/v2/server?SFN='
 
 if 'fnal.gov' in _host_name:
     output_dir_srm  = 'cmssrm.fnal.gov:8443/srm/managerv2?SFN='
