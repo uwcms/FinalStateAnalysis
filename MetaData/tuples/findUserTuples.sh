9ac62636b5f697927362e4d8be038d6a7abeb360
Nick Smith <ncsmith6@wisc.edu>
2015-02-06 11:53:25 -0600

    Updated findUserTuples tool to proper json (EXCEPT LAST COMMA, BUYER BEWARE)

--- MetaData/tuples/findUserTuples.sh
+++ MetaData/tuples/findUserTuples.sh
@@ -7,4 +7,4 @@ then
 fi
 user=$1
 campaign=$2
-hdfs dfs -ls -R /store/user/${user} |grep "${campaign}$"|sed "s:.*/store/user/${user}/\(.*\)/\(.*/AODSIM/.*\):    '\1' \: '/store/user/${user}/\1/\2':"
+hdfs dfs -ls -R /store/user/${user} |grep "${campaign}$"|sed "s:.*/store/user/${user}/\(.*\)/\(.*/AODSIM/.*\):    \"\1\" \: \"/store/user/${user}/\1/\2\",:"
