00e69f64744c718b2bf63dbacfa103991cb06be7
Nick Smith <ncsmith6@wisc.edu>
2015-02-11 19:15:00 -0600

    More sleuthing tools for finding PAT

--- MetaData/tuples/findUserTuples.py
+++ MetaData/tuples/findUserTuples.py
@@ -1,5 +1,8 @@
-from FinalStateAnalysis.MetaData.datadefs import datadefs
+#!/bin/env python
+import sys
 import subprocess
+import json
+from FinalStateAnalysis.MetaData.datadefs import datadefs
 
 def getUserHDFSlisting ( username ) :
   cmd = ['hdfs dfs']
@@ -15,12 +18,37 @@ def getUserHDFSlisting ( username ) :
       dir_subset.append(dirs[i])
   return dir_subset
 
-store_user = getUserHDFSlisting('nsmith')
-json_lines = []
-for name, datadef in datadefs.iteritems() :
-  if 'datasetpath' in datadef :
-    for dir in store_user :
-      if datadef['datasetpath'] in dir :
-        json_lines.append('    "%s" : "%s"' % (name, '/hdfs'+dir) )
+def findUserPatConfigJsonFiles ( username ) :
+  user_pat_configs = []
+  with subprocess.Popen("./scrape_pat_config.sh "+username, shell=True, stdout=subprocess.PIPE).stdout as user_pat_configs_filelist :
+    for file in user_pat_configs_filelist :
+      fn = file.strip()
+      # dict is wrapped by hash but hash is in dict...
+      wrapped_dict = json.load(open(fn, 'r'))
+      if len(wrapped_dict.keys()) > 0 :
+        dict = wrapped_dict[wrapped_dict.keys()[0]]
+        dict['source'] = fn
+        user_pat_configs.append(dict)
+  return user_pat_configs
 
-print ",\n".join(json_lines)
+if __name__ == "__main__" :
+  username = sys.argv[1]
+  store_user = getUserHDFSlisting(username)
+  user_pat_configs = findUserPatConfigJsonFiles(username)
+
+  pat_locations = {}
+  for name, datadef in datadefs.iteritems() :
+    pat_locations[name] = []
+    if 'datasetpath' in datadef :
+      for dir in store_user :
+        if datadef['datasetpath'] in dir :
+          loc = {}
+          loc['dir'] = '/hdfs'+dir
+          for config in user_pat_configs :
+            if config['PAT Location'] == loc['dir'] :
+              loc['possible PAT config'] = config
+          pat_locations[name].append(loc)
+    if len(pat_locations[name]) == 0 :
+      pat_locations.pop(name)
+
+  print json.dumps(pat_locations, indent=4, separators=(',',': '))
