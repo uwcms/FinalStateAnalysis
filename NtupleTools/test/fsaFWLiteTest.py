0ec3e894520231e1ed95d2c36dcdd5b030befb8d
Nate Woods <nwoods@hep.wisc.edu>
2015-05-13 14:46:41 -0500

    Illustrate gen collection as well.

--- NtupleTools/test/fsaFWLiteTest.py
+++ NtupleTools/test/fsaFWLiteTest.py
@@ -19,12 +19,14 @@ from DataFormats.FWLite import Handle, Events
 events = Events("file:ntuplize.root")
 
 fs4Mu, fs4MuLabel = Handle("edm::OwnVector<PATFinalState,edm::ClonePolicy<PATFinalState> >"), "cleanedFinalStateMuMuMuMu"
+gen, genLabel = Handle("std::vector<reco::GenParticle>"), "prunedGenParticles"
 
 
 n4MuFS = 0
 for iev, ev in enumerate(events):
 
     ev.getByLabel(fs4MuLabel, fs4Mu)
+    ev.getByLabel(genLabel, gen)
 
     print "event %d:%d:%d: %d 4 mu final states:"%(ev.eventAuxiliary().run(),
                                                    ev.eventAuxiliary().luminosityBlock(),
@@ -36,6 +38,11 @@ for iev, ev in enumerate(events):
         for i in range(4):
             print '        mu %d: charge %d, pt %.3f'%(i, fs.daughter(i).pdgId()/abs(fs.daughter(i).pdgId()), fs.daughter(i).pt())
         print "    # jets: %d"%len(fs.evt().jets())
+    
+    print "    Final state gen muons:"
+    for g in gen.product():
+        if abs(g.pdgId()) == 13 and g.status() == 1:
+            print "        pt: %.3f  eta: %.3f  phi: %.3f"%(g.pt(), g.eta(), g.phi())
 
     if n4MuFS > 10:
         break
