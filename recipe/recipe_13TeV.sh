0584c9ed20f4445b5c90254030c30811727e7195
Devin Taylor <dntaylor@wisc.edu>
2015-06-23 09:27:19 -0500

    fix to cbid (turn of svfit)

--- recipe/recipe_13TeV.sh
+++ recipe/recipe_13TeV.sh
@@ -13,13 +13,6 @@ MINOR_VERSION=`echo $CMSSW_VERSION | sed "s|CMSSW_\([0-9]\)_\([0-9]\)_.*|\2|"`
 
 pushd $CMSSW_BASE/src
 
-# recipe for electron cutbased ids (https://twiki.cern.ch/twiki/bin/viewauth/CMS/CutBasedElectronIdentificationRun2#Recipe_for_regular_users_for_74X)
-git cms-merge-topic 9003 #this is the version that is in CMSSW_7_4_X
-rm -rf RecoEgamma/ElectronIdentification/data
-git clone https://github.com/cms-data/RecoEgamma-ElectronIdentification.git RecoEgamma/ElectronIdentification/data
-rm -rf RecoEgamma/PhotonIdentification/data
-git clone https://github.com/cms-data/RecoEgamma-PhotonIdentification.git RecoEgamma/PhotonIdentification/data
-
 # HZZ MELA, MEKD etc.
 if [ "$HZZ" = "1" ]; then
     echo "Checking out ZZ MELA and Higgs combine"
