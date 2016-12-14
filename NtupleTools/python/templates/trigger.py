8fd53e562c419b6845d0a3f5e8d30195d18a3dc5
Silvia Taroni <Silvia.Taroni@cern.ch>
2016-12-14 11:22:22 -0600

    adding new triggers in ntuples

--- NtupleTools/python/templates/trigger.py
+++ NtupleTools/python/templates/trigger.py
@@ -35,6 +35,11 @@ singleLepton_25ns_MC = PSet(
         paths=r'HLT_IsoMu22_v\\d+'
         ),
     _trig_template.replace(
+        name='singleIsoMu22eta2p1',
+        paths=r'HLT_IsoMu22_eta2p1_v\\d+'
+        ),
+
+    _trig_template.replace(
         name='singleIsoMu27',
         paths=r'HLT_IsoMu27_v\\d+'
         ),
@@ -47,8 +52,24 @@ singleLepton_25ns_MC = PSet(
         paths=r'HLT_IsoTkMu22_v\\d+'
         ),
     _trig_template.replace(
+        name='singleIsoTkMu22eta2p1',
+        paths=r'HLT_IsoTkMu22_eta2p1_v\\d+'
+        ),
+    _trig_template.replace(
         name='singleIsoMu24',
         paths=r'HLT_IsoMu24_v\\d+'
+        ),
+    _trig_template.replace(
+        name='singleIsoTkMu24',
+        paths=r'HLT_IsoTkMu24_v\\d+'
+        ),
+    _trig_template.replace(
+        name='singleIsoMu24eta2p1',
+        paths=r'HLT_IsoMu24_eta2p1__v\\d+'
+        ),
+    _trig_template.replace(
+        name='singleIsoTkMu24eta2p1',
+        paths=r'HLT_IsoTkMu24_eta2p1__v\\d+'
         ),
     _trig_template.replace(
         name='singleE',
@@ -107,6 +128,10 @@ singleLepton_25ns = PSet(
         paths=r'HLT_IsoTkMu22_v\\d+'
         ),
     _trig_template.replace(
+        name='singleIsoTkMu22eta2p1',
+        paths=r'HLT_IsoTkMu22_eta2p1_v\\d+'
+        ),
+    _trig_template.replace(
         name='singleIsoMu20',
         paths=r'HLT_IsoMu20_v\\d+'
         ),
@@ -121,6 +146,14 @@ singleLepton_25ns = PSet(
     _trig_template.replace(
         name='singleIsoMu27',
         paths=r'HLT_IsoMu27_v\\d+'
+        ),
+    _trig_template.replace(
+        name='singleIsoMu24',
+        paths=r'HLT_IsoMu24_v\\d+'
+        ),
+    _trig_template.replace(
+        name='singleIsoTkMu24',
+        paths=r'HLT_IsoTkMu24_v\\d+'
         ),
     _trig_template.replace(
         name='singleE27LooseEr',
@@ -214,6 +247,10 @@ doubleLepton_25ns = PSet(
         paths=r'HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v\\d+'
         ),
     _trig_template.replace(
+        name='singleMu23SingleE8',
+        paths=r'HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_DZ_v\\d+'
+        ),
+    _trig_template.replace(
         name='doubleTau40',
         paths=r'HLT_DoubleMediumIsoPFTau40_Trk1_eta2p1_Reg_v\\d+'
         ),
@@ -225,7 +262,19 @@ doubleLepton_25ns = PSet(
         name='doubleTau32',
         paths=r'HLT_DoubleMediumIsoPFTau32_Trk1_eta2p1_Reg_v\\d+'
         ),
-    _trig_template.replace(
+     _trig_template.replace(
+         name='doubleTauCmbIso35Reg',
+         paths=r'HLT_DoubleMediumCombinedIsoPFTau35_Trk1_eta2p1_Reg_v\\d+'
+         ),
+     _trig_template.replace(
+         name='doubleTauCmbIso40Reg',
+         paths=r'HLT_DoubleMediumCombinedIsoPFTau40_Trk1_eta2p1_Reg_v\\d+'
+         ),
+     _trig_template.replace(
+         name='doubleTauCmbIso40',
+         paths=r'HLT_DoubleMediumCombinedIsoPFTau40_Trk1_eta2p1_v\\d+'
+         ),
+     _trig_template.replace(
         name='singleE22SingleTau20SingleL1',
         paths=r'HLT_Ele22_eta2p1_WPLoose_Gsf_LooseIsoPFTau20_SingleL1_v\\d+'
         ),
@@ -261,7 +310,14 @@ doubleLepton_25ns = PSet(
         name='singleE36SingleTau30',
         paths=r'HLT_Ele36_eta2p1_WPLoose_Gsf_LooseIsoPFTau20_SingleL1_v\\d+'
         ),
-
+    _trig_template.replace(
+        name='singleMu19eta2p1LooseTau20singleL1',
+        paths=r'HLT_IsoMu19_eta2p1_LooseIsoPFTau20_SingleL1_v\\d+'
+        ),
+    _trig_template.replace(
+        name='singleMu19eta2p1LooseTau20',
+        paths=r'HLT_IsoMu19_eta2p1_LooseIsoPFTau20_v\\d+'
+        ),
     )
 
 tripleLepton = PSet(
