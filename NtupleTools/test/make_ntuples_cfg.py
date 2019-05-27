9b7b866eb973dc0a5efd19284ce624735b72eff2
Cecile Caillol <cecilecaillol@gmail.com>
2019-05-27 11:24:43 -0500

    rerunning met filter

--- NtupleTools/test/make_ntuples_cfg.py
+++ NtupleTools/test/make_ntuples_cfg.py
@@ -608,11 +608,36 @@ if options.htt :
         verbose = cms.untracked.bool(False),
     )
 
+    process.load('RecoMET.METFilters.ecalBadCalibFilter_cfi')
+    
+    baddetEcallist = cms.vuint32(
+        [872439604,872422825,872420274,872423218,
+         872423215,872416066,872435036,872439336,
+         872420273,872436907,872420147,872439731,
+         872436657,872420397,872439732,872439339,
+         872439603,872422436,872439861,872437051,
+         872437052,872420649,872422436,872421950,
+         872437185,872422564,872421566,872421695,
+         872421955,872421567,872437184,872421951,
+         872421694,872437056,872437057,872437313])
+    
+    
+    process.ecalBadCalibReducedMINIAODFilter = cms.EDFilter(
+        "EcalBadCalibFilter",
+        EcalRecHitSource = cms.InputTag("reducedEgamma:reducedEERecHits"),
+        ecalMinEt        = cms.double(50.),
+        baddetEcal    = baddetEcallist, 
+        taggingMode = cms.bool(True),
+        debug = cms.bool(False)
+        )
+    
+
     process.filterTagger = cms.Path(
         process.badGlobalMuonTaggerMAOD
         + process.cloneGlobalMuonTaggerMAOD
         + process.BadPFMuonFilter
         + process.BadChargedCandidateFilter
+	+ process.ecalBadCalibReducedMINIAODFilter 
         + process.filterFlags)
 
     process.schedule.append( process.filterTagger )
